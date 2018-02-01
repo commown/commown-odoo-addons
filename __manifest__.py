500613c6002cea3ce911a9656e4247be13d81e31
Florent Cayré <florent.cayre@logilab.fr>
2018-02-01 11:11:58 +0100

    Add the Commown application with dependencies to payment_slimpay and l10n_fr_certification

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Commown SCIC SAS',
+    'category': 'Business',
+    'summary': 'Commown SCIC SAS business application',
+    'version': '10.0.1.0.0',
+    'description': """Commown SCIC SAS business application on top of dependant modules""",
+    'author': "Commown SCIC SAS",
+    'license': "AGPL-3",
+    'website': "https://commown.fr",
+    'depends': ['payment_slimpay', 'l10n_fr_certification'],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
