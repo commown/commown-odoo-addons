e40c72a34442fb9d06d8c4c8478866e4bde3725f
Florent Cayré <florent.cayre@logilab.fr>
2018-02-07 12:41:10 +0100

    Use OCA website_sale_require_login module to force e-commerce users to get an account

--- __manifest__.py
+++ __manifest__.py
@@ -7,8 +7,11 @@
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
     'website': "https://commown.fr",
-    'depends': ['payment_slimpay', 'l10n_fr_certification', 'base_action_rule',
-                'website_sale_default_country', 'auth_signup'],
+    'depends': [
+        'payment_slimpay', 'l10n_fr_certification', 'base_action_rule',
+        'website_sale_default_country', 'auth_signup',
+        'website_sale_require_login',
+    ],
     'external_dependencies': {},
     'data': [
         'views/address_template.xml',
