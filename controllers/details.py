974ee330ca024bcafe12b6f663ef4ed50127b0c8
Florent Cayré <florent@commown.fr>
2018-03-27 15:24:41 +0200

    Display Slimpay's partner validation rule errors nicely on partner details page
    Those rules may trigger a more cryptic Slimpay error when calling the
    API.

--- controllers/details.py
+++ controllers/details.py
@@ -17,6 +17,20 @@ class WebSiteAccount(website_account):
         "id_card1", "id_card2", "proof_of_address", "state_id",
     ]
 
+    def details_form_validate(self, data):
+        """ Add Slimpay validation of submitted partner data """
+        error, error_message = super(
+            WebSiteAccount, self).details_form_validate(data)
+        Partner = http.request.env['res.partner']
+        values = {key: data[key] for key in self.MANDATORY_BILLING_FIELDS}
+        values.update({key: data[key] for key in self.OPTIONAL_BILLING_FIELDS
+                       if key in data})
+        values.update({'zip': values.pop('zipcode', '')})
+        for attribute, message in Partner.slimpay_checks(values).items():
+            error[attribute] = 'error'
+            error_message.append(message)
+        return error, error_message
+
     @http.route(['/my/account'])
     def details(self, redirect=None, **post):
         if post:
