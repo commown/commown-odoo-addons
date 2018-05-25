7a6682074a7d0c5005a419208667930abde6aec8
Florent Cayré <florent@commown.fr>
2018-05-25 16:34:39 +0200

    [payment] Add test to check slimpay label for contract payments
    A mock is used to avoid http calls to Slimpay.
    
    For the moment, this label is equal to the invoice's number.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,3 @@
 from . import test_sale_order
 from . import test_res_partner
+from . import test_payment
---
+++ tests/test_payment.py
@@ -0,0 +1,38 @@
+from odoo.addons.contract.tests.test_contract import TestContractBase
+
+from mock import patch
+
+from odoo.tests.common import at_install, post_install
+
+
+@at_install(False)
+@post_install(True)
+class ContractPaymentTC(TestContractBase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ContractPaymentTC, cls).setUpClass()
+        slimpay = cls.env['payment.acquirer'].search(
+            [('provider', '=', 'slimpay')]).ensure_one()
+        payment_token = cls.env['payment.token'].create({
+            'name': 'Test Slimpay Token',
+            'partner_id': cls.contract.partner_id.id,
+            'active': True,
+            'acquirer_id': slimpay.id,
+            'acquirer_ref': 'Slimpay mandate ref',
+        })
+        cls.contract.payment_token_id = payment_token
+
+    def setUp(self):
+        super(ContractPaymentTC, self).setUp()
+        client_patcher = patch('odoo.addons.payment_slimpay.models.'
+                               'slimpay_utils.get_client')
+        client_patcher.start()
+        self.addCleanup(client_patcher.stop)
+
+    def test_payin_label(self):
+        with patch('odoo.addons.payment_slimpay.models.'
+                   'slimpay_utils.SlimpayClient.create_payin') as create_payin:
+            invoice = self.contract.recurring_create_invoice()
+            label = create_payin.call_args[0][-1]
+            self.assertEqual(label, invoice.number)
