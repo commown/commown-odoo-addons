58b921e6e545c38549c553f6fae000f0cf74092b
Florent Cayré <florent@commown.fr>
2018-07-04 05:04:22 +0200

    [tests] Add tests for sale order automatic actions

--- tests/test_sale_order.py
+++ tests/test_sale_order.py
@@ -16,9 +16,15 @@ class SaleOrderTC(TransactionCase):
             'user_ids': [(6, 0, [self.user.id])]})
         self.g1 = self.env['res.groups'].create({'name': 'standard'})
         self.g2 = self.env['res.groups'].create({'name': 'premium'})
+        contract_template = self.env['account.analytic.contract'].create({
+            'recurring_rule_type': 'monthly',
+            'recurring_interval': 1,
+            'name': 'Test Contract Template',
+            })
         product = self.env['product.product'].create({
             'name': 'fairphone rental', 'type': 'service',
-        })
+            'is_rental': True, 'rental_contract_tmpl_id': contract_template.id,
+            })
         product.product_tmpl_id.support_group_ids |= self.g1 + self.g2
         oline = (0, 0, {
             'name': product.name, 'product_id': product.id,
@@ -45,11 +51,40 @@ class SaleOrderTC(TransactionCase):
         self.assertIn(self.g1, self.user.groups_id)
         self.assertIn(self.g2, self.user.groups_id)
 
-    def test_add_buyer_to_product_support_groups_ar(self):
+    def test_buyer_automatic_actions(self):
+        """ Buying a product must trigger automatic actions, check some:
+        - add buyer to product's support groups
+        - add a rental followup card
+        - add a specific receivable account for the buyer
+        - add a rental contract
+        """
         # Check test prerequisites
         self.assertFalse(self.user.groups_id & (self.g1 | self.g2))
         # Trigger the automatic action
         self.so.write({'state': 'sent'})
-        # Check action effects
+        #
+        # Check effects:
+        #
+        # - support groups
         self.assertIn(self.g1, self.user.groups_id)
         self.assertIn(self.g2, self.user.groups_id)
+        # - followup card
+        sales_team_id = self.env.ref('sales_team.salesteam_website_sales').id
+        partner = self.so.partner_id
+        leads = self.env['crm.lead'].search([
+            ('team_id', '=', sales_team_id),
+            ('partner_id', '=', partner.id),
+            ('name', 'ilike', '%' + self.so.name + '%'),
+        ])
+        self.assertEqual(len(leads), 1)
+        # - receivable account
+        self.assertEqual(partner.property_account_receivable_id.name,
+                         partner.name)
+        # - rental contract
+        product = self.so.product_id
+        contracts = self.env['account.analytic.account'].search([
+            ('partner_id', '=', partner.id),
+            ('name', 'ilike', '%' + self.so.name + '%'),
+            ('contract_template_id', '=', product.rental_contract_tmpl_id.id),
+        ])
+        self.assertEqual(len(contracts), 1)
