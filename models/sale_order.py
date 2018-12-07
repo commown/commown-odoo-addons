a7ac4fbe4900b34897c80c459448bf1708f22ea9
Florent Cayré <florent@commown.fr>
2018-12-07 14:31:19 +0100

    Add a sale order method to get all rental contract attachments
    They will be considered as contractual documents.

--- models/sale_order.py
+++ models/sale_order.py
@@ -13,3 +13,12 @@ class ProductRentalSaleOrder(models.Model):
         self.ensure_one()
         return any(line.product_id.product_tmpl_id.is_rental
                    for line in self.order_line)
+
+    @api.multi
+    def contractual_documents(self):
+        self.ensure_one()
+        rcts = self.mapped('order_line.product_id.rental_contract_tmpl_id')
+        return self.env['ir.attachment'].search([
+            ('res_model', '=', 'account.analytic.contract'),
+            ('res_id', 'in', rcts.ids),
+        ])
