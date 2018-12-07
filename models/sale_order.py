a59ea25219465b588a2d61c766c9b10891dff7be
Florent Cayré <florent@commown.fr>
2018-12-07 15:31:15 +0100

    Send contractual documents when a sale with rental contracts is sent

--- models/sale_order.py
+++ models/sale_order.py
@@ -22,3 +22,15 @@ class ProductRentalSaleOrder(models.Model):
             ('res_model', '=', 'account.analytic.contract'),
             ('res_id', 'in', rcts.ids),
         ])
+
+    @api.multi
+    def action_quotation_send(self):
+        self.ensure_one()
+        email_act = super(ProductRentalSaleOrder, self).action_quotation_send()
+        order_attachments = self.contractual_documents()
+        if order_attachments:
+            ids = [att.id for att in sorted(order_attachments,
+                                            key=lambda att: att.name)]
+            email_act['context'].setdefault(
+                'default_attachment_ids', []).append((6, 0, ids))
+        return email_act
