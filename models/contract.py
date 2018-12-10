11fffe30d2e67abfceed0cd4685e13df9f2429c4
Florent Cayré <florent@commown.fr>
2018-12-10 22:37:39 +0100

    [model] Add a computed field to display the commitment period end date

--- models/contract.py
+++ models/contract.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountAnalyticContract(models.Model):
@@ -12,5 +12,24 @@ class AccountAnalyticContract(models.Model):
     )
 
     min_contract_duration = fields.Integer(
-        string='Min contract duration',
-        help='Minimum contract duration in recurring interval unit')
+        string='Min contract duration', default=0,
+        help='Minimum contract duration in recurring interval unit',
+    )
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    min_contract_end_date = fields.Date(
+        string='Min contract end date',
+        compute='_compute_min_end_date', store=True,
+    )
+
+    @api.depends('date_start', 'min_contract_duration')
+    def _compute_min_end_date(self):
+        for record in self:
+            date_delta = self.get_relative_delta(
+                record.recurring_rule_type,
+                record.min_contract_duration*record.recurring_interval)
+            record.min_contract_end_date = fields.Date.from_string(
+                record.date_start) + date_delta
