03d36968e7681187f215d11e55a9b73411abb31d
Florent Cayré <florent@commown.fr>
2019-04-03 09:03:04 +0200

    Bump version to 1.1.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Rating project issue: Net Promoter Score',
     'category': 'Business',
     'summary': 'Implement net promoter score on top of odoo project issue rating',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
     'website': "https://commown.fr",
