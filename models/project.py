c7b0eb0e2f3bbed421dec1cbe1f29e40d702bf5b
Florent Cayré <florent@commown.fr>
2019-04-03 09:36:08 +0200

    Fix incorrect path to rate icons in issue's threads

--- models/project.py
+++ models/project.py
@@ -2,7 +2,7 @@
 
 from datetime import timedelta
 
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class Project(models.Model):
@@ -36,3 +36,44 @@ class Project(models.Model):
                 base_domain + [('rating', '<=', 6)])
             self.net_promoter_score = int(
                 100 * (1. * promoters_count - detractors_count) / total_count)
+
+
+class ProjectIssue(models.Model):
+
+    _inherit = 'project.issue'
+
+    @api.multi
+    def rating_apply(self, rate, token=None, feedback=None, subtype=None):
+        """ Overloading of the `rating` module's method to avoid the hard-coded
+        path to this module's images.
+        """
+        Rating, rating = self.env['rating.rating'], None
+        if token:
+            rating = self.env['rating.rating'].search(
+                [('access_token', '=', token)], limit=1)
+        else:
+            rating = Rating.search([('res_model', '=', self._name),
+                                    ('res_id', '=', self.ids[0])], limit=1)
+        if rating:
+            rating.write(
+                {'rating': rate, 'feedback': feedback, 'consumed': True})
+            if hasattr(self, 'message_post'):
+                feedback = tools.plaintext2html(feedback or '')
+                body = ("<img src='/rating_project_issue_nps/static/src"
+                        "/img/rate_%s.png' style='width:20px;height:20px;"
+                        "float:left;margin-right: 5px;'/>%s")
+                # None will set the default author in mail_thread.py
+                author_id = rating.partner_id and rating.partner_id.id or None
+                self.message_post(
+                    body=body % (rate, feedback),
+                    subtype=subtype or "mail.mt_comment",
+                    author_id=author_id,
+                )
+            if (hasattr(self, 'stage_id') and self.stage_id
+                    and hasattr(self.stage_id, 'auto_validation_kanban_state')
+                    and self.stage_id.auto_validation_kanban_state):
+                if rating.rating > 5:
+                    self.write({'kanban_state': 'done'})
+                if rating.rating < 5:
+                    self.write({'kanban_state': 'blocked'})
+        return rating
