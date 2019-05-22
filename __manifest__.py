c7f99ab51921c35c483c4721609ff23765e8d4df
Florent Cayré <florent@commown.fr>
2019-05-22 06:12:14 +0200

    Automatically send a SMS to the customer when we warn him about an issue by mail
    SMS Pro is used for that purpose. The email API was chosen for its
    ability:
    - to be used without much code (maintenance cost)
    - to easily integrate prod / dev environments without risks

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Commown slimpay issue integration',
+    'category': 'Business',
+    'summary': 'Commown slimpay issue integration',
+    'version': '10.0.0.0.1',
+    'description': "Commown slimpay issue integration",
+    'author': "Commown SCIC SAS",
+    'license': "AGPL-3",
+    'website': "https://commown.coop",
+    'depends': [
+        'payment_slimpay_issue',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'data/mail_template.xml',
+        'data/project.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
