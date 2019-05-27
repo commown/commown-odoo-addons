08185bb41d5428ee0af8c961ec0f2b2228874aee
Florent Cayré <florent@commown.fr>
2019-05-27 09:57:36 +0200

    Do not copy sale note into invoice comment

--- models/sale_order.py
+++ models/sale_order.py
@@ -96,3 +96,15 @@ class SaleOrder(models.Model):
                 generate_invoice_rental_line(
                     contract, CONTRACT_ACCESSORY_MARKER,
                     accessory, int(so_line.product_uom_qty))
+
+    @api.multi
+    def _prepare_invoice(self):
+        """We do not want the sale order note to be copied to the invoice
+        comment (pre-sale notes do generally not apply to invoices).
+        We want to be able to add an invoice note (the "comment"
+        attribute) however, so we do not remove it from the report but
+        rather suppress the sale > invoice copy.
+        """
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals.pop('comment', None)
+        return invoice_vals
