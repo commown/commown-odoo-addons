ca098c3f83e2fc2a98514c74a2f6356aa15be28d
Florent Cayré <florent@commown.fr>
2019-07-11 13:42:39 +0200

    By default, use slimpay acquirer reference in the dump/ restore methods

--- models/payment.py
+++ models/payment.py
@@ -7,12 +7,15 @@ class PaymentAcquirerSlimpayDumpRestore(models.Model):
     _inherit = 'payment.acquirer'
 
     @api.model
-    def _slimpay_dump_all_mandates(self, refresh=True,
+    def _slimpay_dump_all_mandates(self, refresh=True, acquirer=None,
                                    mandates_fpath='/tmp/mandates.json'):
-        slimpay = self.env.ref('payment.payment_acquirer_slimpay')
-        dump_all_mandates(slimpay, refresh, mandates_fpath)
+        if acquirer is None:
+            acquirer = self.env.ref('payment.payment_acquirer_slimpay')
+        dump_all_mandates(acquirer, refresh, mandates_fpath)
 
     @api.model
-    def _slimpay_restore_mandates(self, acquirer,
+    def _slimpay_restore_mandates(self, acquirer=None,
                                   mandates_fpath='/tmp/mandates.json'):
+        if acquirer is None:
+            acquirer = self.env.ref('payment.payment_acquirer_slimpay')
         restore_all_missing_mandates(acquirer, mandates_fpath)
