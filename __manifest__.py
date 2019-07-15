f50c99a9db70609ac87ff4cc8d54387d3966a496
Florent Cayré <florent@commown.fr>
2019-07-15 21:39:51 +0200

    Bump version to 1.7.1

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Commown SCIC SAS',
     'category': 'Business',
     'summary': 'Commown SCIC SAS business application',
-    'version': '10.0.1.7.0',
+    'version': '10.0.1.7.1',
     'description': """Commown SCIC SAS business application on top of dependant modules""",
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
