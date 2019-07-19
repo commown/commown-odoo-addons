2b96c778c6f3e03e6eca2a27d35ea24cb8cde6c8
Florent Cayré <florent@commown.fr>
2019-07-19 12:02:00 +0200

    [test] Rename test file

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 from . import test_sale_order
 from . import test_res_partner
-from . import test_payment
+from . import test_contract
 from . import test_crm_lead
 from . import test_reports
 from . import test_project_issue
