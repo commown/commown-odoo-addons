f0a8e74d8d2d63ca2382944c53c1e65d74ecf3f5
Florent Cayré <florent@commown.fr>
2019-08-07 13:09:11 +0200

    Fix invoice payment mode check
    It was not testing `auto_merge` value. One side effect was that the
    duplication of any non auto_merge=True invoice was impossible.

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -13,6 +13,6 @@ class InvoiceMergeAutoPayInvoice(models.Model):
     @api.constrains('auto_merge')
     def _check_auto_merge(self):
         for inv in self:
-            if not inv.payment_mode_id:
+            if inv.auto_merge and not inv.payment_mode_id:
                 raise models.ValidationError(
                     'Payment mode is needed to auto pay an invoice')
