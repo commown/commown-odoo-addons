cd07bd082a39685b4b5ace5a619585dc9cc10e35
Florent Cayré <florent@commown.fr>
2019-11-14 09:51:05 +0100

    [tests] Fix test crash related to request mock
    Probably a change in the order the setUp methods are executed (after
    moving some code to another module for instance), lead to a "unbound"
    request object in affiliation module.
    
    Fixed by starting the mock before executing super setUp.

--- tests/common.py
+++ tests/common.py
@@ -6,14 +6,14 @@ from odoo.tests.common import TransactionCase
 class MockedEmptySessionMixin(object):
 
     def setUp(self):
-        super(MockedEmptySessionMixin, self).setUp()
-
         request_patcher = patch('odoo.addons.website_sale_affiliate'
                                 '.models.sale_affiliate_request.request')
         self.request_mock = request_patcher.start()
         self.request_mock.configure_mock(session={})
         self.fake_session = self.request_mock.session
         self.addCleanup(request_patcher.stop)
+
+        super(MockedEmptySessionMixin, self).setUp()
 
 
 class MockedEmptySessionTC(MockedEmptySessionMixin, TransactionCase):
