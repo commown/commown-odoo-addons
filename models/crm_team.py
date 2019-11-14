8c7264cee1b1ae7778897ef36bbf49b88f4cc743
Florent Cayré <florent@commown.fr>
2019-11-14 17:21:57 +0100

    Fix crm team used_for_shipping label

--- models/crm_team.py
+++ models/crm_team.py
@@ -5,6 +5,6 @@ class CrmTeam(models.Model):
     _name = 'crm.team'
     _inherit = ['crm.team', 'commown.shipping.parent.mixin']
 
-    used_for_shipping = fields.Boolean('Track delivery', default=False)
+    used_for_shipping = fields.Boolean('Used for shipping', default=False)
     on_delivery_email_template_id = fields.Many2one(
         'mail.template', string='Default delivery email model for this team')
