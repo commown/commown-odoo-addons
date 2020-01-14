293678813c3ffbefd60daf8a58176d8e84089002
Florent Cayré <florent@commown.fr>
2020-01-14 14:02:50 +0100

    Bump version to 1.9.3

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Commown SCIC SAS',
     'category': 'Business',
     'summary': 'Commown SCIC SAS business application',
-    'version': '10.0.1.9.2',
+    'version': '10.0.1.9.3',
     'description': "Commown SCIC SAS business application",
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
