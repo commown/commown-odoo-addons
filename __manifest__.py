583aa4305f6247210280a27b9cc5fecb1f52d0ff
Florent Cayré <florent@commown.fr>
2020-03-24 19:08:26 +0100

    empty module

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Copyright (C) 2018 - Today: Commown (https://commown.coop)
+# @author: Florent Cayré
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'My module name',
+    'category': '',
+    'version': '10.0.0.0.1',
+    'author': "Commown SCIC SAS",
+    'license': "AGPL-3",
+    'website': "https://commown.coop",
+    'external_dependencies': {
+        # 'python': []
+    },
+    'depends': [
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
