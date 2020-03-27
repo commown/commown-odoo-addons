eb8840f04f46fe40aafcca3e0f2192c45948fde0
Florent Cayré <florent@commown.fr>
2020-03-27 09:26:53 +0100

    [camera] Add 5th step when everything was solved online

--- static/src/fp2-camera.js
+++ static/src/fp2-camera.js
@@ -5,11 +5,14 @@ $(document).ready(function(){
         $("#smartwizard li").eq(2).toggleClass('disabled', this.value!=='new');
         $("#smartwizard li").eq(3).toggleClass('disabled', this.value!=='old');
         $("#smartwizard li").eq(4).toggleClass('disabled', this.value!=='old');
+        $('input[name="screwdriver"]').attr('required', this.value==='old' ? 'required' : null)
     });
 
     $('#tighten_screws_solved_pb').change(function () {
         $("#smartwizard li").eq(3).toggleClass('disabled', this.value!=='no');
         $("#smartwizard li").eq(4).toggleClass('disabled', this.value!=='no');
+        $('input[name="screwdriver"]').attr('required', this.value==='no' ? 'required' : null)
+        $("#smartwizard li").eq(5).toggleClass('disabled', this.value!=='yes');
     });
 
     $('#camera_model').change();
