2b20a1b02015cf6f14dec4ae74423837646fec15
Florent Cayré <florent@commown.fr>
2020-03-31 12:25:36 +0200

    Refactor js a bit and dpply basic javascript linting

--- static/src/fp2-camera.js
+++ static/src/fp2-camera.js
@@ -1,16 +1,17 @@
-$(document).ready(function(){
+/* global $ */
+$(document).ready(function() {
 
-    var $wizard = setUpWizard($('#smartwizard'));
+    const wizard = setUpWizard($('#smartwizard'));
 
     $('#camera_model').change(function () {
-        $wizard.toggleStep(2, this.value==='new');
-        $wizard.toggleStep(5, this.value==='new');
+        wizard.toggleStep(2, this.value==='new');
+        wizard.toggleStep(5, this.value==='new');
     });
 
     $('#tighten_screws_solved_pb').change(function () {
-        $wizard.toggleStep(3, this.value==='no');
-        $wizard.toggleStep(4, this.value==='no');
-        $wizard.toggleStep(5, this.value==='yes');
+        wizard.toggleStep(3, this.value==='no');
+        wizard.toggleStep(4, this.value==='no');
+        wizard.toggleStep(5, this.value==='yes');
     });
 
 });
--- static/src/setup_wizard.js
+++ static/src/setup_wizard.js
@@ -1,98 +1,112 @@
+/* global $ */
+// eslint-disable-next-line no-unused-vars
 function setUpWizard($container) {
+  /*
+   * Utility vars and functions
+   */
 
-    var extraButtons = [];
+  const widgets = ['input', 'select', 'radio', 'checkbox', 'textarea'];
+  const formStepIdPrefix = 'form-step-';
+  const allStepsSelector = 'div[id*="' + formStepIdPrefix + '"]';
 
-    var $humanContactButton = $container.find('button[value="contact"]');
-    if ($humanContactButton.length) {
-        var prefix = 'form-step-';
-        var contactStep = parseInt(
-            $humanContactButton
-                .closest('div[id*="form-step"]')[0].id
-                .substring(prefix.length));
-        extraButtons.push(
-            $('<button/>').text('Contacter un être humain !')
-                .addClass('btn btn-warning')
-                .on('click', function(event) {
-                    event.preventDefault();
-                    var wizard = $container.data('smartWizard');
-                    $.map(wizard.steps, function(step, stepIndex) {
-                        if (stepIndex !== contactStep) {
-                            $container.toggleStep(stepIndex, false);
-                            wizard.stepState(stepIndex, 'disable');
-                        }
-                    });
-                    wizard.goToStep(contactStep);
-                })
-        );
-    }
+  function stepNumber(formContainer) {
+    return parseInt(formContainer.id.substring(formStepIdPrefix.length), 10);
+  }
 
-    $container.smartWizard({
-        selected: 0,
-        keyNavigation: false,
-        useURLhash: false,
-        showStepURLhash: false,
-        transitionEffect: 'slide',
-        toolbarSettings: {
-            toolbarPosition: 'top',
-            toolbarButtonPosition: 'end', // does not work with bootstrap 3!
-            toolbarExtraButtons: extraButtons
-        },
-        theme: 'arrows',
-        lang: {
-            next: 'Suivant',
-            previous: 'Précédent',
-        },
-        anchorSettings: {
-            markDoneStep: true, // add done css
-            markAllPreviousStepsAsDone: false, // When a step selected by url hash, all previous steps are marked done
-            removeDoneStepOnNavigateBack: true, // While navigate back done step after active step will be cleared
-            enableAnchorOnDoneStep: true // Enable/Disable the done steps navigation
-        }
+  function computeRequiredFields() {
+    const requiredFields = {};
+    const selector = widgets.map(function(w) { return w + '[required]'; }).join(',');
+    $container.find(allStepsSelector).each(function(index, formContainer) {
+      requiredFields[stepNumber(formContainer)] = $(selector, formContainer);
     });
+    return requiredFields;
+  }
 
-    $container.on("leaveStep", function(e, anchorObject, stepNumber, stepDirection) {
-        var elmForm = $("#form-step-" + stepNumber);
-        // stepDirection === 'forward' :- this condition allows to do the form validation
-        // only on forward navigation, that makes easy navigation on backwards still do the validation when going next
-        if(stepDirection === 'forward' && elmForm){
-            elmForm.validator('validate');
-            var elmErr = elmForm.find('.form-group.has-error');
-            if(elmErr && elmErr.length > 0){
-                // Form validation failed
-                return false;
-            }
-        }
-        return true;
-    });
-
-    var prefix = "form-step-";
-    var required_fields = {};
-    var widgets = ['input', 'select', 'radio', 'checkbox', 'textarea'];
-
-    var selector = widgets.map(function(w) {return w + '[required]'}).join(',');
-
-    $container.find('div[id*="form-step-"]').each(function(index, formContainer) {
-        var step = formContainer.id.substring(prefix.length);
-        required_fields[step] = $(selector, formContainer);
+  function createHumanContactButton(contactStep) {
+    return $('<button/>').text('Contacter un être humain !')
+      .addClass('btn btn-warning')
+      .on('click', function(event) {
+        event.preventDefault();
+        const wizard = $container.data('smartWizard');
+        $.map(wizard.steps, function(step, stepIndex) {
+          if (stepIndex !== contactStep) {
+            $container.toggleStep(stepIndex, false);
+            wizard.stepState(stepIndex, 'disable');
+          }
+        });
+        $container.toggleStep(contactStep, true);
+        wizard.goToStep(contactStep);
       });
+  }
 
-    /**
-     * Enable or disable a wizard step, handling required attributes
-     * at the same time (i.e: unsetting required on fields which step
-     * is disabled, resetting it if step is re-enabled)
-     *
-     * @param  {integer} number    0-index of the step.
-     * @param  {boolean} enabled   0-index of the step.
-     */
-    $container.toggleStep = function(number, enabled) {
-        $container.find("li").eq(number).toggleClass('disabled', !enabled);
-        required_fields[number].attr('required', enabled ? 'required' : null);
-        $container
-            .find('div[id*="form-step-' + number + '"]')
-            .find(widgets.join(','))
-            .attr('disabled', enabled ? null : 'disabled');
+  // coucou
+
+  let contactStep = null;
+  const extraButtons = [];
+  const requiredFields = computeRequiredFields();
+
+  const $humanContactButton = $container.find('button[value="contact"]');
+  if ($humanContactButton.length) {
+    contactStep = stepNumber($humanContactButton.closest(allStepsSelector)[0]);
+    extraButtons.push(createHumanContactButton(contactStep));
+  }
+
+  const wizard = $container.smartWizard({
+    selected: 0,
+    keyNavigation: false,
+    useURLhash: false,
+    showStepURLhash: false,
+    transitionEffect: 'slide',
+    toolbarSettings: {
+      toolbarPosition: 'top',
+      toolbarButtonPosition: 'end', // does not work with bootstrap 3!
+      toolbarExtraButtons: extraButtons,
+    },
+    theme: 'arrows',
+    lang: {
+      next: 'Suivant',
+      previous: 'Précédent',
+    },
+    anchorSettings: {
+      markDoneStep: true,
+      markAllPreviousStepsAsDone: false,
+      removeDoneStepOnNavigateBack: true,
+      enableAnchorOnDoneStep: true,
+    },
+  });
+
+  $container.on('leaveStep', function(e, anchorObject, stepNum, stepDirection) {
+    const elmForm = $('#' + formStepIdPrefix + stepNum);
+    if (stepDirection === 'forward' && elmForm) {
+      elmForm.validator('validate');
+      const elmErr = elmForm.find('.form-group.has-error');
+      if (elmErr && elmErr.length > 0) {
+        return false;
+      }
     }
+    return true;
+  });
 
-    return $container;
+  /**
+   * Enable or disable a wizard step, handling required attributes
+   * at the same time (i.e: unsetting required on fields which step
+   * is disabled, resetting it if step is re-enabled)
+   *
+   * @param  {integer} number    0-index of the step.
+   * @param  {boolean} enabled   0-index of the step.
+   */
+  wizard.toggleStep = function(number, enabled) {
+    $container.find('li').eq(number).toggleClass('disabled', !enabled);
+    requiredFields[number].attr('required', enabled ? 'required' : null);
+    $container
+      .find('#' + formStepIdPrefix + number)
+      .find(widgets.join(','))
+      .attr('disabled', enabled ? null : 'disabled');
+  };
 
+  if (contactStep !== null) {
+    wizard.toggleStep(contactStep, false);
+  }
+
+  return wizard;
 }
