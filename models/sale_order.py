671d7ff21370204d6e9e4ac43182bb0741dff284
Florent Cayré <florent@commown.fr>
2020-06-04 15:32:33 +0200

    [model] Add computed fields related to contracts Also improve the way has_rental() works, using the relations to contract, preserving backward compatibility.

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import api, models
+from odoo import api, models, fields
 
 
 _logger = logging.getLogger(__name__)
@@ -9,11 +9,31 @@ _logger = logging.getLogger(__name__)
 class ProductRentalSaleOrder(models.Model):
     _inherit = "sale.order"
 
+    has_rental_product = fields.Boolean(
+        'Has rental product', compute='_compute_has_rental', store=True)
+
+    contract_count = fields.Integer(
+        u'Related contract count', compute='_compute_contract_count')
+
+    def _compute_contract_count(self):
+        for sale in self:
+            sale.contract_count = len(sale.order_line.mapped('contract_id'))
+
+    @api.depends(
+        'order_line',
+        'order_line.product_uom_qty',
+        'order_line.product_id.product_tmpl_id.is_rental',
+    )
+    def _compute_has_rental(self):
+        for sale in self:
+            sale.has_rental_product = any(
+                l.product_id.product_tmpl_id.is_rental and l.product_uom_qty > 0
+                for l in sale.order_line)
+
     @api.multi
     def has_rental(self):
         self.ensure_one()
-        return any(line.product_id.product_tmpl_id.is_rental
-                   for line in self.order_line)
+        return self.has_rental_product
 
     @api.multi
     def contractual_documents(self):
