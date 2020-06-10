0c65ce95b2af0549d661359b0a3aa5beba5e2697
Florent Cayré <florent@commown.fr>
2020-06-10 16:03:37 +0200

    Fix transaction label for invoices creating by contract, after contract module update
    This still requires fixes in the contract module, an issue will soon be
    created on OCA for this.

--- models/account_analytic_account.py
+++ models/account_analytic_account.py
@@ -1,6 +1,11 @@
+import logging
+
 from dateutil.relativedelta import relativedelta
 
 from odoo import api, models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountAnalyticAccount(models.Model):
@@ -15,39 +20,13 @@ class AccountAnalyticAccount(models.Model):
             result.append((record.id, name))
         return result
 
-    def _transaction_label_subtitutions(self, invoice):
-        """ Return a dict {marker: value} mapping marker in the contract's
-        transaction label attribute value to their real value.
-        As of today, the returned markers are #START#, #END#, #INV#.
-        """
-        lang = self.env['res.lang'].search(
-            [('code', '=', self.partner_id.lang)])
-        date_format = lang.date_format or '%m/%d/%Y'
-        if self.recurring_invoicing_type == 'pre-paid':
-            date_from = self.env.context['old_date']
-            date_to = self.env.context['next_date'] - relativedelta(days=1)
-        else:
-            date_from = (
-                self.env.context['old_date'] -
-                self.get_relative_delta(self.recurring_rule_type,
-                                        self.recurring_interval) +
-                relativedelta(days=1))
-            date_to = self.env.context['old_date']
-        return {
-            '#INV#': invoice.number,
-            '#START#': date_from.strftime(date_format),
-            '#END#': date_to.strftime(date_format),
-        }
-
     @api.multi
     def _pay_invoice(self, invoice):
         """ Insert custom payment transaction label into the context
         before executing the standard payment process. """
-        if (self.transaction_label
-                and 'old_date' in self.env.context
-                and 'next_date' in self.env.context):
-            label = self.transaction_label
-            for k, v in self._transaction_label_subtitutions(invoice).items():
-                label = label.replace(k, v)
+        if self.transaction_label:
+            label = self._insert_markers(self.transaction_label)
+            label = label.replace('#INV#', invoice.number)
+            _logger.debug('Bank label for invoice %s: %s', invoice.number, label)
             self = self.with_context(slimpay_payin_label=label)
         return super(AccountAnalyticAccount, self)._pay_invoice(invoice)
