8eab9fbf0c1d03ce5e834d6370e1ff96ce7aaf70
Chafique <chafique.delli@akretion.com>
2020-06-12 10:51:37 +0200

    [ADD] custom_report module

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Commown SCIC SAS (https://commown.fr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Custom reports',
+    'category': 'Custom',
+    'summary': 'Custom reports',
+    'version': '10.0.1.0.0',
+    'description': """This module adds custom reports.""",
+    'author': "Commown SCIC SAS",
+    'license': "AGPL-3",
+    'website': "https://commown.fr",
+    'depends': [
+        'report_py3o_fusion_server',
+        'account_usability',  # needed for account_invoice.has_discount (py3o)
+        'base_company_extension',  # required for legal_type (py3o)
+        'sale_usability',  # required for sale.layout_category (py3o)
+        'l10n_fr_siret',   # required for siret (py3o)
+        'scic',     # required for is_equity (py3o)
+    ],
+    'data': [
+        'data/fusion_server.xml',
+        'report/reports.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
