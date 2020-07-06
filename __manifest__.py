0b991869e8aa6c0bcadd6457d6a383fb9b00b310
Florent Cayré <florent@commown.fr>
2020-07-06 18:49:28 +0200

    Add/ improve package metadata (add icon, fix website, improve name)

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2020 Commown SCIC SAS (https://commown.fr)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Custom reports',
+    'name': 'Commown Custom reports',
     'category': 'Custom',
     'summary': 'Custom reports',
     'version': '10.0.1.0.0',
     'description': """This module adds custom reports.""",
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
-    'website': "https://commown.fr",
+    'website': "https://commown.coop",
     'depends': [
         'report_py3o_fusion_server',
         'account_usability',  # needed for account_invoice.has_discount (py3o)
