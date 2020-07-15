26c260f21efd33bcc162488bdfb5397dc18279ee
Chafique <chafique.delli@akretion.com>
2020-07-15 15:15:37 +0200

    [MIG] commown_ergonomy_asset module to V12

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,19 @@
+# Copyright 2020 Commown SCIC SAS (https://commown.fr)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Commown ergonomy: assets',
-    'category': 'Business',
+    'category': 'Accounting & Finance',
     'summary': 'Assets ergonomy improvements for Commown',
-    'version': '10.0.0.0.1',
+    'version': '12.0.1.0.0',
     'description': "Assets ergonomy improvements for Commown",
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
     'website': "https://commown.coop",
     'depends': [
-        'account_asset',
+        'account_asset_management',
     ],
-    'external_dependencies': {
-    },
     'data': [
         'views/account_asset.xml',
-    ],
-    'qweb': [
     ],
     'installable': True,
 }
--- models/account_asset.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class AccountAssetAsset(models.Model):
-    _inherit = 'account.asset.asset'
-    _name = 'account.asset.asset'
-
-    value_residual = fields.Float(store=True)
