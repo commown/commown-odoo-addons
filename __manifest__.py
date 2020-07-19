9595a2a3418d0407789546307459d6f00296a046
Florent Cayré <florent@commown.fr>
2020-07-19 12:24:38 +0200

    Bump version to 1.3.0

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 {
     'name': 'Payment Slimpay issue',
     'category': 'Website',
-    'version': '10.0.1.2.2',
+    'version': '10.0.1.3.0',
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
     'website': "https://commown.fr",
