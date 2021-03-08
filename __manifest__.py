8bc1cf53be0491667f276efada81c21e9fba5234
Florent Cayré <florent@commown.fr>
2021-03-08 10:57:42 +0100

    Migrate to 12.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,19 @@
-# coding: utf-8
-# Copyright (C) 2018 - Today: Commown (https://commown.coop)
+# Copyright (C) 2021 Commown SCIC (https://commown.coop)
 # @author: Florent Cayré
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Commown - LineageOS',
     'category': '',
-    'version': '10.0.0.0.1',
-    'author': "Commown SCIC SAS",
+    'version': '12.0.1.0.0',
+    'author': "Commown SCIC",
     'license': "AGPL-3",
     'website': "https://commown.coop",
     'external_dependencies': {
         # 'python': []
     },
     'depends': [
-        'base_action_rule',
+        'base_automation',
         'crm',
         'commown_shipping',
     ],
