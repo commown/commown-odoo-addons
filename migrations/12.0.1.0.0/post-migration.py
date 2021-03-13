df82e5f3f01727df3ab9d689acb4bdf856a9a89f
Florent Cayré <florent@commown.fr>
2021-03-13 18:35:42 +0100

    Use SQL in the migration script

--- migrations/12.0.1.0.0/post-migration.py
+++ migrations/12.0.1.0.0/post-migration.py
@@ -7,16 +7,29 @@ _logger = logging.getLogger(__name__)
 
 def _rename_contract_template_commitment_info(env):
     _logger.info("Rename contract template commitment info")
+    openupgrade.logged_query(
+        env.cr, """
+        UPDATE contract_template_line CTL
+        SET commitment_duration = CT.min_contract_duration
+        FROM contract_template CT
+        WHERE CT.id = CTL.contract_id
+        """)
+
     for template in env['contract.template'].search([]):
         template.commitment_duration = template.min_contract_duration
 
 
 def _move_contract_commitment_info_to_line(env):
     _logger.info("Move contract commitment info to line level")
-    for contract in env['contract.contract'].search([]):
-        for line in contract.contract_line_ids:
-            line.commitment_end_date = contract.min_contract_end_date
-            line.commitment_duration = contract.min_contract_duration
+    openupgrade.logged_query(
+        env.cr, """
+        UPDATE contract_line CL
+        SET
+          commitment_end_date = CC.min_contract_end_date,
+          commitment_duration = CC.min_contract_duration
+        FROM contract_contract CC
+        WHERE CC.id = CL.contract_id
+        """)
 
 
 @openupgrade.migrate()
