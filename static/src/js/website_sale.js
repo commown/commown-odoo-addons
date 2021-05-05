c52fb198a990bc0f3e2dbc2522c1e0d1f3c44926
Florent Cayré <florent@commown.fr>
2021-05-05 22:51:07 +0200

    [website] Display deposit absolute number instead of ratio to lease payment
    This is nicer for offers where this ratio is not an integer, and it
    makes customer more aware of the fact this payment is not an advance of
    the first lease payments.

--- static/src/js/website_sale.js
+++ static/src/js/website_sale.js
@@ -35,4 +35,49 @@ odoo.define('product_rental.website_sale', function(require) {
         });
     });
 
+  // Remove me ASAP!
+  // This code updates the deposit amount when the variant selector changes.
+  // It should be much better in v12...
+
+  var base = require('web_editor.base');
+  var utils = require('web.utils');
+  var core = require('web.core');
+  var _t = core._t;
+
+  function price_to_str(price) {
+    var l10n = _t.database.parameters;
+    var precision = 2;
+
+    if ($(".decimal_precision").length) {
+      precision = parseInt($(".decimal_precision").last().data('precision'));
+      if (!precision) { precision = 0; }
+    }
+    var formatted = _.str.sprintf('%.' + precision + 'f', price).split('.');
+    formatted[0] = utils.insert_thousand_seps(formatted[0]);
+    return formatted.join(l10n.decimal_point);
+  }
+
+  $('.oe_website_sale').on('change', 'input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]', function (ev) {
+    var $ul = $(ev.target).closest('.js_add_cart_variants');
+    var $parent = $ul.closest('.js_product');
+    var variant_ids = $ul.data("attribute_value_ids");
+    var values = [];
+    var unchanged_values = $parent.find('div.oe_unchanged_value_ids').data('unchanged_value_ids') || [];
+    var ratio = $('.oe_website_sale .oe_rental_price').data('rental-ratio');
+
+    $parent.find('input.js_variant_change:checked, select.js_variant_change').each(function () {
+      values.push(+$(this).val());
+    });
+    values =  values.concat(unchanged_values);
+
+    for (var k in variant_ids) {
+      if (_.isEmpty(_.difference(variant_ids[k][1], values))) {
+        $.when(base.ready()).then(function() {
+          $(".oe_deposit_price .oe_currency_value").html(price_to_str(variant_ids[k][2]*ratio))
+        });
+        break;
+      }
+    }
+  })
+
 });
