2402f45a35952acc712752db1d8695f491c00f36
Florent Cayré <florent@commown.fr>
2021-05-16 18:47:01 +0200

    Fix website tests
    ... and factor out common test pattern into a view rendering method.

--- product_rental/tests/test_website.py
+++ product_rental/tests/test_website.py
@@ -1,5 +1,9 @@
 import lxml.html
+from mock import patch
 
+from odoo.addons.website.models.website import Website  # see mock
+
+from odoo import api
 from odoo.addons.website.tools import MockRequest
 
 from .common import RentalSaleOrderTC
@@ -13,27 +17,30 @@ class WebsiteTC(RentalSaleOrderTC):
 
     def setUp(self):
         super().setUp()
-        partner = self.env.ref('base.res_partner_3')
+        partner = self.env.ref('base.partner_demo_portal')
         self.so = self.create_sale_order(partner)
         self.so.action_confirm()
-        self.website = self.env.ref('website.default_website')
+        # Use a portal user to avoid language selector rendering
+        # (other page is editable and the selector is more complex)
+        env = api.Environment(self.env.cr, partner.user_ids[0].id, {})
+        self.website = self.env.ref('website.default_website').with_env(env)
         self.contracts = self.env['contract.contract'].search([
             ('name', 'ilike', '%' + self.so.name + '%'),
             ])
 
+    def render_view(self, ref, **render_kwargs):
+        view = self.env.ref(ref)
+        with patch.object(Website, 'get_alternate_languages', return_value=()):
+            with MockRequest(self.env, website=self.website) as request:
+                request.httprequest.args = []
+                request.httprequest.query_string = u''
+                request.endpoint_arguments = {}
+                html = view.render(render_kwargs)
+        return lxml.html.fromstring(html)
+
     def test_portal_sale_order_view(self):
-        view = self.env.ref('sale.sale_order_portal_template')
-
-        with MockRequest(self.env, website=self.website) as request:
-            request['httprequest']['args'] = []
-            request.csrf_token = lambda *args: 'token'
-            html = view.render({
-                'sale_order': self.so,
-                'website': self.website,
-                'main_object': (),
-            })
-
-        doc = lxml.html.fromstring(html)
+        doc = self.render_view('sale.sale_order_portal_template',
+                               sale_order=self.so, website=self.website)
         product_names = map(_clean_text, doc.xpath("//td[@id='product_name']"))
         self.assertEqual(list(product_names), [
             'Fairphone Premium (deposit)',
@@ -47,20 +54,10 @@ class WebsiteTC(RentalSaleOrderTC):
         ])
 
     def test_portal_contract_view(self):
-        contract = self.contracts[0]
-        view = self.env.ref('contract.portal_contract_page')
-
-        with MockRequest(self.env, website=self.website) as request:
-            request['httprequest']['args'] = []
-            request.csrf_token = lambda *args: 'token'
-            html = view.render({
-                "page_name": "Contracts",
-                "contract": contract,
-                'website': self.website,
-                'main_object': (),
-            })
-
-        doc = lxml.html.fromstring(html)
+        doc = self.render_view('contract.portal_contract_page',
+                               page_name='Contracts',
+                               contract=self.contracts[0],
+                               website=self.website)
         self.assertEqual(
             doc.xpath("//div[@id='general_information']//h6//text()"),
             ['Customer:'])  # Responsible was removed from original view
