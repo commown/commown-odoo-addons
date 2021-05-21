ba9ac023e441e8b4e4b66a3d94dacc23d9af8444
Florent Cayré <florent@commown.fr>
2021-05-21 18:09:31 +0200

    Turn crm_lead to contract relation into an access button

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -1,9 +1,21 @@
-from odoo import models, fields
+from odoo import models, _
 
 
 class CrmLead(models.Model):
     _inherit = "crm.lead"
 
-    contract_id = fields.Many2one(
-        'account.analytic.account',
-        string=u'Contract')
+    def button_open_contract(self):
+        contract = self.env['account.analytic.account'].search([
+            ('recurring_invoice_line_ids.sale_order_line_id',
+             '=', self.so_line_id.id),
+        ])
+        view = self.env.ref(
+            'contract.account_analytic_account_recurring_form_form')
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.analytic.account',
+            'res_id': contract.id,
+            'name': _('Related contracts'),
+            'context': {'is_contract': 1},
+            'views': [(view.id, 'form')],
+        }
