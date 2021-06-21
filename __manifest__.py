e799ab79aebcc8a62d5860cb7d563e9314d8273c
Florent Cayré <florent@commown.coop>
2021-06-21 15:41:44 +0200

    Bump version to 1.24.4

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Commown SCIC',
     'category': 'Business',
     'summary': 'Commown SCIC business application',
-    'version': '10.0.1.24.3',
+    'version': '10.0.1.24.4',
     'description': "Commown SCIC business application",
     'author': "Commown SCIC",
     'license': "AGPL-3",
