a24a4e84c908fedb9875c32d5e4ef67ac81f04a6
Florent Cayré <florent@commown.coop>
2021-07-02 12:51:04 +0200

    Add partner method to get or create its supplier location

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,6 +4,31 @@ from odoo import models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    def _set_location(self, usage, parent_xml_id, attr):
+        parent_location = self.env.ref(parent_xml_id)
+
+        if getattr(self, attr) == parent_location:
+            location = self.env['stock.location'].create({
+                'name': self.name,
+                'usage': usage,
+                'partner_id': self.id,
+                'location_id': parent_location.id,
+            })
+            self.update({attr: location.id})
+
+        return getattr(self, attr)
+
+    def set_supplier_location(self):
+        """Set property_stock_supplier to a dedicated location and return it
+
+        The dedicated location is a child of standard location for all
+        suppliers.
+        """
+        return self._set_location('supplier',
+                                  'stock.stock_location_suppliers',
+                                  'property_stock_supplier')
+
+
     def set_customer_location(self):
         """Set property_stock_customer to a dedicated location and return it
 
@@ -11,15 +36,6 @@ class ResPartner(models.Model):
         customers.
         """
 
-        customer_location = self.env.ref('stock.stock_location_customers')
-
-        if self.property_stock_customer == customer_location:
-            location = self.env['stock.location'].create({
-                'name': self.name,
-                'usage': 'internal',
-                'partner_id': self.id,
-                'location_id': customer_location.id,
-            })
-            self.update({'property_stock_customer': location.id})
-
-        return self.property_stock_customer
+        return self._set_location('internal',
+                                  'stock.stock_location_customers',
+                                  'property_stock_customer')
