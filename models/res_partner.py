addb5885923630f5e8843985ae84022378e8c170
Florent Cayré <florent@commown.coop>
2021-07-09 16:55:04 +0200

    Only set custom locations on customer partners
    Supplier locations must be configured properly (especially the "usage"
    field) to avoid quant duplication. This work is left to the odoo user.

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,31 +4,6 @@ from odoo import models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    def _set_location(self, usage, parent_xml_id, attr):
-        parent_location = self.env.ref(parent_xml_id)
-
-        if getattr(self, attr) == parent_location:
-            location = self.env['stock.location'].create({
-                'name': self.name,
-                'usage': usage,
-                'partner_id': self.id,
-                'location_id': parent_location.id,
-            })
-            self.update({attr: location.id})
-
-        return getattr(self, attr)
-
-    def set_supplier_location(self):
-        """Set property_stock_supplier to a dedicated location and return it
-
-        The dedicated location is a child of standard location for all
-        suppliers.
-        """
-        return self._set_location('supplier',
-                                  'stock.stock_location_suppliers',
-                                  'property_stock_supplier')
-
-
     def set_customer_location(self):
         """Set property_stock_customer to a dedicated location and return it
 
@@ -36,6 +11,15 @@ class ResPartner(models.Model):
         customers.
         """
 
-        return self._set_location('customer',
-                                  'stock.stock_location_customers',
-                                  'property_stock_customer')
+        parent_location = self.env.ref('stock.stock_location_customers')
+
+        if self.property_stock_customer == parent_location:
+            location = self.env['stock.location'].create({
+                'name': self.name,
+                'usage': 'customer',
+                'partner_id': self.id,
+                'location_id': parent_location.id,
+            })
+            self.update({'property_stock_customer': location.id})
+
+        return self.property_stock_customer
