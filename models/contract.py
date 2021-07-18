d5226be3b90d8f0ade5f2e3d81b4a449206de468
Florent Cayré <florent@commown.coop>
2021-07-18 07:49:53 +0200

    Use double quotes instead of quotes
    This is the standard Black configuration in v12.

--- models/contract.py
+++ models/contract.py
@@ -10,50 +10,50 @@ class Contract(models.Model):
     _inherit = "account.analytic.account"
 
     picking_ids = fields.One2many(
-        'stock.picking',
-        'contract_id',
-        string=u'Pickings')
+        "stock.picking",
+        "contract_id",
+        string=u"Pickings")
 
     def send_all_picking(self):
         products = self.recurring_invoice_line_ids.mapped(
-            'sale_order_line_id.product_id.product_tmpl_id.stockable_product_id'
+            "sale_order_line_id.product_id.product_tmpl_id.stockable_product_id"
         )
 
         if not products:
-            _logger.warning('Empty initial picking for contract %s', self.name)
+            _logger.warning("Empty initial picking for contract %s", self.name)
             return None
         elif len(products) > 1:
-            _logger.warning('More than 1 product to deliver for %s', self.name)
+            _logger.warning("More than 1 product to deliver for %s", self.name)
             return None
 
         # XXX hardcoded
-        picking_type = self.env.ref('stock.picking_type_internal')
-        orig_location = self.env.ref('stock.stock_location_stock')
+        picking_type = self.env.ref("stock.picking_type_internal")
+        orig_location = self.env.ref("stock.stock_location_stock")
 
         # XXX incorrect partner for B2B:
         dest_location = self.partner_id.set_customer_location()
 
         picking_data = {
-            'move_type': 'direct',
-            'picking_type_id': picking_type.id,
-            'location_id': orig_location.id,
-            'location_dest_id': dest_location.id,
-            'min_date': fields.Date.today(),
-            'origin': self.name,
-            'move_lines': [
+            "move_type": "direct",
+            "picking_type_id": picking_type.id,
+            "location_id": orig_location.id,
+            "location_dest_id": dest_location.id,
+            "min_date": fields.Date.today(),
+            "origin": self.name,
+            "move_lines": [
                 (0, 0, {
-                    'name': '/',
-                    'picking_type_id': picking_type.id,
-                    'location_id': orig_location.id,
-                    'location_dest_id': dest_location.id,
-                    'product_id': products.id,
-                    'product_uom_qty': 1,  # XXX hardcoded
-                    'product_uom': self.env.ref('product.product_uom_unit').id,
+                    "name": "/",
+                    "picking_type_id": picking_type.id,
+                    "location_id": orig_location.id,
+                    "location_dest_id": dest_location.id,
+                    "product_id": products.id,
+                    "product_uom_qty": 1,  # XXX hardcoded
+                    "product_uom": self.env.ref("product.product_uom_unit").id,
                 })
             ],
         }
 
-        picking = self.env['stock.picking'].create(picking_data)
+        picking = self.env["stock.picking"].create(picking_data)
         picking.action_confirm()
         picking.action_assign()
         self.picking_ids |= picking
