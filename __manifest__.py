7b13bdf347f409ea80c49c1bd8581cbca9fd2d8f
Florent Cayré <florent@commown.coop>
2021-07-22 10:14:28 +0200

    Bump version 10.0.1.0.2

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Contract variable discount',
     'category': 'Contract Management',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.0.2',
     'author': "Commown SCIC",
     'license': "AGPL-3",
     'website': "https://commown.coop",
