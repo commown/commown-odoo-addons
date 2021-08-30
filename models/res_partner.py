a9cb3427578af6e649e98096b151cf2b76e605ba
Florent Cayré <florent@commown.coop>
2021-08-30 13:03:24 +0200

    Add debug logs for tracking stock location creations and attributions to customers

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,4 +1,9 @@
+import logging
+
 from odoo import models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
@@ -16,10 +21,11 @@ class ResPartner(models.Model):
         if self.property_stock_customer == parent_location:
             # Handle B2B: do not create a location for each employee
             # Special case: our own employees!
+            _logger.debug(u"Partner %d (%s) has property_stock_customer=%s",
+                          self.id, self.name, self.property_stock_customer.name)
             if (self.commercial_partner_id != self
                     and self.commercial_partner_id.id != 1):
                 location = self.commercial_partner_id.set_customer_location()
-
             else:
                 location = self.env['stock.location'].create({
                     'name': self.name,
@@ -27,6 +33,12 @@ class ResPartner(models.Model):
                     'partner_id': self.id,
                     'location_id': parent_location.id,
                 })
+                _logger.debug(u"Created location %d (%s) for partner %d (%s)",
+                              location.id, location.name, self.id, self.name)
             self.update({'property_stock_customer': location.id})
-
+            _logger.debug(u"Set location %d (%s) on partner %d (%s)",
+                          location.id, location.name, self.id, self.name)
+        _logger.debug(u"set_customer_location result %d (%s), partner %d (%s)",
+                      self.property_stock_customer.id,
+                      self.property_stock_customer.name, self.id, self.name)
         return self.property_stock_customer
