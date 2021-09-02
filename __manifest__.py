f5b10af0d5e568f8e6dba8a95a22c28f3f033870
Florent Cayré <florent@commown.coop>
2021-09-02 14:57:22 +0200

    Bump version to 1.1.3

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 {
     'name': 'Commown devices',
     'category': 'stock',
-    'version': '10.0.1.1.2',
+    'version': '10.0.1.1.3',
     'author': "Commown SCIC",
     'license': "AGPL-3",
     'website': "https://commown.coop",
