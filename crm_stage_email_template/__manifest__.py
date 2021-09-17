04fc1e5407b7dd8b3ac9b9a4eccbbd6159f97507
Florent Cayré <florent@commown.coop>
2021-09-17 14:11:57 +0200

    Import the crm_stage_email_template module into the 12.0 branch

---
+++ crm_stage_email_template/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2021 Commown SCIC (https://commown.coop)
+# @author: Florent Cayré
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'CRM stage email template',
+    'category': '',
+    'version': '12.0.1.0.0',
+    'author': "Commown SCIC",
+    'license': "AGPL-3",
+    'website': "https://commown.coop",
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_stage.xml',
+    ],
+    'installable': True,
+}
