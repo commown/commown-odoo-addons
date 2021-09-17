2f52f5f29c8c952ce5d2b2ec5e55dcc375e9a15f
Florent Cayré <florent@commown.coop>
2021-09-17 14:11:54 +0200

    Import the contract_variable_discount module into the 10.0 branch

---
+++ contract_variable_discount/models/contract.py
@@ -0,0 +1,220 @@
+# coding: utf-8
+# Copyright (C) 2021 - Commown (https://commown.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+import uuid
+
+from odoo import fields, models, api, _
+
+
+_logger = logging.getLogger(__name__)
+
+
+def _format_discount(value):
+    if isinstance(value, unicode):
+        return value
+    return u"%.2f %%" % value
+
+
+def _format_amount(value):
+    if isinstance(value, unicode):
+        return value
+    return u"%.2f €" % value
+
+
+class ContractTemplateLine(models.Model):
+    _inherit = "account.analytic.contract.line"
+
+    discount_line_ids = fields.One2many(
+        string=u"Variable discount lines",
+        comodel_name="contract.template.discount.line",
+        inverse_name="contract_template_line_id",
+        cascade="delete",
+        copy=True,
+    )
+
+    variable_discount = fields.Boolean(
+        "Variable discount?", store=False, compute='_compute_variable_discount')
+
+    @api.depends('discount_line_ids')
+    def _compute_variable_discount(self):
+        for record in self:
+            record.variable_discount = bool(record.discount_line_ids)
+
+
+class ContractLine(models.Model):
+    _inherit = "account.analytic.invoice.line"
+
+    contract_template_line_id = fields.Many2one(
+        string=u"Contract template line",
+        help=u"Contract template line which discount lines apply here",
+        comodel_name="account.analytic.contract.line",
+        domain=lambda self: self._domain_contract_template_line_id(),
+    )
+
+    inherited_discount_line_ids = fields.One2many(
+        string=u"Inherited discount lines",
+        help=u"Discount lines from the related contract model",
+        related=u"contract_template_line_id.discount_line_ids",
+        readonly=True,
+    )
+
+    specific_discount_line_ids = fields.One2many(
+        string=u"Specific discount lines",
+        help=u"These lines complete the contract template's, if any",
+        comodel_name="contract.discount.line",
+        inverse_name="contract_line_id",
+        copy=True,
+    )
+
+    variable_discount = fields.Boolean(
+        "Variable discount?", store=False, compute='_compute_variable_discount')
+
+    def _domain_contract_template_line_id(self):
+        contract = self.analytic_account_id
+        if not contract and "analytic_account_id" in self.env.context:
+            contract = contract.browse(self.env.context["analytic_account_id"])
+        return [('analytic_account_id', '=', contract.contract_template_id.id)]
+
+    @api.depends('contract_template_line_id.discount_line_ids',
+                 'specific_discount_line_ids')
+    def _compute_variable_discount(self):
+        for record in self:
+            record.variable_discount = bool(
+                record.specific_discount_line_ids or
+                record.contract_template_line_id.discount_line_ids)
+
+    @api.multi
+    def compute_discount(self, date_invoice):
+        """Compute the discount of current contract line for given invoice date
+
+        Returns a dict like:
+        - total: the total computed amount
+        - descriptions: a list of strings describing the discounts
+
+        A contract line specific discount line may replace a discount
+        of its related contract template line using the
+        replace_discount_line_id relation.
+
+        0-valued discounts are ignored, so to remove a contract
+        template's discount, one can replace it with a 0-valued
+        contract line discount.
+        """
+
+        total_discount = 0.
+        customer_descriptions = []
+
+        for discount_line in self._applicable_discount_lines():
+            value = discount_line.compute(self, date_invoice)
+            if value is not None and value > 0.:
+                total_discount += value
+                customer_descriptions.append(discount_line.name)
+
+        return {
+            "total": total_discount,
+            "descriptions": customer_descriptions,
+        }
+
+    def _applicable_discount_lines(self):
+        """ Yields applicable discount lines, either contract.discount.line or
+        contract.template.discount.line instances.
+        """
+        self.ensure_one()
+
+        c_lines = self.specific_discount_line_ids
+        replaced_ids = c_lines.mapped('replace_discount_line_id').ids
+
+        for line in self.contract_template_line_id.discount_line_ids:
+            if line.id not in replaced_ids:
+                yield line
+
+        for line in c_lines:
+            yield line
+
+
+class Contract(models.Model):
+    _inherit = "account.analytic.account"
+    discount_date_units = {"days", "weeks", "months", "years"}
+
+    @api.multi
+    def _convert_contract_lines(self, contract):
+        """On each contract line, add the relation to the contract template
+        line which generated it.
+
+        This makes it easy to find the application contract template
+        discount lines.
+        """
+        new_lines = super(Contract, self)._convert_contract_lines(contract)
+        for (_0, _0, vals), contract_line in zip(
+                new_lines, contract.recurring_invoice_line_ids):
+            vals["contract_template_line_id"] = contract_line.id
+            vals.pop("discount_line_ids", None)
+        return new_lines
+
+    @api.model
+    def _prepare_invoice_line(self, line, invoice_id):
+        "Compute discount and append discount description to invoice line name"
+        vals = super(Contract, self)._prepare_invoice_line(line, invoice_id)
+
+        date_invoice = fields.Date.from_string(
+            line.analytic_account_id.recurring_next_date)
+        result = line.compute_discount(date_invoice)
+
+        vals["discount"] = result["total"]
+        descriptions = result["descriptions"]
+        if descriptions:
+            vals["name"] += u"\n" + (_("Applied discounts:\n- %s")
+                                     % u"\n- ".join(descriptions))
+
+        return vals
+
+    @api.multi
+    def simulate_payments(self):
+        self.ensure_one()
+
+        max_date = fields.Date.from_string(self.date_start)
+
+        for contract_line in self.recurring_invoice_line_ids:
+            for discount_line in contract_line._applicable_discount_lines():
+                date_start = discount_line._compute_date(contract_line, "start")
+                if date_start > max_date:
+                    max_date = date_start
+                if discount_line.end_type != "empty":
+                    date_end = discount_line._compute_date(contract_line, "end")
+                    if date_end > max_date:
+                        max_date = date_end
+
+        _logger.debug("Contract payment simulation until: %s...", max_date)
+
+        inv_data = []
+        last_amount = None
+
+        point_name = uuid.uuid1().hex
+        self.env.cr.execute('SAVEPOINT "%s"' % point_name)
+
+        # This is ugly (as is_auto_pay is introduced by contract_payment_auto)
+        # BUT it is so dangerous not to unset it in the simulation that we
+        # prefer this over more complex and not so secure solutions...
+        if getattr(self, 'is_auto_pay', False):
+            self.is_auto_pay = False
+
+        last_date = fields.Date.from_string(self.recurring_next_date)
+        try:
+            while last_date < max_date:
+                inv = self.recurring_create_invoice()
+                last_date = fields.Date.from_string(inv.date_invoice)
+                if last_amount != inv.amount_total:
+                    _logger.debug("> KEEP invoice %s (amount %s)",
+                                  inv.date_invoice, inv.amount_total)
+                    last_amount = inv.amount_total
+                    data = inv.read()[0]
+                    data["invoice_line_ids"] = inv.invoice_line_ids.read()
+                    inv_data.append(data)
+                else:
+                    _logger.debug("> SKIP invoice %s (amount %s)",
+                                  inv.date_invoice, inv.amount_total)
+
+            return inv_data
+        finally:
+            self.env.cr.execute('ROLLBACK TO SAVEPOINT "%s"' % point_name)
---
+++ contract_variable_discount/tests/test_contract.py
@@ -0,0 +1,258 @@
+# coding: utf-8
+
+from datetime import date
+
+import lxml.html
+from mock import patch
+
+from odoo.addons.contract.tests.test_contract import TestContractBase
+from odoo import fields, models
+
+
+class TestConditionDiscountLine(models.Model):
+    _inherit = 'contract.discount.line'
+
+    condition = fields.Selection(selection_add=[('test', "Test")])
+
+    def _compute_condition_test(self, line, date_invoice):
+        "Overriden by a mock"
+
+
+class ContractTC(TestContractBase):
+
+    @classmethod
+    def _init_test_model(cls, model_cls):
+        """ Build a model from model_cls in order to test abstract models.
+        Note that this does not actually create a table in the database, so
+        there may be some unidentified edge cases.
+        Args:
+            model_cls (openerp.models.BaseModel): Class of model to initialize
+        Returns:
+            model_cls: Instance
+        """
+        registry = cls.env.registry
+        cr = cls.env.cr
+        inst = model_cls._build_model(registry, cr)
+        model = cls.env[model_cls._inherit].with_context(todo=[])
+        model._prepare_setup()
+        model._setup_base(partial=False)
+        model._setup_fields(partial=False)
+        model._setup_complete()
+        model._auto_init()
+        model.init()
+        model._auto_end()
+        return inst
+
+    @classmethod
+    def setUpClass(cls):
+        super(ContractTC, cls).setUpClass()
+        cls._init_test_model(TestConditionDiscountLine)
+
+    def tdiscount(self, ct_line=None, **kwargs):
+        kwargs.setdefault("contract_template_line_id", ct_line.id)
+        return self.env['contract.template.discount.line'].create(kwargs)
+
+    def cdiscount(self, c_line=None, **kwargs):
+        kwargs.setdefault("contract_line_id", (c_line or self.acct_line).id)
+        return self.env['contract.discount.line'].create(kwargs)
+
+    def set_cdiscounts(self, discounts):
+        self.acct_line.specific_discount_line_ids = discounts
+
+    def check_cdiscounts(self, expected_discounts):
+        for discount_date, expected_value in sorted(expected_discounts):
+            while self.contract.recurring_next_date <= discount_date:
+                invoice = self.contract.recurring_create_invoice()
+                if invoice.date_invoice == discount_date:
+                    actual = invoice.mapped('invoice_line_ids.discount')
+                    self.assertEqual(
+                        actual, [expected_value],
+                        "Incorrect discount %s (expected: %s) at %s" % (
+                            actual, [expected_value], discount_date))
+                    break
+            else:
+                raise ValueError(
+                    'Expected inv date %s never reached' % discount_date)
+
+    def _discount_date(self, prefix=u"start", **kwargs):
+        kwargs.setdefault("name", u"Test discount")
+        kwargs.setdefault("amount_value", 1.)
+        discount = self.cdiscount(**kwargs)
+        return discount._compute_date(self.acct_line, prefix)
+
+    def test_discount_compute_date_ok(self):
+        "Start date must be computed correctly"
+        self.assertEqual(
+            self._discount_date(start_value=-5, start_unit="days"),
+            date(2016, 2, 10))
+        self.assertEqual(
+            self._discount_date(start_unit="weeks", start_value=3),
+            date(2016, 3, 7))
+        self.assertEqual(
+            self._discount_date(start_unit="months", start_value=2),
+            date(2016, 4, 15))
+        self.assertEqual(
+            self._discount_date(start_unit="years", start_value=3),
+            date(2019, 2, 15))
+        self.assertEqual(
+            self._discount_date(start_type="absolute", start_date="2021-07-17"),
+            date(2021, 7, 17))
+        self.assertEqual(
+            self._discount_date(prefix="start"),
+            date(2016, 2, 15))
+        self.assertIsNone(self._discount_date(prefix="end"))
+
+    def test_discount_compute_0(self):
+        self.set_cdiscounts(self.cdiscount(
+            name=u"Fix discount",
+            amount_type=u"fix",
+            amount_value=2.,
+        ))
+        invoice = self.contract.recurring_create_invoice()
+        self.assertEqual(invoice.mapped("invoice_line_ids.discount"), [2.])
+
+    def _discounts_1(self):
+        return (
+            self.cdiscount(
+                name=u"Early adopter discount",
+                amount_type="percent",
+                amount_value=5.,
+            ) | self.cdiscount(
+                name=u"2 years loyalty",
+                amount_type="percent",
+                amount_value=10.,
+                start_reference=u"date_start",
+                start_value=2,
+                start_unit=u"years",
+                end_type=u"relative",
+                end_reference=u"date_start",
+                end_value=3,
+                end_unit=u"years",
+            ) | self.cdiscount(
+                name=u"More than 3 years loyalty",
+                amount_type="percent",
+                amount_value=20.,
+                start_reference=u"date_start",
+                start_value=3,
+                start_unit=u"years",
+            )
+        )
+
+    def test_discount_compute_1(self):
+        self.set_cdiscounts(self._discounts_1())
+
+        self.check_cdiscounts([
+            ('2016-02-29', 5.),
+            ('2018-01-28', 5.),
+            ('2018-02-28', 15.),
+            ('2019-01-28', 15.),
+            ('2019-02-28', 25.),
+            ('2020-03-28', 25.),
+        ])
+
+    def test_discount_override(self):
+        " Discounts on contract template are handled but can be overidden "
+
+        # Add a line with 2 discounts on contract template
+        _vals = self.line_vals.copy()
+        _vals["analytic_account_id"] = self.template.id
+        ct_line = self.env['account.analytic.contract.line'].create(_vals)
+
+        self.tdiscount(ct_line, name=u"Fix discount",
+                       amount_value=2., amount_type=u"fix")
+        tdiscount2 = self.tdiscount(ct_line, name=u"5% discount",
+                                    amount_value=5., amount_type=u"percent")
+
+        # Use this template as the model for self.contract:
+        self.contract.recurring_invoice_line_ids.unlink()
+        self.template.update({"recurring_interval": 1,
+                              "recurring_rule_type": "monthly"})
+        self.contract.contract_template_id = self.template
+        self.contract._onchange_contract_template_id()
+
+        # Check applied discounts
+        inv = self.contract.recurring_create_invoice()
+        self.assertEqual(inv.mapped("invoice_line_ids.name"),
+                         [u"Services from 02/29/2016 to 03/28/2016\n"
+                          u"Applied discounts:\n"
+                          u"- Fix discount\n"
+                          u"- 5% discount"])
+
+        # Add an override for the 5% discount
+        self.cdiscount(
+            self.contract.recurring_invoice_line_ids,
+            name=u"10% discount", amount_type="percent", amount_value=10.,
+            replace_discount_line_id=tdiscount2.id)
+
+        # Check applied discounts
+        inv = self.contract.recurring_create_invoice()
+        self.assertEqual(inv.mapped("invoice_line_ids.name"),
+                         [u"Services from 03/29/2016 to 04/28/2016\n"
+                          u"Applied discounts:\n"
+                          u"- Fix discount\n"
+                          u"- 10% discount"])
+
+    def test_condition_and_description(self):
+        self.set_cdiscounts(self.cdiscount(
+            name=u"Fix discount after 1 month under condition",
+            condition=u"test",
+            amount_value=5.,
+            amount_type=u"percent",
+            start_reference=u"date_start",
+            start_value=1,
+            start_unit=u"months",
+        ))
+
+        with patch.object(TestConditionDiscountLine,
+                          "_compute_condition_test",
+                          create=True) as mock:
+
+            mock.return_value = True
+            inv1 = self.contract.recurring_create_invoice()
+            inv2 = self.contract.recurring_create_invoice()
+
+            mock.return_value = False
+            inv3 = self.contract.recurring_create_invoice()
+
+        self.assertEqual(mock.call_count, 3)
+        self.assertEqual([tuple(c) for c in mock.call_args_list], [
+            ((self.acct_line, fields.Date.from_string(inv1.date_invoice)), {}),
+            ((self.acct_line, fields.Date.from_string(inv2.date_invoice)), {}),
+            ((self.acct_line, fields.Date.from_string(inv3.date_invoice)), {}),
+        ])
+
+        self.assertEqual(inv1.mapped("invoice_line_ids.discount"), [0.])
+        self.assertEqual(inv2.mapped("invoice_line_ids.discount"), [5.])
+        self.assertEqual(inv3.mapped("invoice_line_ids.discount"), [0.])
+
+        self.assertEqual(inv1.mapped("invoice_line_ids.name"),
+                         [u"Services from 02/29/2016 to 03/28/2016"])
+        self.assertEqual(inv2.mapped("invoice_line_ids.name"),
+                         [u"Services from 03/29/2016 to 04/28/2016\n"
+                          u"Applied discounts:\n"
+                          u"- Fix discount after 1 month under condition"])
+        self.assertEqual(inv3.mapped("invoice_line_ids.name"),
+                         [u"Services from 04/29/2016 to 05/28/2016"])
+
+    def test_simulate_payments(self):
+        self.set_cdiscounts(self._discounts_1())
+
+        self.contract.recurring_next_date = self.contract.date_start
+
+        report = self.env.ref(
+            "contract_variable_discount.report_simulate_payments_html")
+        fragment = report.render({"docs": self.contract}, 'ir.qweb')
+
+        html = ('<html><head><meta charset="utf-8"/></head>'
+                '<body>%s<//body></html>' % fragment)
+        doc = lxml.html.fromstring(html)
+
+        # First column contains dates:
+        self.assertEqual(
+            doc.xpath("//tbody/tr/td[1][not(@colspan)]/text()"),
+            ['2016-02-15', '2018-02-15', '2019-02-15'])
+
+        # Fourth contains discounts:
+        self.assertEqual(
+            [n.text_content() for n in doc.xpath("//tbody/tr/td[4]")],
+            [u"5.0 %", u"15.0 %", u"25.0 %"])
