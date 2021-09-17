ae445a72c20c54596ad3663a39ea8e23747331a6
Florent Cayré <florent@commown.coop>
2021-09-17 14:11:58 +0200

    Import the project_issue_last_partner_msg_date module into the 12.0 branch

---
+++ project_issue_last_partner_msg_date/models/project_task.py
@@ -0,0 +1,29 @@
+from odoo import models, fields, api
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+    _order = 'priority desc, last_partner_msg_date asc'
+
+    last_partner_msg_date = fields.Datetime('Last partner message date')
+
+    def create(self, values):
+        "Set last_partner_msg_date to utcnow by default"
+        task = super().create(values)
+        if task.last_partner_msg_date is False:
+            task.last_partner_msg_date = task.create_date
+        return task
+
+    def message_post(self, *args, **kwargs):
+        """ Update the last_partner_msg_date field when a message is posted.
+
+        If the partner has made at least one email or comment on the task,
+        the last one's date is used.
+        """
+        msg = super().message_post(*args, **kwargs)
+        date = self.last_partner_msg_date or self.create_date
+        if (msg.create_date > date and
+                msg.author_id == self.partner_id and
+                msg.message_type in ('email', 'comment')):
+            self.last_partner_msg_date = msg.create_date
+        return msg
