ca3cae8eaca6898fea0e2ca00bd041a52433ba08
Florent Cayré <florent@commown.coop>
2021-09-17 14:11:53 +0200

    Import the commown_devices module into the 10.0 branch

---
+++ commown_devices/models/wizard_project_issue_picking.py
@@ -0,0 +1,87 @@
+from odoo import models, fields, api
+
+
+class ProjectIssueAbstractPickingWizard(models.AbstractModel):
+    _name = "project.issue.abstract.picking.wizard"
+
+    issue_id = fields.Many2one(
+        "project.issue",
+        string=u"Issue",
+        required=True,
+    )
+
+    date = fields.Datetime(
+        string=u"date",
+        help=u"Defaults to now - To be set only to force a date",
+    )
+
+
+class ProjectIssueOutwardPickingWizard(models.TransientModel):
+    _name = "project.issue.outward.picking.wizard"
+    _inherit = "project.issue.abstract.picking.wizard"
+
+    product_id = fields.Many2one(
+        "product.product",
+        string=u"Product",
+        domain="[('tracking', '=', 'serial')]",
+        required=True,
+        default=lambda self: self._compute_default_product_id(),
+    )
+
+    lot_id = fields.Many2one(
+        "stock.production.lot",
+        string=u"Device",
+        domain=lambda self: '''[
+            ("product_id", "=", product_id),
+            ("quant_ids.location_id", "child_of", %d)]''' % self.env.ref(
+                "commown_devices.stock_location_available_for_rent").id,
+        required=True,
+    )
+
+    def _compute_default_product_id(self):
+        if not self.issue_id and "default_issue_id" in self.env.context:
+            issue = self.env["project.issue"].browse(
+                self.env.context["default_issue_id"])
+        else:
+            issue = self.issue
+        return issue.lot_id.product_id
+
+    @api.multi
+    def create_picking(self):
+        return self.issue_id.contract_id.send_device(
+            self.lot_id.quant_ids[0], date=self.date)
+
+
+class ProjectIssueInwardPickingWizard(models.TransientModel):
+    _name = "project.issue.inward.picking.wizard"
+    _inherit = "project.issue.abstract.picking.wizard"
+
+    quant_id = fields.Many2one(
+        "stock.quant",
+        string=u"Device",
+        required=True,
+    )
+
+    location_dest_id = fields.Many2one(
+        "stock.location",
+        string=u"Destination",
+        domain=lambda self: [(
+            'location_id', '=', self.env.ref(
+                'commown_devices.stock_location_devices_to_check').id)],
+        required=True,
+    )
+
+    @api.onchange("issue_id")
+    def onchange_issue_id(self):
+        if self.issue_id:
+            quants = self.issue_id.contract_id.quant_ids
+            return {
+                "domain": {
+                    "quant_id": [("id", "in", quants.ids)]
+                }
+            }
+
+    @api.multi
+    def create_picking(self):
+        return self.issue_id.contract_id.receive_device(
+            self.quant_id.lot_id, self.location_dest_id, date=self.date)
---
+++ commown_devices/models/wizard_project_task_picking.py
@@ -0,0 +1,75 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+from .common import internal_picking
+
+
+class ProjectTaskPickingWizard(models.Model):
+    _name = "project.task.picking.wizard"
+
+    destination_refs = (
+        "stock_location_fp3_outsourced_repair",
+        "stock_location_fp3_repackaged",
+        "stock_location_devices_to_check",
+        "stock_location_fp3_to_check",
+        "stock_location_repairer",
+    )
+
+    task_id = fields.Many2one(
+        "project.task",
+        string=u"Task",
+        required=True,
+    )
+
+    location_dest_id = fields.Many2one(
+        "stock.location",
+        string=u"Destination",
+        required=True,
+    )
+
+    date = fields.Datetime(
+        string=u"date",
+        help=u"Defaults to now - To be set only to force a date",
+    )
+
+    def present_location(self):
+        return self.env["stock.quant"].search([
+            ("lot_id", "=", self.task_id.lot_id.id)]).location_id
+
+    def _possible_dest_locations(self):
+        """ Possible destinations: all listed the `destination_ref` attribute
+        and their children, excluding views.
+        """
+        orig_location = self.present_location()
+        result = self.env["stock.location"]
+
+        for ref in self.destination_refs:
+            loc = self.env.ref("commown_devices.%s" % ref)
+            if loc != orig_location:
+                if loc.usage == "view":
+                    result |= result.search([("id", "child_of", loc.id),
+                                             ("usage", "!=", "view")])
+                else:
+                    result |= loc
+        return result
+
+    @api.onchange("task_id")
+    def onchange_task_id(self):
+        if self.task_id:
+            dest_locations = self._possible_dest_locations()
+            return {
+                "domain": {
+                    "location_dest_id": [("id", "in", dest_locations.ids)]
+                }
+            }
+
+    @api.multi
+    def create_picking(self):
+        lot = self.task_id.lot_id
+
+        if not lot:
+            raise UserError(_("Can't move device: no device set on this task!"))
+
+        return internal_picking(
+            u"Task-%s" % self.task_id.id, [lot], self.present_location(),
+            self.location_dest_id, date=self.date, do_transfer=True)
