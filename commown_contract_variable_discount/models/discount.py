ad99960c4f0d8ebe07cd9d909282a7ab59a52bdd
Florent Cayré <florent@commown.coop>
2021-09-18 10:06:32 +0200

    [commown_contract_variable_discount] Add a coupon-driven discount condition
    The discount condition is True when the contract is related to a sale
    order associated to a coupon from the campaign specified in the
    discount.
    
    Note that this mecanism has only been implemented at contract template
    level: it is currently not possible to create such a discount specific
    to a contract.

--- commown_contract_variable_discount/models/discount.py
+++ commown_contract_variable_discount/models/discount.py
@@ -2,14 +2,27 @@
 # Copyright (C) 2021 - Commown (https://commown.coop)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from odoo import models, fields
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ContractTemplateAbstractDiscountLine(models.AbstractModel):
     _inherit = "contract.template.abstract.discount.line"
 
     condition = fields.Selection(
-        selection_add=[("no_issue_to_date", "No contractual issue to date")],
+        selection_add=[
+            ("no_issue_to_date", "No contractual issue to date"),
+            ("coupon_from_campaign", "Coupon from supplied campaign"),
+        ],
+    )
+
+    coupon_campaign_id = fields.Many2one(
+        comodel_name="coupon.campaign",
+        string="Campaign",
     )
 
     start_reference = fields.Selection(
@@ -26,3 +39,12 @@ class ContractTemplateAbstractDiscountLine(models.AbstractModel):
             if (not t.penalty_exemption
                 and fields.Date.from_string(t.contractual_issue_date) < date)
         ]
+
+    def _compute_condition_coupon_from_campaign(self, line, date):
+        try:
+            coupons = line.sale_order_line_id.order_id.used_coupons()
+            return self.coupon_campaign_id in coupons.mapped("campaign_id")
+        except Exception as exc:
+            import traceback as tb
+            _logger.exception(tb.format_exc(exc))
+            return False
