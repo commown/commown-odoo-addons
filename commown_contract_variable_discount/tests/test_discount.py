888c4168743e01be3402e6aacc0ac44f4be0983d
Florent Cayré <florent@commown.coop>
2021-09-18 10:06:32 +0200

    [commown_contract_variable_discount] Add a coupon-driven discount condition
    The discount condition is True when the contract is related to a sale
    order associated to a coupon from the campaign specified in the
    discount.
    
    Note that this mecanism has only been implemented at contract template
    level: it is currently not possible to create such a discount specific
    to a contract.

--- commown_contract_variable_discount/tests/test_discount.py
+++ commown_contract_variable_discount/tests/test_discount.py
@@ -1,5 +1,7 @@
 from odoo.addons.contract.tests.test_contract import TestContractBase
 
+from odoo.addons.product_rental.tests.common import RentalSaleOrderTC
+
 
 class DiscountLineTC(TestContractBase):
 
@@ -68,3 +70,59 @@ class DiscountLineTC(TestContractBase):
             self.assertEqual(inv.date_invoice, date)
             self.assertEqual(inv.mapped("invoice_line_ids.discount"),
                              [discount], "incorrect discount at %s" % date)
+
+
+class CouponConditionTC(RentalSaleOrderTC):
+
+    def test(self):
+        partner = self.env.ref('portal.demo_user0_res_partner')
+
+        campaign = self.env['coupon.campaign'].create({
+            "name": u"test campaign",
+            "seller_id": 1,
+        })
+
+        contract_tmpl = self._create_rental_contract_tmpl(
+            1, recurring_invoice_line_ids=[
+                self._invoice_line(1, 'PC rental', specific_price=30.)])
+
+        iline = contract_tmpl.recurring_invoice_line_ids[0]
+
+        self.env['contract.template.discount.line'].create({
+            "name": u"Test coupon discount: 80% first 3 months!",
+            "amount_value": 80., "amount_type": u"percent",
+            "start_reference": "date_start",
+            "end_reference": "date_start",
+            "start_type": "relative", "start_value": 0, "start_unit": "months",
+            "end_type": "relative", "end_value": 3, "end_unit": "months",
+            "contract_template_line_id": iline.id,
+            "condition": "coupon_from_campaign",
+            "coupon_campaign_id": campaign.id,
+        })
+
+        product = self._create_rental_product(
+            name='Fairphone Premium', list_price=60., rental_price=30.,
+            contract_template_id=contract_tmpl.id)
+
+        so = self.env['sale.order'].create({
+            'partner_id': partner.id,
+            'order_line': [self._oline(product)],
+        })
+
+        coupon = self.env['coupon.coupon'].create({
+            "code": u"TEST-CODE",
+            "campaign_id": campaign.id,
+            "used_for_sale_id": so.id,
+        })
+
+        so.action_confirm()
+
+        contract = self.env['account.analytic.account'].search([
+            ('name', 'ilike', '%' + so.name + '%'),
+        ]).ensure_one()
+
+        # Default tax is 15%
+        self.assertEqual(contract.recurring_create_invoice().amount_total, 6.9)
+        self.assertEqual(contract.recurring_create_invoice().amount_total, 6.9)
+        self.assertEqual(contract.recurring_create_invoice().amount_total, 6.9)
+        self.assertEqual(contract.recurring_create_invoice().amount_total, 34.5)
