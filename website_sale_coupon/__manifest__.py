51c2ef0da55661c1e1cb2d898383714e3cd8eacb
Florent Cayré <florent@commown.coop>
2021-10-03 11:23:35 +0200

    Bump version to 2.0.0, add doc and fix web address

--- website_sale_coupon/__manifest__.py
+++ website_sale_coupon/__manifest__.py
@@ -1,14 +1,27 @@
 {
     'name': 'Website sale coupon',
     'category': 'Business',
-    'summary': ('Emit coupons that customers can buy elsewhere'
-                ' and use to reduce the price of a product on your shop'),
-    'version': '10.0.1.0.4',
-    'description': ('Emit coupons that customers can buy elsewhere'
-                    ' and use to reduce the price of a product on your shop'),
-    'author': "Commown SCIC SAS",
+    'summary': (
+        'Manage promotion campaigns with or without coupons.'
+        ' Coupons are secret codes that customers can get (paid or for free)'
+        ' to get an advantage when they buy a product on the online shop.'
+    ),
+    'version': '10.0.2.0.0',
+    'description': (
+        'This module is intended to manage promotion campaigns.\n'
+        'These promotion campaigns have optional start and end validity dates.'
+        ' According to the campaign setup, customers must either enter a unique'
+        ' secret code or directly the campaign name to benefit from the'
+        ' advantages granted by the campaign when they buy a product on the'
+        ' online shop.\n'
+        'Important note: the present module does NOT implement any advantage,'
+        ' which must be implemented in dedicated modules. This is on purpose,'
+        ' as these advantages can be various, not necessarily an immediate'
+        ' price reduction.'
+    ),
+    'author': "Commown SCIC",
     'license': "AGPL-3",
-    'website': "https://commown.fr",
+    'website': "https://commown.coop",
     'depends': [
         'website_sale',
     ],
