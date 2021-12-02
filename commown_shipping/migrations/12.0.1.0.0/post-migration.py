f539a7469d7b43c8c721808c60d0238a1d606398
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-12-02 22:22:35 +0100

    [ADD] commown_shipping : migration scripts

---
+++ commown_shipping/migrations/12.0.1.0.0/post-migration.py
@@ -0,0 +1,9 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
+    # TODO
