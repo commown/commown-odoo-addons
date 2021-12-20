675e4baa5b2106e80d0102f0ad962443e2869bcb
Florent Cayré <florent@commown.coop>
2021-12-20 13:04:59 +0100

    Adapt crm.lead expedtion wizard to the new model

--- commown_devices/models/wizard_crm_lead_picking.py
+++ commown_devices/models/wizard_crm_lead_picking.py
@@ -11,29 +11,37 @@ class CrmLeadPickingWizard(models.TransientModel):
     )
 
     date = fields.Datetime(
-        string=u"date",
+        string=u"Date",
         help=u"Defaults to now - To be set only to force a date",
     )
 
-    product_id = fields.Many2one(
-        "product.product",
+    product_tmpl_id = fields.Many2one(
+        "product.template",
         string=u"Product",
         domain="[('tracking', '=', 'serial')]",
         required=True,
-        default=lambda self: self._compute_default_product_id(),
+        default=lambda self: self._compute_default_product_tmpl_id(),
+    )
+
+    variant_id = fields.Many2one(
+        "product.product",
+        string=u"Variant",
+        domain=("[('tracking', '=', 'serial'),"
+                " ('product_tmpl_id', '=', product_tmpl_id)]"),
+        required=True,
     )
 
     lot_id = fields.Many2one(
         "stock.production.lot",
         string=u"Device",
         domain=lambda self: '''[
-            ("product_id", "=", product_id),
+            ("product_id", "=", variant_id),
             ("quant_ids.location_id", "child_of", %d)]''' % self.env.ref(
                 "commown_devices.stock_location_available_for_rent").id,
         required=True,
     )
 
-    def _compute_default_product_id(self):
+    def _compute_default_product_tmpl_id(self):
         if not self.lead_id and "default_lead_id" in self.env.context:
             lead = self.env["crm.lead"].browse(
                 self.env.context["default_lead_id"])
@@ -43,6 +51,14 @@ class CrmLeadPickingWizard(models.TransientModel):
             "recurring_invoice_line_ids.sale_order_line_id"
             ".product_id.product_tmpl_id.storable_product_id")
         return possible_products and possible_products[0]
+
+    @api.onchange("product_tmpl_id")
+    def onchange_product_tmpl_id(self):
+        if self.product_tmpl_id:
+            if len(self.product_tmpl_id.product_variant_ids) == 1:
+                self.variant_id = self.product_tmpl_id.product_variant_id
+            else:
+                self.variant_id = False
 
     @api.multi
     def create_picking(self):
