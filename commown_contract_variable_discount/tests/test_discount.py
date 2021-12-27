31843b5f8ddb22fc896fb8c9f52ffe89ec2a3664
Florent Cayré <florent@commown.coop>
2021-12-27 07:28:49 +0100

    Use new attribute names to make unittests pass

--- commown_contract_variable_discount/tests/test_discount.py
+++ commown_contract_variable_discount/tests/test_discount.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from odoo.addons.contract.tests.test_contract import TestContractBase
 
 from .common import ContractSaleWithCouponTC
@@ -19,8 +21,8 @@ class DiscountLineTC(TestContractBase):
         })
 
         # Test without penalty
-        self.assertFalse(self.contract.contractual_issue_ids)
-        self.contract.contractual_issue_ids |= self.env['project.task'].create({
+        self.assertFalse(self.contract.issue_ids)
+        self.contract.issue_ids |= self.env['project.task'].create({
             "name": "task1",
             "penalty_exemption": True,
             "contractual_issue_date": self.contract.date_start,
@@ -30,7 +32,7 @@ class DiscountLineTC(TestContractBase):
         self.assertEqual(inv1.mapped("invoice_line_ids.discount"), [5.])
 
         # Add a penalty
-        self.contract.contractual_issue_ids |= self.env['project.task'].create({
+        self.contract.issue_ids |= self.env['project.task'].create({
             "name": "task2",
             "penalty_exemption": False,
             "contractual_issue_date": self.contract.date_start,
@@ -39,38 +41,42 @@ class DiscountLineTC(TestContractBase):
         inv2 = self.contract.recurring_create_invoice()
         self.assertEqual(inv2.mapped("invoice_line_ids.discount"), [0.])
 
-    def test_min_end_contract_date(self):
-        self.contract.update({
-            'min_contract_duration': 2,
-            'recurring_rule_type': 'monthly',
-            'recurring_interval': 1,
+    def test_commitment_end_date(self):
+        self.contract.contract_line_ids.update({
+            "commitment_duration": 2,
+            "recurring_rule_type": "monthly",
+            "recurring_interval": 1,
         })
 
         self.env['contract.discount.line'].create({
             "name": "Post commitment discount",
             "amount_type": "percent",
             "amount_value": 5.,
-            "start_reference": "min_contract_end_date",
+            "start_reference": "commitment_end_date",
             "start_value": 1,
             "start_unit": "months",
             "end_type": "relative",
-            "end_reference": "min_contract_end_date",
+            "end_reference": "commitment_end_date",
             "end_value": 4,
             "end_unit": "months",
             "contract_line_id": self.acct_line.id,
         })
 
-        self.assertEqual(self.contract.min_contract_end_date, "2016-04-15")
+        self.assertEqual(
+            self.contract.mapped("contract_line_ids.commitment_end_date"),
+            [date(2018, 3, 1)])
 
-        expected = [("2016-02-29", 0.), ("2016-03-29", 0.), ("2016-04-29", 0.),
-                    ("2016-05-29", 5.), ("2016-06-29", 5.), ("2016-07-29", 5.),
-                    ("2016-08-29", 0.), ("2016-09-29", 0.)]
+        expected = [(date(2018, 1, 15), 0.), (date(2018, 2, 15), 0.),
+                    (date(2018, 3, 15), 0.), (date(2018, 4, 15), 5.),
+                    (date(2018, 5, 15), 5.), (date(2018, 6, 15), 5.),
+                    (date(2018, 7, 15), 0.), (date(2018, 8, 15), 0.),
+                    ]
 
-        for date, discount in expected:
+        for _date, discount in expected:
             inv = self.contract.recurring_create_invoice()
-            self.assertEqual(inv.date_invoice, date)
+            self.assertEqual(inv.date_invoice, _date)
             self.assertEqual(inv.mapped("invoice_line_ids.discount"),
-                             [discount], "incorrect discount at %s" % date)
+                             [discount], "incorrect discount at %s" % _date)
 
 
 class CouponConditionTC(ContractSaleWithCouponTC):
