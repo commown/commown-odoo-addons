6cd679a13409997b932a14e8eca968f3368b41a4
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-01-19 21:47:33 +0100

    [FIX] commown_cooperative_campaign and contract_variable_discount bad major revision for the version key of manifest

--- contract_variable_discount/__manifest__.py
+++ contract_variable_discount/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Contract variable discount',
     'category': 'Contract Management',
-    'version': '10.0.1.2.0',
+    'version': '12.0.1.2.0',
     'author': "Commown SCIC",
     'license': "AGPL-3",
     'website': "https://commown.coop",
