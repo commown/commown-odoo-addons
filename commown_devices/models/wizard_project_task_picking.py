6562c358f5dd6647ff106cb0d80eef20f18e1c8c
Florent Cayré <florent@commown.coop>
2022-01-26 12:16:14 +0100

    Remove device-specific stock locations and use views instead
    This only impacts the project task wizard, but makes is important
    to ease new location creation in the backend.

--- commown_devices/models/wizard_project_task_picking.py
+++ commown_devices/models/wizard_project_task_picking.py
@@ -9,8 +9,7 @@ class ProjectTaskPickingWizard(models.Model):
 
     destination_refs = (
         "stock_location_outsourced_repair",
-        "stock_location_fp3_repackaged",
-        "stock_location_headsets_repackaged",
+        "stock_location_repackaged_devices",
         "stock_location_devices_to_check",
         "stock_location_repairer",
     )
