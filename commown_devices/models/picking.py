3b3621b75e7277dacdec3f531b7eec8ade6c79b4
Florent Cayré <florent@commown.coop>
2022-01-31 13:01:59 +0100

    Adapt to 12.0 contract model changes

--- commown_devices/models/picking.py
+++ commown_devices/models/picking.py
@@ -4,6 +4,4 @@ from odoo import models, fields
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    contract_id = fields.Many2one(
-        'account.analytic.account',
-        string='Contract')
+    contract_id = fields.Many2one('contract.contract', string='Contract')
