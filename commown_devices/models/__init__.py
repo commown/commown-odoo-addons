57be1e1ee7be68015295cfd2f7bc5ecb52b71c26
Florent Cayré <florent@commown.coop>
2022-02-09 18:55:36 +0100

    Remove project issue related files (models, data, views, security, tests)
    The functionalities they contain will end-up in tasks.

--- commown_devices/models/__init__.py
+++ commown_devices/models/__init__.py
@@ -2,12 +2,10 @@ from . import contract
 from . import crm_lead
 from . import picking
 from . import product
-from . import project_issue
 from . import project_project
 from . import project_task
 from . import res_partner
 from . import stock_production_lot
 from . import stock_quant
 from . import wizard_crm_lead_picking
-from . import wizard_project_issue_picking
 from . import wizard_project_task_picking
