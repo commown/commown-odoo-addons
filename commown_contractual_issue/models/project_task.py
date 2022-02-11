550f0ccbb9cc4439cd635de818d64de5b6ccfcf2
Florent Cayré <florent@commown.coop>
2022-02-11 16:17:15 +0100

    Add UI-related project issue 10.0 methods to project task

--- commown_contractual_issue/models/project_task.py
+++ commown_contractual_issue/models/project_task.py
@@ -5,7 +5,11 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     contract_id = fields.Many2one(
-        'contract.contract', string='Contract')
+        'contract.contract',
+        string='Contract',
+        default=lambda self: self._default_contract(),
+        domain=lambda self: self._domain_contract(),
+    )
     commercial_partner_id = fields.Many2one(
         'res.partner', related='partner_id.commercial_partner_id')
     contractual_issue_type = fields.Selection(
@@ -24,3 +28,37 @@ class ProjectTask(models.Model):
     require_contract = fields.Boolean(
         'Requires a contract',
         related='project_id.require_contract')
+
+    def _default_contract(self):
+        domain = self._domain_contract()
+        if self.env["contract.contract"].search_count(domain) == 1:
+            return self.env["contract.contract"].search(domain)
+
+    def _domain_contract(self):
+        domain = []
+        if self.partner_id:
+            domain.append(("partner_id.commercial_partner_id", "=",
+                           self.partner_id.commercial_partner_id.id))
+        return domain
+
+    @api.onchange("partner_id")
+    def onchange_partner_id_set_contract(self):
+        # Protect against onchange loop
+        if (self.contract_id.partner_id.commercial_partner_id !=
+                self.partner_id.commercial_partner_id):
+            self.contract_id = self._default_contract() or False
+        return {"domain": {"contract_id": self._domain_contract()}}
+
+    @api.onchange("contract_id")
+    def onchange_contract_id_set_partner(self):
+        """ If partner was empty, use contract info to help setting it
+        Also restrict to non-company partners.
+        """
+        if not self.partner_id and self.contract_id:
+            domain = [
+                ("is_company", "=", False),
+                ("commercial_partner_id", "=",
+                 self.contract_id.partner_id.commercial_partner_id.id),
+            ]
+            if self.env["res.partner"].search_count(domain) == 1:
+                self.partner_id = self.env["res.partner"].search(domain).id
