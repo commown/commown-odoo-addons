7e992f840f716752ee8c615f2729f55b8139dff5
Florent Cayré <florent@commown.coop>
2022-03-08 00:06:15 +0100

    Fix commown module's tests

--- commown/tests/test_contract.py
+++ commown/tests/test_contract.py
@@ -17,21 +17,21 @@ class ContractPaymentTC(TestContractBase):
         payment_token = cls.env['payment.token'].create({
             'name': 'Test Slimpay Token',
             'partner_id': cls.contract.partner_id.id,
-            'active': True,
             'acquirer_id': slimpay.id,
             'acquirer_ref': 'Slimpay mandate ref',
         })
-        cls.contract.payment_token_id = payment_token
+        cls.contract.is_auto_pay = True
+        cls.contract.partner_id.payment_token_id = payment_token
 
     def setUp(self):
         super(ContractPaymentTC, self).setUp()
-        client_patcher = patch('odoo.addons.payment_slimpay.models.'
+        client_patcher = patch('odoo.addons.account_payment_slimpay.models.'
                                'slimpay_utils.get_client')
         client_patcher.start()
         self.addCleanup(client_patcher.stop)
 
     def test_default_payin_label(self):
-        with patch('odoo.addons.payment_slimpay.models.'
+        with patch('odoo.addons.account_payment_slimpay.models.'
                    'slimpay_utils.SlimpayClient.create_payment') as pay:
             invoice = self.contract.recurring_create_invoice()
             label = pay.call_args[0][-1]
@@ -40,14 +40,11 @@ class ContractPaymentTC(TestContractBase):
     def test_custom_payin_label(self):
         self.contract.write({
             'transaction_label': 'Invoice #START# - #END# (#INV#)',
-            'recurring_invoicing_type': 'pre-paid',
-            'recurring_next_date': '2018-02-15',
-            'recurring_rule_type': 'monthly',
         })
-        with patch('odoo.addons.payment_slimpay.models.'
+        with patch('odoo.addons.account_payment_slimpay.models.'
                    'slimpay_utils.SlimpayClient.create_payment') as pay:
             invoice = self.contract.recurring_create_invoice()
             label = pay.call_args[0][-1]
-            expected_label = ('Invoice 02/15/2018 - 03/14/2018 (%s)'
+            expected_label = ('Invoice 01/15/2018 - 02/14/2018 (%s)'
                               % invoice.number)
             self.assertEqual(label, expected_label)
--- commown/tests/test_crm_lead.py
+++ commown/tests/test_crm_lead.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from odoo.tests.common import TransactionCase, at_install, post_install
 
 
@@ -9,22 +11,21 @@ class CrmLeadTC(TransactionCase):
         super(CrmLeadTC, self).setUp()
         self.team = self.env.ref('sales_team.salesteam_website_sales')
         self.team.default_perform_actions_on_delivery = True
-        self.lead = self._add_lead(name='[SO99999-01] TEST',
+        self.lead = self._add_lead(name='TEST LEAD',
                                    send_email_on_delivery=False)
 
     def _add_lead(self, **kwargs):
         kwargs.setdefault('name', 'TEST')
         kwargs.setdefault('team_id', self.team.id)
         kwargs.setdefault('partner_id',
-                          self.env.ref('portal.demo_user0_res_partner').id)
+                          self.env.ref('base.partner_demo_portal').id)
         kwargs.setdefault('type', 'opportunity')
         return self.env['crm.lead'].create(kwargs)
 
-    def _link_lead_to_contract(self):
+    def _create_contract(self):
         "Create a contract link to the lead by its (sale order like) name"
-        return self.env['account.analytic.account'].create({
-            'name': 'SO99999-01',
-            'recurring_invoices': True,
+        return self.env['contract.contract'].create({
+            'name': 'TEST CONTRACT',
             'is_auto_pay': False,
             'date_start': '2030-01-01',
             'recurring_next_date': '2030-01-01',
@@ -49,22 +50,24 @@ class CrmLeadTC(TransactionCase):
     def test_actions_on_delivery_start_contract(self):
 
         self.assertTrue(self.lead.start_contract_on_delivery)
-        contract = self._link_lead_to_contract()
+        contract = self._create_contract()
+        self.lead.contract_id = contract.id
 
         # Simulate delivery
-        self.lead.delivery_date = '2018-01-01'
+        self.lead.delivery_date = date(2018, 1, 1)
 
         # Check results: contract started
         self.assertTrue(contract.is_auto_pay)
-        self.assertTrue(contract.date_start.startswith('2018-01-01'))
+        self.assertEqual(contract.date_start, date(2018, 1, 1))
 
     def test_actions_on_delivery_no_start_contract(self):
 
         self.lead.start_contract_on_delivery = False
-        contract = self._link_lead_to_contract()
+        contract = self._create_contract()
+        self.lead.contract_id = contract.id
 
         # Simulate delivery
-        self.lead.delivery_date = '2018-01-01'
+        self.lead.delivery_date = date(2018, 1, 1)
 
         # Check results: contract not started
         self.assertFalse(contract.is_auto_pay)
