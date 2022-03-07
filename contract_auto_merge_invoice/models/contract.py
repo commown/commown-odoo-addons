cc27fc76d896d2e5701c28e190987cc5acc980d8
Florent Cayré <florent@commown.coop>
2022-03-08 00:04:09 +0100

    Fix contract prepare invoice may be called with keyword args too

--- contract_auto_merge_invoice/models/contract.py
+++ contract_auto_merge_invoice/models/contract.py
@@ -5,8 +5,8 @@ class Contract(models.Model):
     _inherit = 'contract.contract'
 
     @api.multi
-    def _prepare_invoice(self, *args):
+    def _prepare_invoice(self, *args, **kwargs):
         " Override contract invoice creation to add auto_merge=True "
-        vals = super(Contract, self)._prepare_invoice(*args)
+        vals = super(Contract, self)._prepare_invoice(*args, **kwargs)
         vals['auto_merge'] = True
         return vals
