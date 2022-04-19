0e14aa80ad71c188921b047f52888308492d7470
Florent Cayré <florent@commown.coop>
2022-04-19 15:53:24 +0200

    Improve cooperative campaign double optin error message

--- commown_cooperative_campaign/models/ws_utils.py
+++ commown_cooperative_campaign/models/ws_utils.py
@@ -10,6 +10,7 @@ import phonenumbers
 import requests
 
 from odoo import _
+from odoo.exceptions import UserError
 
 
 _logger = logging.getLogger(__name__)
@@ -142,7 +143,8 @@ def coop_human_readable_subscriptions(subscriptions, dt_format):
     return u"\n".join(result)
 
 
-def coop_ws_optin(base_url, campaign_ref, customer_key, date, tz, hour=9):
+def coop_ws_optin(base_url, campaign_ref, customer_key, date, tz, hour=9,
+                  silent_double_optin=True):
     "Query the cooperative web services to insert a new subscription"
 
     _logger.info(u"Optin %s: %s on %s...", campaign_ref, customer_key, date)
@@ -153,10 +155,18 @@ def coop_ws_optin(base_url, campaign_ref, customer_key, date, tz, hour=9):
     url = base_url + "/campaigns/%s/opt-in" % urllib.quote_plus(campaign_ref)
     resp = requests.post(
         url, json={"customer_key": customer_key, "optin_ts": optin_ts})
-    resp.raise_for_status()
+    if resp.status_code == 422:
+        json = resp.json()
+        if json.get("detail", None) == 'Already opt-in':
+            _logger.info(u"Double opt-in for %s", customer_key)
+            if not silent_double_optin:
+                raise UserError(_("Already opt-in (may not be visible"
+                                  " if before the campaign start)"))
+        else:
+            _logger.error(u"Opt-in error json: %s" % json)
 
-    resp_data = resp.json()
-    _logger.debug(u"Got web services response:\n %s", pformat(resp_data))
+    _logger.debug(u"Got web services response:\n %s", resp.text)
+    resp.raise_for_status()
 
 
 def coop_ws_optout(base_url, campaign_ref, customer_key, date, tz, hour=9):
