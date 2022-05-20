af4c702c032d152ec3110b81567256ac4509563d
Florent Cayré <florent@commown.coop>
2022-05-20 15:59:18 +0200

    Use NO_DATE instead of hard-coded 2030-01-01

--- commown/tests/test_crm_lead.py
+++ commown/tests/test_crm_lead.py
@@ -1,60 +1,61 @@
 from datetime import date
 
-from odoo.tests.common import TransactionCase, at_install, post_install
+from odoo.addons.product_rental.models.sale_order_line import NO_DATE
+from odoo.addons.product_rental.tests.common import RentalSaleOrderTC
+from odoo.tests.common import at_install, post_install
 
 
 @at_install(False)
 @post_install(True)
-class CrmLeadTC(TransactionCase):
+class CrmLeadTC(RentalSaleOrderTC):
 
     def setUp(self):
         super(CrmLeadTC, self).setUp()
-        self.team = self.env.ref('sales_team.salesteam_website_sales')
-        self.team.default_perform_actions_on_delivery = True
-        self.lead = self._add_lead(name='TEST LEAD',
-                                   send_email_on_delivery=False)
-
-    def _add_lead(self, **kwargs):
-        kwargs.setdefault('name', 'TEST')
-        kwargs.setdefault('team_id', self.team.id)
-        kwargs.setdefault('partner_id',
-                          self.env.ref('base.partner_demo_portal').id)
-        kwargs.setdefault('type', 'opportunity')
-        return self.env['crm.lead'].create(kwargs)
-
-    def _create_contract(self):
-        "Create a contract link to the lead by its (sale order like) name"
-        return self.env['contract.contract'].create({
-            'name': 'TEST CONTRACT',
-            'is_auto_pay': False,
-            'date_start': '2030-01-01',
-            'recurring_next_date': '2030-01-01',
-            'partner_id': self.lead.partner_id.id,
+        self.so = self.create_sale_order()
+        self.so.team_id.default_perform_actions_on_delivery = True
+        self.so.mapped("order_line.product_id").update({
+            'followup_sales_team_id': self.so.team_id.id,
         })
 
-    def test_default_no_action_on_delivery(self):
-        team = self.lead.team_id
-
-        team.default_perform_actions_on_delivery = False
-        lead = self._add_lead(team_id=team.id)
-
-        self.assertFalse(lead.send_email_on_delivery)
-        self.assertFalse(lead.start_contract_on_delivery)
+    def test_default_action_on_delivery(self):
+        team = self.so.team_id
 
         team.default_perform_actions_on_delivery = True
-        lead = self._add_lead(team_id=team.id)
+        lead = self._create_ra_leads()[0]
 
         self.assertTrue(lead.send_email_on_delivery)
         self.assertTrue(lead.start_contract_on_delivery)
 
-    def test_actions_on_delivery_start_contract(self):
+    def test_default_no_action_on_delivery(self):
+        team = self.so.team_id
 
-        self.assertTrue(self.lead.start_contract_on_delivery)
-        contract = self._create_contract()
-        self.lead.contract_id = contract.id
+        team.default_perform_actions_on_delivery = False
+        lead = self._create_ra_leads()[0]
+
+        self.assertFalse(lead.send_email_on_delivery)
+        self.assertFalse(lead.start_contract_on_delivery)
+
+    def _create_ra_leads(self):
+        """ Confirm the sale and return the its just-created risk-analysis leads
+        related to a contract (some products do not have a contract).
+        """
+        self.so.action_confirm()
+        return self.env["crm.lead"].search([
+            ("so_line_id.order_id", "=", self.so.id),
+            ("contract_id", "!=", False),
+        ])
+
+    def test_actions_on_delivery_start_contract(self):
+        lead = self._create_ra_leads()[0]
+        contract = lead.contract_id
+        lead.send_email_on_delivery = False  # avoid setting-up email template
+
+        # Check contract state before delivery
+        self.assertFalse(contract.is_auto_pay)
+        self.assertEqual(contract.date_start, NO_DATE)
 
         # Simulate delivery
-        self.lead.delivery_date = date(2018, 1, 1)
+        lead.delivery_date = date(2018, 1, 1)
 
         # Check results: contract started
         self.assertTrue(contract.is_auto_pay)
@@ -62,12 +63,15 @@ class CrmLeadTC(TransactionCase):
 
     def test_actions_on_delivery_no_start_contract(self):
 
-        self.lead.start_contract_on_delivery = False
-        contract = self._create_contract()
-        self.lead.contract_id = contract.id
+        lead = self._create_ra_leads()[0]
+        contract = lead.contract_id
+        lead.send_email_on_delivery = False  # avoid setting-up email template
+
+        lead.start_contract_on_delivery = False
 
         # Simulate delivery
-        self.lead.delivery_date = date(2018, 1, 1)
+        lead.delivery_date = date(2018, 1, 1)
 
         # Check results: contract not started
         self.assertFalse(contract.is_auto_pay)
+        self.assertEqual(contract.date_start, NO_DATE)
