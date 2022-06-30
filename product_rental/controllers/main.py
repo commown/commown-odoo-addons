5b5ddbca833b44fbd53d22215d13913947218892
Florent Cayré <florent@commown.coop>
2022-06-30 11:03:18 +0200

    Fix crash on shop display of rental but non-contract products
    This is typically the case of accessories (keyboard, ...), which need
    to be rented with a contract product (computer), but cannot otherwise.

--- product_rental/controllers/main.py
+++ product_rental/controllers/main.py
@@ -8,15 +8,18 @@ class RentalProductWebsiteSale(WebsiteSale):
     @http.route()
     def product(self, product, category='', search='', **kwargs):
         result = super().product(product, category='', search='', **kwargs)
-        if product.is_rental and product.is_contract:
-            ct = product.sudo().property_contract_template_id
-            rtypes = dict(ct.fields_get()['commitment_period_type']['selection'])
+        if product.is_rental:
             result.qcontext.update({
-                "commitment_period": {
-                    "number": ct.commitment_period_number,
-                    "type": rtypes[ct.commitment_period_type].lower(),
-                },
                 "rental_price_base": product.rental_price,
                 "rental_price_ratio": product.rental_price_ratio(),
             })
+            if product.is_contract:
+                ct = product.sudo().property_contract_template_id
+                rtypes = dict(ct.fields_get()['commitment_period_type']['selection'])
+                result.qcontext.update({
+                    "commitment_period": {
+                        "number": ct.commitment_period_number,
+                        "type": rtypes[ct.commitment_period_type].lower(),
+                    },
+                })
         return result
