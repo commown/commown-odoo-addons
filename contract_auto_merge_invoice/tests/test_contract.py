74f9b3c81ec7035ef586f400f873efbc17e43e12
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 10:34:29 +0200

    [META] enable black

--- contract_auto_merge_invoice/tests/test_contract.py
+++ contract_auto_merge_invoice/tests/test_contract.py
@@ -6,7 +6,6 @@ from odoo.tests.common import at_install, post_install
 @at_install(False)
 @post_install(True)
 class ContractTC(TestContractBase):
-
     def test_auto_merge_invoice(self):
         invoice = self.contract.recurring_create_invoice()
         self.assertTrue(invoice.auto_merge)
