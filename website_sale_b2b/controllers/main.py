3c7ba305c681e8d80fd3c6b771eba4f978f73ccd
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 10:34:29 +0200

    [META] enable black

--- website_sale_b2b/controllers/main.py
+++ website_sale_b2b/controllers/main.py
@@ -9,37 +9,41 @@ _logger = logging.getLogger(__name__)
 
 
 class WebsiteSaleB2B(WebsiteSale):
-
     def get_attribute_value_ids(self, product):
-        """ When in B2B, return prices without taxes. This is a duplication of
+        """When in B2B, return prices without taxes. This is a duplication of
         website_sale controller method but replacing website_public_price by
         lst_price for B2B products.
         """
 
-        _logger.debug(
-            'In WebsiteSaleB2B.get_attribute_value_ids, product %s', product)
+        _logger.debug("In WebsiteSaleB2B.get_attribute_value_ids, product %s", product)
         if product.is_b2b():
-            _logger.info('Displaying product in B2B mode: %r', product.name)
-            quantity = product._context.get('quantity') or 1
+            _logger.info("Displaying product in B2B mode: %r", product.name)
+            quantity = product._context.get("quantity") or 1
             product = product.with_context(quantity=quantity)
 
-            visible_attrs_ids = product.attribute_line_ids.filtered(
-                lambda l: len(l.value_ids) > 1).mapped('attribute_id').ids
+            visible_attrs_ids = (
+                product.attribute_line_ids.filtered(lambda l: len(l.value_ids) > 1)
+                .mapped("attribute_id")
+                .ids
+            )
             to_currency = request.website.get_current_pricelist().currency_id
             res = []
             for variant in product.product_variant_ids:
                 if to_currency != product.currency_id:
-                    lst_price = variant.currency_id.compute(
-                        variant.lst_price, to_currency) / quantity
+                    lst_price = (
+                        variant.currency_id.compute(variant.lst_price, to_currency)
+                        / quantity
+                    )
                 else:
                     lst_price = variant.lst_price / quantity
                 visible_attribute_ids = [
-                    v.id for v in variant.attribute_value_ids
-                    if v.attribute_id.id in visible_attrs_ids]
-                res.append([
-                    variant.id,
-                    visible_attribute_ids,
-                    variant.price, lst_price])
+                    v.id
+                    for v in variant.attribute_value_ids
+                    if v.attribute_id.id in visible_attrs_ids
+                ]
+                res.append(
+                    [variant.id, visible_attribute_ids, variant.price, lst_price]
+                )
         else:
             res = super(WebsiteSaleB2B, self).get_attribute_value_ids(product)
         return res
