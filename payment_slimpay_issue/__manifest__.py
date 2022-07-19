6dcab377f9e6102108fdc95fab2a855860db5bbd
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 10:34:29 +0200

    [META] enable black

--- payment_slimpay_issue/__manifest__.py
+++ payment_slimpay_issue/__manifest__.py
@@ -3,26 +3,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Payment Slimpay issue',
-    'category': 'Website',
-    'version': '12.0.1.0.0',
-    'author': "Commown SCIC SAS",
-    'license': "AGPL-3",
-    'website': "https://commown.coop",
-    'depends': [
-        'account_payment_partner',
-        'account_payment_slimpay',
-        'base_automation',
-        'project',
+    "name": "Payment Slimpay issue",
+    "category": "Website",
+    "version": "12.0.1.0.0",
+    "author": "Commown SCIC SAS",
+    "license": "AGPL-3",
+    "website": "https://commown.coop",
+    "depends": [
+        "account_payment_partner",
+        "account_payment_slimpay",
+        "base_automation",
+        "project",
     ],
-    'data': [
-        'data/mail_template.xml',
-        'data/project.xml',
-        'data/product.xml',
-        'data/cron.xml',
-        'data/action.xml',
-        'data/res_partner.xml',
-        'views/project_task.xml',
+    "data": [
+        "data/mail_template.xml",
+        "data/project.xml",
+        "data/product.xml",
+        "data/cron.xml",
+        "data/action.xml",
+        "data/res_partner.xml",
+        "views/project_task.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
