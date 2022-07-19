a4c2e41cbfb6e8eba4ddadfe1a7a696a4086a453
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 10:34:29 +0200

    [META] enable black

--- scic/__manifest__.py
+++ scic/__manifest__.py
@@ -1,26 +1,26 @@
 # Copyright 2020 Commown SCIC SAS (https://commown.fr)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "SCIC (Société Coopérative d'Intérêts Collectifs)",
-    'category': 'Custom',
-    'summary': 'Module that defines functionalities related to the SCIC companies',
-    'version': '12.0.1.0.0',
-    'description': """
+    "name": "SCIC (Société Coopérative d'Intérêts Collectifs)",
+    "category": "Custom",
+    "summary": "Module that defines functionalities related to the SCIC companies",
+    "version": "12.0.1.0.0",
+    "description": """
     SCIC
     ====
     This module adds functionalities:
 
 - ability to mark products as equities
     """,
-    'author': "Commown SCIC SAS",
-    'license': "AGPL-3",
-    'website': "https://commown.coop",
-    'depends': [
-        'website_sale',
+    "author": "Commown SCIC SAS",
+    "license": "AGPL-3",
+    "website": "https://commown.coop",
+    "depends": [
+        "website_sale",
     ],
-    'data': [
-        'views/product_template.xml',
+    "data": [
+        "views/product_template.xml",
     ],
-    'installable': True,
-    'application': True,
+    "installable": True,
+    "application": True,
 }
