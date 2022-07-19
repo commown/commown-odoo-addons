f8139f9e346a39ccc167ba91bedaf337f370c451
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-19 10:34:29 +0200

    [META] enable black

--- payment_slimpay_dump_restore_utils/models/payment.py
+++ payment_slimpay_dump_restore_utils/models/payment.py
@@ -4,22 +4,20 @@ from .utils import dump_all_mandates, restore_all_missing_mandates
 
 
 class PaymentAcquirerSlimpayDumpRestore(models.Model):
-    _inherit = 'payment.acquirer'
+    _inherit = "payment.acquirer"
 
     @api.model
     def _slimpay_dump_all_mandates(
-            self, refresh=True, acquirer=None,
-            mandates_fpath='/tmp/mandates.json', **params):
+        self, refresh=True, acquirer=None, mandates_fpath="/tmp/mandates.json", **params
+    ):
         if acquirer is None:
-            acquirer = self.env.ref(
-                'account_payment_slimpay.payment_acquirer_slimpay')
+            acquirer = self.env.ref("account_payment_slimpay.payment_acquirer_slimpay")
         dump_all_mandates(acquirer, refresh, mandates_fpath, **params)
 
     @api.model
     def _slimpay_restore_mandates(
-            self, acquirer=None,
-            mandates_fpath='/tmp/mandates.json', **params):
+        self, acquirer=None, mandates_fpath="/tmp/mandates.json", **params
+    ):
         if acquirer is None:
-            acquirer = self.env.ref(
-                'account_payment_slimpay.payment_acquirer_slimpay')
+            acquirer = self.env.ref("account_payment_slimpay.payment_acquirer_slimpay")
         restore_all_missing_mandates(acquirer, mandates_fpath, **params)
