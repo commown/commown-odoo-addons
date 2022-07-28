5d9e8561d69aa30bc89b3e5b475a7af3ec4b56e1
logan <loganlenevez@gmail.com>
2022-07-28 12:44:12 +0200

    Implementing new case to post a troubleshoot on the Termination project

--- commown_self_troubleshooting/controllers/main.py
+++ commown_self_troubleshooting/controllers/main.py
@@ -39,7 +39,7 @@ class SelfHelp(http.Controller):
             "contract_id": int(post["device_contract"]),
             "partner_id": partner.id,
             "description": self._description(**post),
-            "project_id": self.ref("support_project").id,
+            "project_id": env.ref("commown_self_troubleshooting." + post.get("project_ref", "support_project")).id,
             "tag_ids": [(6, 0, self._tag_ids(**post))],
         }
         if post.get("stage_id", None):
