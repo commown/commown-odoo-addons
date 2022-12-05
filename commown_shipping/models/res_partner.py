6faccb2d5288df9baca89ff0ddcb2edf692af39e
Florent Cayré <florent@commown.coop>
2022-12-05 06:03:07 +0100

    Limit address lines maximum length to the one enforced by Colissimo (35)
    This is done using SQL constraints for both street and street2 fields.
    
    An helper method is supplied and tested for controllers which need it
    (portal and shop).

--- commown_shipping/models/res_partner.py
+++ commown_shipping/models/res_partner.py
@@ -1,12 +1,45 @@
-from odoo import api, models
+from odoo import _, api, models
 
-from .colissimo_utils import delivery_data
+from .colissimo_utils import MAX_ADDRESS_SIZE_COLISSIMO, delivery_data
+
+ADDRESS_LENGTH_ERROR = (
+    "The street field size cannot exceed %d" % MAX_ADDRESS_SIZE_COLISSIMO
+)
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
+    MAX_ADDRESS_SIZE = MAX_ADDRESS_SIZE_COLISSIMO
+
+    _sql_constraints = [
+        (
+            "street_max_size_colissimo",
+            "CHECK(CHAR_LENGTH(street) <= %d)" % MAX_ADDRESS_SIZE_COLISSIMO,
+            ADDRESS_LENGTH_ERROR,
+        ),
+        (
+            "street2_max_size_colissimo",
+            "CHECK(CHAR_LENGTH(street2) <= %d)" % MAX_ADDRESS_SIZE_COLISSIMO,
+            ADDRESS_LENGTH_ERROR,
+        ),
+    ]
 
     @api.multi
     def colissimo_delivery_data(self):
         self.ensure_one()
         return delivery_data(self)
+
+    @classmethod
+    def validate_street_lines(cls, data, error, error_message):
+        street_error = False
+        for field in ("street", "street2"):
+            value = data.get(field)
+            if value and len(value) > MAX_ADDRESS_SIZE_COLISSIMO:
+                error[field] = "error"
+                street_error = True
+
+        if street_error:
+            error_message.append(
+                _("Address lines' length is limited to %s. Please fix.")
+                % MAX_ADDRESS_SIZE_COLISSIMO
+            )
