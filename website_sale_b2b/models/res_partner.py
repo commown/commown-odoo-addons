2173aaf186873444171f6d8a3d4622652584d7c7
Florent Cayré <florent@commown.coop>
2022-12-12 14:55:49 +0100

    Do not considerer only started contracts to determine the price drop on the B2B shop

--- website_sale_b2b/models/res_partner.py
+++ website_sale_b2b/models/res_partner.py
@@ -22,7 +22,23 @@ class ResPartner(models.Model):
 
     @api.multi
     def rented_quantity(self, product_template=None, product_category=None):
+        """Return the number of devices rented by the partner (or its company)
+
+        We MUST take not yet started contracts in consideration here
+        (but not finished ones) as this method is used for volume
+        price discount, and future contracts are welcome!
+
+        We count rented devices which are a variant of the given
+        product template or in the given web category. Note that one
+        (and only one) of those two filters must be given, otherwise
+        an AssertionError is raised.
+        """
+
         self.ensure_one()
+        assert (
+            bool(product_template) != bool(product_category),
+            "One of product_template and product_category must be given and non-empty",
+        )
 
         today = fields.Date.today()
 
@@ -31,7 +47,6 @@ class ResPartner(models.Model):
                 "|",
                 ("date_end", "=", False),
                 ("date_end", ">", today),
-                ("date_start", "<=", today),
                 (
                     "contract_id.partner_id.commercial_partner_id",
                     "=",
@@ -48,11 +63,11 @@ class ResPartner(models.Model):
                 rented_by_product[pt.id] += cline.quantity
                 categories_by_product[pt.id] = set(pt.public_categ_ids.ids)
 
-        if product_template is not None:
+        if product_template:
             assert product_template._name == "product.template"
             result = rented_by_product.get(product_template.id, 0.0)
 
-        if product_category:
+        elif product_category:
             categ_ids = _all_children(product_category)
             result = sum(
                 rented_by_product[p_id]
