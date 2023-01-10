712839a9ce199b6f9c8edcc665a8e50d1545ec7f
Florent Cayré <florent@commown.coop>
2023-01-10 21:48:40 +0100

    Add dynamic rental price on shop optional products modal
    The rental price may depend on the quantity, so it is dynamically updated.

--- product_rental/static/src/js/website_sale.js
+++ product_rental/static/src/js/website_sale.js
@@ -17,3 +17,23 @@ sAnimations.registry.WebsiteSale.include({
 return ProductConfiguratorMixin;
 
 });
+
+
+odoo.define('product_rental.OptionalProductsModal', function (require) {
+'use strict';
+
+var optionalProductModal = require('sale.OptionalProductsModal');
+
+optionalProductModal.include({
+    _onChangeCombination: function (ev, $parent, combination){
+        this._super.apply(this, arguments);
+        let $rental_price = $parent.find('.oe_rental_price');
+        let ratio = parseFloat($rental_price.data('rental-ratio'));
+        let price = this._priceToStr(combination.price / ratio);
+        $rental_price.find(".oe_currency_value").html(price);
+    }
+});
+
+return optionalProductModal;
+
+});
