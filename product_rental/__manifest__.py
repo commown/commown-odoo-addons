01d4bfb4d70cc81ea13cd6357cfc9e3df7f11b0e
Florent Cayré <florent@commown.coop>
2023-01-11 19:04:21 +0100

    product_rental 12.0.1.1.0

--- product_rental/__manifest__.py
+++ product_rental/__manifest__.py
@@ -2,9 +2,9 @@
     "name": "Rental product",
     "category": "Sale",
     "summary": "Define products as rental products",
-    "version": "12.0.1.0.4",
+    "version": "12.0.1.1.0",
     "description": """Rental products have a fixed deposit and recurring price, displayed on the product page""",
-    "author": "Commown SCIC SAS",
+    "author": "Commown SCIC",
     "license": "AGPL-3",
     "website": "https://commown.coop",
     "depends": [
