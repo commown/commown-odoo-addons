9af443b36a692778909db9d8290f8986b4602533
Florent Cayré <florent@commown.coop>
2023-01-17 16:51:00 +0100

    Restore report t10n feature for invoices

---
+++ custom_report/i18n/__init__.py
@@ -0,0 +1,9 @@
+"""
+Import i18n-dedicated python file
+
+This makes it possible to insert name-specific ir.translation lines so
+that the translated words are really specific: they may have a legal
+role which requires precision.
+"""
+
+from . import i18n
