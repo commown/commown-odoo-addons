1128f31271ba5fbefd42f0f014776caf93d024ab
Florent Cayré <florent@commown.coop>
2023-03-07 10:11:01 +0100

    Add payment token obsolescence action that set new token partner's invoice merge/ payment preferences

--- commown/models/payment_token_obsolescence_action.py
+++ commown/models/payment_token_obsolescence_action.py
@@ -9,8 +9,14 @@ class PaymentTokenUniquifyObsolescenceAction(models.Model):
     technical_name = fields.Selection(
         selection_add=[
             ("reattribute_contracts", "Reattribute contracts"),
-            ("reattribute_draft_contract_invoices",
-             "Reattribute draft contract invoices"),
+            (
+                "reattribute_draft_contract_invoices",
+                "Reattribute draft contract invoices",
+            ),
+            (
+                "set_partner_invoice_merge_prefs",
+                "Set partner invoice merge preferences",
+            ),
         ],
     )
 
@@ -42,7 +48,9 @@ class PaymentTokenUniquifyObsolescenceAction(models.Model):
         )
 
     @api.model
-    def _run_action_reattribute_draft_contract_invoices(self, obsolete_tokens, new_token):
+    def _run_action_reattribute_draft_contract_invoices(
+        self, obsolete_tokens, new_token
+    ):
         """Reattribute the draft invoices of partners which token just became obsolete
         to the partner of the new token.
         """
@@ -55,3 +63,44 @@ class PaymentTokenUniquifyObsolescenceAction(models.Model):
                 ("invoice_line_ids.contract_line_id", "!=", False),
             ]
         ).update({"partner_id": new_token.partner_id.id})
+
+    @api.model
+    def _run_action_set_partner_invoice_merge_prefs(self, obsolete_tokens, new_token):
+        """Set new token's partner invoice merge/ payment preferences if not already set
+
+        Following choices are made for the merge preferences:
+
+        - Recurring type will be the smallest of the obsolete_tokens' partners
+        - The recurring interval is the one corresponding to the chosen
+          recurring type
+        - Next merge date is computed as the max merge date of the
+          obsoleted token partners.
+
+        Note that most of the time there will be only one partner
+        whose token has become obsolete.
+
+        """
+
+        field_rtype = "invoice_merge_recurring_rule_type"
+        field_rinterval = "invoice_merge_recurring_interval"
+        field_date = "invoice_merge_next_date"
+
+        partners = obsolete_tokens.mapped("partner_id")
+
+        rtypes = [
+            rt[0] for rt in partners.fields_get(field_rtype)[field_rtype]["selection"]
+        ]
+        rtype, rinterval = (partners[0][field_rtype], partners[0][field_rinterval])
+
+        for partner in partners:
+            if rtypes.index(partner[field_rtype]) < rtypes.index(rtype):
+                rtype = partner[field_rtype]
+                rinterval = partner[field_rinterval]
+
+        new_token.partner_id.update(
+            {
+                field_rtype: rtype,
+                field_rinterval: rinterval,
+                field_date: max(partners.mapped(field_date)),
+            }
+        )
