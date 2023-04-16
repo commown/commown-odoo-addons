744c86b6d4e00fad56118003e6dc3f06d28cabc3
Florent Cayré <florent@commown.coop>
2023-04-16 11:05:04 +0200

    Add an index on account_move_line.full_reconcile_id to speed-up reconcilation dramatically
    This will have a negative performance impact on insertion of course, but we do not massively
    insert account_move_line lines so it should not be a problem for us.

--- commown/models/account_move_line.py
+++ commown/models/account_move_line.py
@@ -1,9 +1,11 @@
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class CommownAccountMoveLine(models.Model):
 
     _inherit = "account.move.line"
+
+    full_reconcile_id = fields.Many2one(index=True)
 
     @api.multi
     def step_workflow(self):
