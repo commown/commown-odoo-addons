17fe02da0da00c5b96b745567d8f7b9da200dce7
louck <luc.parent@commown.coop>
2023-06-15 17:26:37 +0200

    Modify action's code to fit with v12 method and add tests

--- commown/models/account_invoice.py
+++ commown/models/account_invoice.py
@@ -15,3 +15,47 @@ class AccountInvoice(models.Model):
         except ValueError:
             pass
         return key_cols
+
+    @api.multi
+    def _multiply_investments(self, multiplier=10):
+
+        product_ids = (
+            self.env["product.template"]
+            .with_context(active_test=False)
+            .search(
+                [
+                    ("is_equity", "=", True),
+                    ("equity_type", "=", "invest"),
+                ]
+            )
+            .ids
+        )
+
+        for invoice in self:
+            has_invests = any(
+                l
+                for l in invoice.invoice_line_ids
+                if l.product_id.product_tmpl_id.id in product_ids
+            )
+
+            if invoice.residual != 0 or not has_invests:
+                continue
+
+            invoice.payment_move_line_ids
+            invoice.payment_move_line_ids.remove_move_reconcile()
+            self.env.cache.invalidate()
+
+            invoice.action_invoice_cancel()
+            self.env.cache.invalidate()
+
+            invoice.action_invoice_draft()
+            investment_payment_term = self.env.ref("commown.investment_payment_term")
+            invoice.write({"payment_term_id": investment_payment_term.id})
+            self.env.cache.invalidate()
+
+            for line in invoice.invoice_line_ids:
+                if line.product_id.product_tmpl_id.id in product_ids:
+                    line.update({"price_unit": line.price_unit * multiplier})
+
+            invoice.action_invoice_open()
+            self.env.cache.invalidate()
--- commown/tests/test_account_invoice.py
+++ commown/tests/test_account_invoice.py
@@ -25,3 +25,77 @@ class AccountInvoiceTC(AutoPayInvoiceTC):
 
         with patch.object(PaymentTransaction, "s2s_do_transaction", fake_do_tx_ok):
             self._multiple_invoice_merge_test([inv_1, inv_2])
+
+    def test_multiply_investments(self):
+        account = self.env.ref("l10n_fr.1_pcg_2751")
+        equity = self.env["product.product"].create(
+            {
+                "name": "Investment test product",
+                "is_equity": True,
+                "equity_type": "invest",
+                "list_price": 60.0,
+            }
+        )
+        not_equity = self.env["product.product"].create(
+            {
+                "name": "Not Investment test product",
+                "is_equity": False,
+                "list_price": 60.0,
+            }
+        )
+
+        equity_invoice_line = self.env["account.invoice.line"].create(
+            {
+                "name": "Test investment invoice line",
+                "account_id": account.id,
+                "product_id": equity.id,
+                "quantity": 1,
+                "price_unit": 60,
+            }
+        )
+        not_equity_invoice_line = self.env["account.invoice.line"].create(
+            {
+                "name": "Test not investment invoice line",
+                "account_id": account.id,
+                "product_id": not_equity.id,
+                "quantity": 1,
+                "price_unit": 60,
+            }
+        )
+
+        equity_invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.partner_1.id,
+            }
+        )
+        not_equity_invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.partner_1.id,
+            }
+        )
+        equity_invoice.invoice_line_ids |= equity_invoice_line
+        equity_invoice.stage = "paid"
+
+        not_equity_invoice.invoice_line_ids |= not_equity_invoice_line
+        not_equity_invoice.stage = "paid"
+
+        equity_old_price = equity_invoice_line.price_unit
+        not_equity_old_price = not_equity_invoice_line.price_unit
+
+        multiplier = 10
+
+        equity_invoice._multiply_investments(multiplier)
+        not_equity_invoice._multiply_investments(multiplier)
+
+        self.assertEqual(
+            equity_invoice.invoice_line_ids.price_unit,
+            equity_old_price * multiplier,
+        )
+        self.assertEqual(
+            equity_invoice.payment_term_id,
+            self.env.ref("commown.investment_payment_term"),
+        )
+        self.assertEqual(
+            not_equity_invoice.invoice_line_ids.price_unit,
+            not_equity_old_price,
+        )
