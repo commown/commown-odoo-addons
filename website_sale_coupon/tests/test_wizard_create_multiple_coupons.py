b90becf4757072428cee47f8f433e9d42961cd57
louck <luc.parent@commown.coop>
2023-06-28 15:19:15 +0200

    Select coupons once created with wizard

--- website_sale_coupon/tests/test_wizard_create_multiple_coupons.py
+++ website_sale_coupon/tests/test_wizard_create_multiple_coupons.py
@@ -10,22 +10,30 @@ class TestCreateMultipleCoupons(common.SavepointCase):
         cls.partner_test_campaign = cls.env["res.partner"].create(
             {"name": "Partner test campaign"}
         )
-        cls.campaign = cls.env["coupons.campain"].create(
+
+        cls.campaign = cls.env["coupon.campaign"].create(
             {
-                "name": "Test camapign",
-                "partner_id": cls.partner_test_campaign.id,
+                "name": "Test campaign",
+                "seller_id": cls.partner_test_campaign.id,
             }
         )
 
     def test_create_multiple_coupons(self):
         coupon_number = 13
-        self.env["website_sale_coupon.create_multiple_coupons_wizard"].create(
-            {
-                "campaign_id": self.campaign.id,
-                "coupon_nb": coupon_number,
-            }
-        ).create_multiple_coupons()
-        created_coupons = self.env[
-            "website_sale_coupon.create_multiple_coupons_wizard"
-        ].search([("campaign_id", "=", self.campaign)])
+        created_coupons_ids = (
+            self.env["website_sale_coupon.create_multiple_coupons_wizard"]
+            .create(
+                {
+                    "campaign_id": self.campaign.id,
+                    "coupon_nb": coupon_number,
+                }
+            )
+            .button_create_and_open_coupons()
+        )
+        created_coupons = self.env["coupon.coupon"].search(
+            [("campaign_id.id", "=", self.campaign.id)]
+        )
         self.assertEqual(len(created_coupons), coupon_number)
+        self.assertEqual(
+            [("id", "in", created_coupons.ids)], created_coupons_ids["domain"]
+        )
