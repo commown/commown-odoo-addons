1109dc90364fa15b2b20fede718a5aeb19fc0536
Florent Cayré <florent@commown.coop>
2023-06-28 08:43:09 +0200

    urban_mine 1.1.0

--- urban_mine/__manifest__.py
+++ urban_mine/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Urban mine",
     "category": "Business",
     "summary": "Urban mine offer: people sell their device to the company",
-    "version": "12.0.1.0.3",
+    "version": "12.0.1.1.0",
     "description": "Urban mine offer: people sell their device to the company",
     "author": "Commown SCIC",
     "license": "AGPL-3",
