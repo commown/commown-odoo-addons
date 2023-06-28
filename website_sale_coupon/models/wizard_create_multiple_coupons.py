7fe357c5930fa934d532657ead8576d44b5d71ed
louck <luc.parent@commown.coop>
2023-06-28 15:19:15 +0200

    Select coupons once created with wizard

--- website_sale_coupon/models/wizard_create_multiple_coupons.py
+++ website_sale_coupon/models/wizard_create_multiple_coupons.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class CouponCreateMultipleWizard(models.TransientModel):
@@ -18,4 +18,17 @@ class CouponCreateMultipleWizard(models.TransientModel):
 
     def create_multiple_coupons(self):
         coupons = [{"campaign_id": self.campaign_id.id} for i in range(self.coupon_nb)]
-        self.env["coupon.coupon"].create(coupons)
+        created_coupons = self.env["coupon.coupon"].create(coupons)
+        return created_coupons.ids
+
+    @api.multi
+    def button_create_and_open_coupons(self):
+        self.ensure_one()
+        created_coupons_ids = self.create_multiple_coupons()
+        return {
+            "name": _("Coupons"),
+            "domain": [("id", "in", created_coupons_ids)],
+            "type": "ir.actions.act_window",
+            "view_mode": "tree,form",
+            "res_model": "coupon.coupon",
+        }
