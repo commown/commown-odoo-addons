e7a2aba7e5fbcc82f16199d380d5b18cc4d7e71b
louck <luc.parent@commown.coop>
2023-07-13 10:22:06 +0200

    Add fields storing primary & secondary storable product on service variant
    Also added a funnction to compute those fields using Service storable
    configuarions.

--- commown_devices/models/product.py
+++ commown_devices/models/product.py
@@ -1,7 +1,7 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
-class Product(models.Model):
+class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     storable_product_id = fields.Many2one(
@@ -15,3 +15,66 @@ class Product(models.Model):
         comodel_name="product.service_storable_config",
         inverse_name="service_tmpl_id",
     )
+
+    @api.multi
+    def create_variant_ids(self):
+        res = super(ProductTemplate, self).create_variant_ids()
+        self.product_variant_ids._set_storable_variants()
+        return res
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    primary_storable_variant_id = fields.Many2one(
+        "product.product",
+        string="Primary storable variant",
+        domain='[("type", "=", "product")]',
+    )
+
+    secondary_storable_variant_ids = fields.Many2many(
+        "product.product",
+        relation="secondary_variants",
+        column1="service_product_id",
+        column2="storable_product_id",
+        string="Secondary storable variants",
+        domain='[("type", "=", "product")]',
+    )
+
+    @api.multi
+    def _set_storable_variants(self, template_configs=None):
+        for rec in self:
+            if template_configs is None:
+                template_configs = rec.product_tmpl_id.storable_config_ids
+            configs = template_configs.filtered(
+                lambda c: set(c.attribute_value_ids.ids).issubset(
+                    rec.attribute_value_ids.ids
+                )
+            )
+            primary_variant = self.env["product.product"]
+            secondary_variants = self.env["product.product"]
+            for c in configs:
+                if c.storable_type == "primary":
+                    primary_variant |= c.storable_variant_id
+                elif c.storable_type == "secondary":
+                    secondary_variants |= c.storable_variant_id
+            assert len(primary_variant) <= 1, _(
+                "More than one primary variant configured for %s with attributes %s"
+                % (rec.name, rec.attribute_value_ids.mapped("name"))
+            )
+            if len(secondary_variants) > len(
+                secondary_variants.mapped("product_tmpl_id")
+            ):
+                raise Warning(
+                    _(
+                        "For product %s, two secondary variants derive from the same template,"
+                        " are you sure that there is no configuration mistake ?"
+                        % rec.name
+                    )
+                )
+            rec.update(
+                {
+                    "primary_storable_variant_id": primary_variant.id,
+                    "secondary_storable_variant_ids": [(6, 0, secondary_variants.ids)],
+                }
+            )
---
+++ commown_devices/tests/test_product.py
@@ -0,0 +1,199 @@
+from .common import DeviceAsAServiceTC, add_attributes_to_product, create_config
+
+
+class ProductTemplateTC(DeviceAsAServiceTC):
+    def setUp(self, *args, **kwargs):
+        super(ProductTemplateTC, self).setUp(*args, **kwargs)
+
+        self.fp3_plus_service_tmpl = self._create_rental_product("fp3+").product_tmpl_id
+        self.fp3_plus_storable_tmpl = self.storable_product.copy({"name": "fp3+"})
+
+        self.protective_screen = self.env["product.template"].create(
+            {
+                "name": "Protective Screen",
+                "type": "product",
+                "tracking": "none",
+            }
+        )
+
+        self.attribute_color = self.env.ref("product.product_attribute_2")
+        color_values = self.env["product.attribute.value"].search(
+            [("attribute_id.id", "=", self.attribute_color.id)]
+        )
+
+        add_attributes_to_product(
+            self.fp3_plus_storable_tmpl,
+            self.attribute_color,
+            color_values,
+        )
+        self.fp3_plus_storable_tmpl.create_variant_ids()
+
+        self.color1 = color_values[0]
+        self.color2 = color_values[1]
+        self.fp3_plus_storable_color1 = self.env["product.product"].search(
+            [
+                ("product_tmpl_id", "=", self.fp3_plus_storable_tmpl.id),
+                ("attribute_value_ids.id", "ilike", self.color1.id),
+            ]
+        )
+        self.fp3_plus_storable_color2 = self.env["product.product"].search(
+            [
+                ("product_tmpl_id", "=", self.fp3_plus_storable_tmpl.id),
+                ("attribute_value_ids.id", "ilike", self.color2.id),
+            ]
+        )
+
+        create_config(
+            self.fp3_plus_service_tmpl,
+            "primary",
+            self.fp3_plus_storable_tmpl,
+            self.fp3_plus_storable_color1,
+            att_val_ids=self.color1,
+        )
+
+        create_config(
+            self.fp3_plus_service_tmpl,
+            "secondary",
+            self.protective_screen,
+            self.protective_screen.product_variant_id,
+        )
+
+    def test_behavior_on_new_variant_or_config(self):
+        self.assertEqual(
+            len(
+                self.fp3_plus_service_tmpl.mapped(
+                    "product_variant_ids.primary_storable_variant_id"
+                )
+            ),
+            0,
+        )
+        self.assertEqual(
+            len(
+                self.fp3_plus_service_tmpl.mapped(
+                    "product_variant_ids.secondary_storable_variant_ids"
+                )
+            ),
+            0,
+        )
+        add_attributes_to_product(
+            self.fp3_plus_service_tmpl,
+            self.attribute_color,
+            self.color1 + self.color2,
+        )
+        self.fp3_plus_service_tmpl.create_variant_ids()
+        # Check that primary and secondary variant has been affected to fp3+ of color1
+        fp3_plus_service_color1 = self.env["product.product"].search(
+            [
+                ("product_tmpl_id", "=", self.fp3_plus_service_tmpl.id),
+                ("attribute_value_ids.id", "ilike", self.color1.id),
+            ]
+        )
+        self.assertEqual(
+            fp3_plus_service_color1.primary_storable_variant_id,
+            self.fp3_plus_storable_color1,
+        )
+        self.assertEqual(
+            fp3_plus_service_color1.secondary_storable_variant_ids,
+            self.protective_screen.product_variant_id,
+        )
+
+        # Create primary variant config to fp3 plus color2
+        create_config(
+            self.fp3_plus_service_tmpl,
+            "primary",
+            self.fp3_plus_storable_tmpl,
+            self.fp3_plus_storable_color2,
+            att_val_ids=self.color2,
+        )
+        # Manually trigger onchange method
+        self.fp3_plus_service_tmpl.onchange_storable_config_ids()
+        # Check primary variant has been set
+        fp3_plus_service_color2 = self.env["product.product"].search(
+            [
+                ("product_tmpl_id", "=", self.fp3_plus_service_tmpl.id),
+                ("attribute_value_ids.id", "ilike", self.color2.id),
+            ]
+        )
+        self.assertEqual(
+            fp3_plus_service_color2.primary_storable_variant_id,
+            self.fp3_plus_storable_color2,
+        )
+        self.assertEqual(
+            fp3_plus_service_color2.secondary_storable_variant_ids,
+            self.protective_screen.product_variant_id,
+        )
+
+        # Check that it is impossible to have more than one primary variant associted to one service
+        with self.assertRaises(AssertionError) as err:
+            false_config = create_config(
+                self.fp3_plus_service_tmpl,
+                "primary",
+                self.fp3_plus_storable_tmpl,
+                self.fp3_plus_storable_color1,
+                att_val_ids=self.color2,
+            )
+            self.fp3_plus_service_tmpl.onchange_storable_config_ids()
+        self.assertEqual(
+            (
+                "More than one primary variant configured for %s with attributes %s"
+                % (
+                    fp3_plus_service_color2.name,
+                    fp3_plus_service_color2.attribute_value_ids.mapped("name"),
+                )
+            ),
+            err.exception.args[0],
+        )
+        # Manually remove false config
+        false_config.unlink()
+
+        # Check that a warning is isued if two secondary variants have the same template
+        backcover = self.env["product.template"].create(
+            {
+                "name": "Back Cover",
+                "type": "product",
+                "tracking": "none",
+            }
+        )
+        add_attributes_to_product(
+            backcover,
+            self.attribute_color,
+            self.color1 + self.color2,
+        )
+        backcover.create_variant_ids()
+        back_cover_color1 = self.env["product.product"].search(
+            [
+                ("product_tmpl_id", "=", backcover.id),
+                ("attribute_value_ids.id", "ilike", self.color1.id),
+            ]
+        )
+        back_cover_color2 = self.env["product.product"].search(
+            [
+                ("product_tmpl_id", "=", backcover.id),
+                ("attribute_value_ids.id", "ilike", self.color2.id),
+            ]
+        )
+        create_config(
+            self.fp3_plus_service_tmpl,
+            "secondary",
+            backcover,
+            back_cover_color1,
+            att_val_ids=self.color1,
+        )
+        self.fp3_plus_service_tmpl.onchange_storable_config_ids()
+        with self.assertRaises(Warning) as err2:
+            create_config(
+                self.fp3_plus_service_tmpl,
+                "secondary",
+                backcover,
+                back_cover_color2,
+                att_val_ids=self.color1,
+            )
+            self.fp3_plus_service_tmpl.onchange_storable_config_ids()
+        self.assertEqual(
+            (
+                "For product %s, two secondary variants derive from the same template,"
+                "are you sure that there is no configuration mistake ?"
+                % fp3_plus_service_color2.name
+            ),
+            err2.exception.args[0],
+        )
