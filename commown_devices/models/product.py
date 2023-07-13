116cd73de2f9b3971cc26c5c989ca9d350d98213
louck <luc.parent@commown.coop>
2023-07-13 10:14:53 +0200

    Display service_storable_config on product template
    Added service storable configuration field on product template and a view to display them.
    This view allows configurations edition directly on product template
    view.

--- commown_devices/models/product.py
+++ commown_devices/models/product.py
@@ -9,3 +9,9 @@ class Product(models.Model):
         string="Storable product",
         domain='[("type", "=", "product")]',
     )
+
+    storable_config_ids = fields.One2many(
+        string="Storable configurations",
+        comodel_name="product.service_storable_config",
+        inverse_name="service_tmpl_id",
+    )
