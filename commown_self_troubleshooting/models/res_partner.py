c0110a8fd9bced0fc283c1124d65a9014ea6edff
Florent Cayré <florent@commown.coop>
2023-07-17 19:29:21 +0200

    Add ST meta-data features: a sequence for page/ category order and no contract domain
    No contract domain is useful for the (coming-up) page of a general way
    to contact Commown, even when user has no pending contract.

--- commown_self_troubleshooting/models/res_partner.py
+++ commown_self_troubleshooting/models/res_partner.py
@@ -14,7 +14,6 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     def _self_troubleshooting_webpage_data(self, page):
-        contract_model = self.env["contract.contract"]
         today = datetime.date.today()
         base_domain = [
             ("partner_id.commercial_partner_id", "=", self.commercial_partner_id.id),
@@ -28,7 +27,7 @@ class ResPartner(models.Model):
             HERE, "..", *page.arch_fs.split(osp.sep)[1:-1], "meta_data.py"
         )
         with open(f_path) as fobj:
-            meta_data = safe_eval(
+            return safe_eval(
                 fobj.read(),
                 {
                     "env": self.env,
@@ -37,14 +36,15 @@ class ResPartner(models.Model):
                 },
             )
 
-        return contract_model.search(meta_data["contract_domain"])
-
     def self_troubleshooting_contracts(self, page_ref):
         page = self.env.ref("commown_self_troubleshooting." + page_ref)
-        return self._self_troubleshooting_webpage_data(page)
+        meta_data = self._self_troubleshooting_webpage_data(page)
+        if meta_data.get("contract_domain", None) is None:
+            return self.env["contract.contract"]
+        else:
+            return self.env["contract.contract"].search(meta_data["contract_domain"])
 
     def self_troubleshooting_all_data(self):
-        categories = defaultdict(list)
         page_refs = self.env["ir.model.data"].search(
             [
                 ("module", "=", "commown_self_troubleshooting"),
@@ -52,11 +52,18 @@ class ResPartner(models.Model):
             ]
         )
 
+        pages = []
         for ref in page_refs:
             page = self.env.ref(ref.module + "." + ref.name)
-            contracts = self._self_troubleshooting_webpage_data(page)
-            if contracts:
-                categories[page.website_meta_title].append(page)
+            meta_data = self._self_troubleshooting_webpage_data(page)
+            if meta_data.get("contract_domain", None) is None or self.env[
+                "contract.contract"
+            ].search(meta_data["contract_domain"]):
+                pages.append((meta_data.get("sequence", 0), page))
+
+        categories = defaultdict(list)
+        for sequence, page in sorted(pages):
+            categories[page.website_meta_title].append(page)
 
         return [
             {
