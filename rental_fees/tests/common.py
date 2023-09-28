df813f656a658fdae04f5818625c49a3b999694d
Florent Cayré <florent@commown.coop>
2023-09-28 14:42:44 +0200

    Refactor test data before adding more tests

--- rental_fees/tests/common.py
+++ rental_fees/tests/common.py
@@ -15,7 +15,13 @@ def _set_date(entity, value, attr_name):
 class RentalFeesTC(DeviceAsAServiceTC):
     def setUp(self):
         super().setUp()
-        self.po = self.create_po_and_picking(("N/S 1", "N/S 2", "N/S 3"))
+
+        p1 = self.storable_product.product_variant_id
+        p2 = self.storable_product.copy({"name": "Other product"}).product_variant_id
+        serials = {p1: ("N/S 1", "N/S 2", "N/S 3"), p2: ("N/S 4", "N/S 5")}
+        prices = {p1: 200.0, p2: 300.0}
+        self.po = self.create_po_and_picking(serials, prices)
+
         self.fees_def = self.env["rental_fees.definition"].create(
             {
                 "name": "Test fees_def",
@@ -60,32 +66,40 @@ class RentalFeesTC(DeviceAsAServiceTC):
 
     def create_po_and_picking(
         self,
-        serials,
+        serials_by_product,
+        prices=None,
         partner=None,
-        product=None,
-        price_unit=200.0,
     ):
-        "Return a purchase order with a done picking, generating given serials"
+        """Return a purchase order with a 'done' picking, generating given serials
+
+        Given `serials_by_product` must be a dict of the form::
+            {<product.template>: [strings]}
+        """
+        prices = prices or {product: 200.0 for product in set(serials_by_product)}
         partner = partner or self.env.ref("base.res_partner_1")
-        product = product or self.storable_product.product_variant_id
 
         po = self.env["purchase.order"].create({"partner_id": partner.id})
-        po.order_line |= self.env["purchase.order.line"].create(
-            {
-                "name": product.name,
-                "product_id": product.id,
-                "product_qty": len(serials),
-                "product_uom": product.uom_id.id,
-                "price_unit": price_unit,
-                "date_planned": date(2021, 1, 1),
-                "order_id": po.id,
-            }
-        )
+        for product, serials in serials_by_product.items():
+            po.order_line |= self.env["purchase.order.line"].create(
+                {
+                    "name": product.name,
+                    "product_id": product.id,
+                    "product_qty": len(serials),
+                    "product_uom": product.uom_id.id,
+                    "price_unit": prices[product],
+                    "date_planned": date(2021, 1, 1),
+                    "order_id": po.id,
+                }
+            )
         po.button_confirm()
 
-        move_lines = po.picking_ids.move_lines.move_line_ids
-        for lot_name, move_line in zip(serials, move_lines):
-            move_line.update({"lot_name": lot_name, "qty_done": 1})
+        for product, serials in serials_by_product.items():
+            move_lines = po.picking_ids.move_line_ids.filtered(
+                lambda ml: ml.product_id == product
+            )
+            for lot_name, move_line in zip(serials, move_lines):
+                move_line.update({"lot_name": lot_name, "qty_done": 1})
+
         po.picking_ids.button_validate()
 
         for move in po.picking_ids.move_lines:
