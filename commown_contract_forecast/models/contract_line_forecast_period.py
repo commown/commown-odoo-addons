68f752d22b663d5cdaacc03068a14b615665089a
Florent Cayré <florent@commown.coop>
2023-10-17 15:48:49 +0200

    Add market and product_range fields to contract.line.forecast.period
    These fields will be useful for forecast analysis and management
    decision making.

--- commown_contract_forecast/models/contract_line_forecast_period.py
+++ commown_contract_forecast/models/contract_line_forecast_period.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ContractLineForecastPeriod(models.Model):
@@ -13,3 +13,33 @@ class ContractLineForecastPeriod(models.Model):
         store=True,
         index=True,
     )
+
+    market = fields.Selection(
+        [("B2C", "B2C"), ("B2B", "B2B")],
+        string="Market",
+        readonly=True,
+        compute="_compute_market_and_product_range",
+        store=True,
+        index=True,
+    )
+
+    product_range = fields.Char(
+        string="Product range",
+        readonly=True,
+        compute="_compute_market_and_product_range",
+        store=True,
+        index=True,
+    )
+
+    @api.depends("contract_line_id")
+    def _compute_market_and_product_range(self):
+        for record in self:
+            ct_name = record.contract_line_id.contract_id.contract_template_id.name
+            if ct_name:
+                name_data = ct_name.split("/", 2)
+                if len(name_data) >= 2:
+                    record.product_range = name_data[0]
+                    record.market = "B2C" if name_data[1] == "B2C" else "B2B"
+                    continue
+            record.product_range = False
+            record.market = False
