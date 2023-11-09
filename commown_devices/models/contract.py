088ad037238528150b3a601ced90dc92ea6496af
louck <luc.parent@commown.coop>
2023-11-09 11:44:25 +0100

    Remove unused function

--- commown_devices/models/contract.py
+++ commown_devices/models/contract.py
@@ -129,31 +129,3 @@ class Contract(models.Model):
                 date or fields.Datetime.now(),
             )
         return new_moves
-
-    @api.multi
-    def stock_at_date(self, date=None):
-        "Return the lots at partner's location at the given date"
-        self.ensure_one()
-
-        if date is None:
-            date = fields.Datetime.now()
-
-        move_lines = self.env["stock.move.line"].search(
-            [
-                ("picking_id.contract_id", "=", self.id),
-                ("date", "<=", date),
-                ("state", "=", "done"),
-            ],
-            order="date ASC",
-        )
-
-        lot_ids = OrderedDict()
-        partner_loc = self.partner_id.get_or_create_customer_location()
-        for m in move_lines:
-            lot = m.lot_id
-            lot_ids.setdefault(lot.id, 0)
-            lot_ids[lot.id] += m.location_dest_id == partner_loc and 1 or -1
-
-        return self.env["stock.production.lot"].browse(
-            [l_id for (l_id, total) in list(lot_ids.items()) if total > 0]
-        )
