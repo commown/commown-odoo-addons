4e53be408ee89754ea9330fa58c9051f70aeeff3
Florent Cayré <florent@commown.coop>
2023-11-09 12:01:19 +0100

    Replace ugly metadata.py files by ST items and ST categories in the DB

--- commown_self_troubleshooting/data/GS/meta_data.py
+++
@@ -1,7 +0,0 @@
-{
-    "contract_domain": base_domain
-    + [
-        ("contract_template_id.name", "like", "GS%"),
-        ("contract_line_ids.sale_order_line_id.name", "ilike", "%day%"),
-    ],
-}
