976ad0b82ada3a19da6543fbf8f5d2e4ca280253
Florent Cayré <florent@commown.coop>
2023-11-14 19:28:42 +0100

    Mute odoo.sql_db logger in tests that check sql constraints

--- commown_shipping/tests/test_res_partner.py
+++ commown_shipping/tests/test_res_partner.py
@@ -1,14 +1,17 @@
 from psycopg2 import IntegrityError
 
 from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
 
 
 class ResPartnerTC(TransactionCase):
+    @mute_logger("odoo.sql_db")
     def test_street_length_low_level(self):
         with self.assertRaises(IntegrityError) as err:
             partner = self.env["res.partner"].create({"name": "T", "street": "a" * 36})
         self.assertIn("res_partner_street_max_size_colissimo", err.exception.pgerror)
 
+    @mute_logger("odoo.sql_db")
     def test_street2_length_low_level(self):
         with self.assertRaises(IntegrityError) as err:
             partner = self.env["res.partner"].create({"name": "T", "street2": "a" * 36})
