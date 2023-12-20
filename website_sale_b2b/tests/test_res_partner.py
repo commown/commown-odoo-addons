8b019a4bcdeabeee22e8490dc23de57d7005e0ae
Florent Cayré <florent@commown.coop>
2023-12-20 06:32:22 +0100

    Add a partner action to create their own intermediate company in their current one
    This is especially useful to create french CAE structures, where each
    employee is also an independant entrepreneur.

--- website_sale_b2b/tests/test_res_partner.py
+++ website_sale_b2b/tests/test_res_partner.py
@@ -1,3 +1,5 @@
+from odoo.exceptions import UserError
+
 from .common import RentedQuantityTC
 
 
@@ -8,3 +10,32 @@ class ResPartnerTC(RentedQuantityTC):
         self.assertEqual(rented_quantity(product_template=self.fp2), 1)
         self.assertEqual(rented_quantity(product_category=self.cat_fp), 2)
         self.assertEqual(rented_quantity(product_category=self.cat_fp_premium), 1)
+
+    def test_action_create_intermediate_company_error(self):
+        comp = self.env["res.partner"].create({"name": "company", "is_company": True})
+        c1 = self.env["res.partner"].create({"name": "c1"})
+        c2 = self.env["res.partner"].create({"name": "c2", "parent_id": comp.id})
+        c3 = self.env["res.partner"].create({"name": "c3"})
+
+        with self.assertRaises(UserError) as err:
+            (c1 | c2 | c3).action_create_intermediate_company()
+
+        self.assertEqual(
+            err.exception.name,
+            "Partners not suitable for intermediate company creation:"
+            "\n- c1 (id %d)\n- c3 (id %d)" % (c1.id, c3.id),
+        )
+
+    def test_action_create_intermediate_company_ok(self):
+        comp = self.env["res.partner"].create({"name": "Company", "is_company": True})
+        c1 = self.env["res.partner"].create({"name": "c1", "parent_id": comp.id})
+        c2 = self.env["res.partner"].create({"name": "c2", "parent_id": comp.id})
+
+        (c1 | c2).action_create_intermediate_company()
+
+        self.assertEqual(c1.parent_id.parent_id, comp)
+        self.assertEqual(c2.parent_id.parent_id, comp)
+        self.assertTrue(c1.parent_id.is_company)
+        self.assertTrue(c2.parent_id.is_company)
+        self.assertEqual(c1.parent_id.name, "c1 (indep. - Company)")
+        self.assertEqual(c2.parent_id.name, "c2 (indep. - Company)")
