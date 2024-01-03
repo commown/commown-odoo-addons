81d5ac00b363be5905e28bba6df7493c18d49294
Florent Cayré <florent@commown.coop>
2024-01-03 17:27:05 +0100

    Add dependency of commown_shipping to commown_contractual_issue
    The commercial_partner_id field of project task is otherwise missing
    and the module cannot install individually.

--- commown_shipping/__manifest__.py
+++ commown_shipping/__manifest__.py
@@ -7,6 +7,7 @@
     "license": "AGPL-3",
     "website": "https://commown.coop",
     "depends": [
+        "commown_contractual_issue",
         "base_automation",
         "sale",
         "website",
