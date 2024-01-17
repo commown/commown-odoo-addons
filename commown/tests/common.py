2960690388b3942f84896d4b99b5682767c5a246
Florent Cayré <florent@commown.coop>
2024-01-17 22:42:58 +0100

    Factor-out a test class to easily create http portal-related tests

--- commown/tests/common.py
+++ commown/tests/common.py
@@ -1,7 +1,15 @@
 import os
 from contextlib import contextmanager
 
+from lxml import html
+from werkzeug.test import Client
+from werkzeug.wrappers import BaseResponse
+
+from odoo.service import wsgi_server
+from odoo.tests.common import get_db_name
+
 from odoo.addons.payment_token_uniquify.tests.common import PaymentTokenUniquifyTC
+from odoo.addons.product_rental.tests.common import MockedEmptySessionMixin
 from odoo.addons.server_environment import server_env
 from odoo.addons.server_environment.models import server_env_mixin
 
@@ -89,3 +97,54 @@ class ContractRelatedPaymentTokenUniquifyTC(PaymentTokenUniquifyTC):
                 ],
             }
         )
+
+
+class PortalMixin(MockedEmptySessionMixin):
+    def setUp(self):
+        super().setUp()
+        self.partner = self.env.ref("base.partner_demo_portal")
+        self.partner.signup_prepare()
+        self.env.cr.commit()
+        self.werkzeug_environ = {"REMOTE_ADDR": "127.0.0.1"}
+        self.headers = {}
+
+    def get_page(self, test_client, path, **data):
+        "Return an lxml doc obtained from the html at given url path"
+        response = test_client.get(
+            path,
+            query_string=data,
+            follow_redirects=True,
+            headers=self.headers,
+        )
+        self.assertEqual(response.status_code, 200, " - ".join((path, response.status)))
+        return html.fromstring(response.data)
+
+    def get_form(self, test_client, path, **data):
+        "Get given page and return a name: value dict of its inputs and selects"
+        page = self.get_page(test_client, path, **data)
+        form = {n.get("name"): n.get("value") for n in page.xpath("//input")}
+        for select in page.xpath("//select"):
+            form[select.get("name")] = select.xpath("string(option[@selected]/@value)")
+        return form
+
+    def portal_client(self):
+        user = self.partner.user_ids.ensure_one()
+        test_client = Client(wsgi_server.application, BaseResponse)
+
+        login_form = self.get_form(test_client, "/web/login/", db=get_db_name())
+        login_form.update(
+            {
+                "login": user.login,
+                "password": "portal",
+                "redirect": "/my/account",
+            }
+        )
+        response = test_client.post(
+            "/web/login/",
+            data=login_form,
+            environ_base=self.werkzeug_environ,
+            headers=self.headers,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("/my/account", str(response.data))
+        return test_client
