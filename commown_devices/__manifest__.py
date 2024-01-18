a34282b77755a4681e5181d776028f4f338f3614
louck <luc.parent@commown.coop>
2024-01-18 11:34:35 +0100

    Add a wizard that alows linking po_lines to invoices_lines

--- commown_devices/__manifest__.py
+++ commown_devices/__manifest__.py
@@ -10,16 +10,19 @@
     "license": "AGPL-3",
     "website": "https://commown.coop",
     "depends": [
+        "account",
         "commown_contractual_issue",
         "commown_shipping",
         "commown_lead_risk_analysis",
         "product_rental",
+        "purchase",
         "stock",
     ],
     "data": [
         "data/action_crm_lead.xml",
         "data/action_picking.xml",
         "data/action_project_task.xml",
+        "data/action_purchase_order.xml",
         "data/project_task.xml",
         "data/stock_location.xml",
         "security/ir.model.access.csv",
@@ -30,6 +33,7 @@
         "views/project_task_type.xml",
         "views/stock_picking.xml",
         "views/wizard_crm_lead_picking.xml",
+        "views/wizard_link_po_invoice.xml",
         "views/wizard_project_task_picking.xml",
     ],
     "demo": [
