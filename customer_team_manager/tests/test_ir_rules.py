5d2f4d2f3598248ce3d7a15f94d769dfd11459bd
Florent Cayré <florent@commown.coop>
2024-01-25 21:35:07 +0100

    Restrict invoice-related access rules for portal users
    Portal users now see:
    - invoices that they directly follow
    - if in the group_customer_accounting group, all invoices of their company

--- customer_team_manager/tests/test_ir_rules.py
+++ customer_team_manager/tests/test_ir_rules.py
@@ -1,3 +1,5 @@
+from odoo.tests import SavepointCase
+
 from .common import CustomerTeamAbstractTC
 
 
@@ -31,10 +33,8 @@ class TeamAndEmployeeIrRulesTC(CustomerTeamAbstractTC):
         self.assertEqual(t_model.sudo(admin_user2.id).search([]), team2)
 
     def test_see_own_employees_only(self):
-        e_model = self.env["customer_team_manager.employee"]
-
         admin_user1 = self.user
-        admin_empl1 = e_model.search([("partner.user_ids", "=", admin_user1.id)])
+        admin_empl1 = admin_user1.partner_id.get_employees()
         empl1 = self.create_employee(
             firstname="F1",
             lastname="L1",
@@ -43,7 +43,7 @@ class TeamAndEmployeeIrRulesTC(CustomerTeamAbstractTC):
         )
 
         admin_user2 = self.user2
-        admin_empl2 = e_model.search([("partner.user_ids", "=", admin_user2.id)])
+        admin_empl2 = admin_user2.partner_id.get_employees()
         empl2 = self.create_employee(
             firstname="F2",
             lastname="L2",
@@ -51,5 +51,60 @@ class TeamAndEmployeeIrRulesTC(CustomerTeamAbstractTC):
             sudo_as=admin_user2,
         )
 
+        e_model = self.env["customer_team_manager.employee"]
         self.assertEqual(e_model.sudo(admin_user1.id).search([]), admin_empl1 | empl1)
         self.assertEqual(e_model.sudo(admin_user2.id).search([]), admin_empl2 | empl2)
+
+
+class PortalInvoiceIrRulesTC(SavepointCase):
+    "Test class for portal user invoice-related access rules"
+
+    def setUp(self):
+        super().setUp()
+        self.inv = self.env.ref("l10n_generic_coa.demo_invoice_1")
+
+        partner1 = self.inv.partner_id.child_ids[0]
+        self.user1 = self._give_portal_access(partner1)
+
+        partner2 = self.inv.partner_id.child_ids[1]
+        self.user2 = self._give_portal_access(partner2)
+
+        self.assertNotIn(partner1, self.inv.message_partner_ids)
+        self.assertNotIn(partner2, self.inv.message_partner_ids)
+
+    def _give_portal_access(self, partner):
+        model = self.env["portal.wizard"].with_context(active_ids=[partner.id])
+        portal_wizard = model.sudo().create({})
+        portal_wizard.user_ids.update({"in_portal": True})
+        portal_wizard.action_apply()
+        self.assertTrue(partner.user_ids)
+        return partner.user_ids[0]
+
+    def seen_invoices(self, user):
+        return self.env["account.invoice"].sudo(user).search([])
+
+    def follow_invoice(self, partner):
+        self.env["mail.followers"].create(
+            {
+                "res_id": self.inv.id,
+                "res_model": self.inv._name,
+                "partner_id": partner.id,
+            }
+        )
+
+    def test_directly_followed_invoice(self):
+        "Portal users who follow an invoice directly must have read access to it"
+        self.assertNotIn(self.inv, self.seen_invoices(self.user1))
+
+        self.follow_invoice(self.user1.partner_id)
+        self.assertIn(self.inv, self.seen_invoices(self.user1))
+
+    def test_in_accounting_group(self):
+        self.assertNotIn(self.inv, self.seen_invoices(self.user1))
+        self.assertNotIn(self.inv, self.seen_invoices(self.user2))
+
+        accounting_grp = self.env.ref("customer_team_manager.group_customer_accounting")
+        accounting_grp.users |= self.user2
+
+        self.assertNotIn(self.inv, self.seen_invoices(self.user1))
+        self.assertIn(self.inv, self.seen_invoices(self.user2))
