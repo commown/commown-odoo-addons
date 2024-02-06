80cc660906e2269004faf435adac1d97b607cbf6
Florent Cayré <florent@commown.coop>
2024-02-06 12:30:30 +0100

    Improve the create employee action
    ... to allow calling it with partner which already have an employee without crashing
    and emit an information notification summary of what was actually created and what
    was pre-existing.

--- customer_team_manager/models/res_partner.py
+++ customer_team_manager/models/res_partner.py
@@ -9,13 +9,17 @@ class ResPartner(models.Model):
         if admin and len(self) != 1:
             raise UserError(_("Cannot create more than one admin at once!"))
 
+        empl_model = self.env["customer_team_manager.employee"]
+        already_exist = empl_model.search([("partner", "in", self.ids)])
+        result = empl_model
+
         ref = self.env.ref
         group_customer_admin = ref("customer_team_manager.group_customer_admin")
         admin_role = ref("customer_team_manager.customer_role_admin")
         user_role = ref("customer_team_manager.customer_role_user")
 
         fields = ["firstname", "lastname", "email", "phone"]
-        for partner in self:
+        for partner in self - already_exist.mapped("partner"):
             if partner.type != "contact":
                 raise UserError(
                     _("Partner '%s' (id %d) is of type '%s', not 'contact'")
@@ -44,8 +48,32 @@ class ResPartner(models.Model):
                     "company": partner.commercial_partner_id.id,
                 }
             )
-            self.env["customer_team_manager.employee"].create(attrs)
+
+            result |= empl_model.create(attrs)
+
+        def employees_decr(title, employees):
+            result = [title]
+            for employee in employees:
+                roles = ", ".join(employee.roles.mapped("name"))
+                result.append("%s (%s)" % (employee.display_name, roles))
+            return result
+
+        msg = ""
+        sep = "<br>- "
+        if result:
+            msg += sep.join(
+                employees_decr(_("<b>Created %d employees:</b>") % len(result), result)
+            )
+        else:
+            msg += _("<b>No employee created!</b>")
+
+        if already_exist:
+            msg += "<br><br>" + sep.join(
+                employees_decr(_("<b>Already existing employee(s):</b>"), already_exist)
+            )
+
+        self.env.user.notify_info(sticky=True, message=msg)
 
     def get_employees(self):
-        employee_model = self.env["customer_team_manager.employee"]
-        return employee_model.search([("partner", "in", self.ids)])
+        empl_model = self.env["customer_team_manager.employee"]
+        return empl_model.search([("partner", "in", self.ids)])
