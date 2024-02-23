84cac22ac40b82f1be11456f9219836e9a554f2d
Florent Cayré <florent@commown.coop>
2024-02-23 18:54:55 +0100

    Create the commown_project module

---
+++ commown_project/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2024-today: Commown (https://commown.coop)
+# @author: Florent Cayré
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Commown Project",
+    "summary": "Implement Commown's policy for project management",
+    "category": "Project",
+    "version": "12.0.1.0.0",
+    "author": "Commown SCIC",
+    "license": "AGPL-3",
+    "website": "https://commown.coop",
+    "development_status": "Alpha",
+    "maintainers": ["fcayre"],
+    "installable": True,
+}
