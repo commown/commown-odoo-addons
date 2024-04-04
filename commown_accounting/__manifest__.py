c2acd569d0ae3451509c2ebb8df798345f6489e5
louck <luc.parent@commown.coop>
2024-04-04 15:43:51 +0200

    Create a module for commown accounting and add a view for loan lines

---
+++ commown_accounting/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2024: Commown (https://commown.coop)
+# @author: Luc Parent
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Commown accounting",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.0",
+    "author": "Commown SCIC",
+    "license": "AGPL-3",
+    "website": "https://commown.coop",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_loan_line.xml",
+    ],
+    "demo": [],
+    "installable": True,
+}
