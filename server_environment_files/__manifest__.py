7196dbe1c680302ca47123a606ab59e24a08e250
louck <luc.parent@commown.coop>
2024-04-24 13:30:53 +0200

    Create server environement file and add css style depending on environement.
    We created empty base.conf file so this module can also be used as it is
    intented, changing configurations depending on running_env if needed.

---
+++ server_environment_files/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2024: Commown (https://commown.coop)
+# @author: Luc Parent
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Server configuration environment files for Commown",
+    "category": "Tools",
+    "version": "12.0.1.0.1",
+    "author": "Commown SCIC",
+    "license": "AGPL-3",
+    "website": "https://commown.coop",
+    "depends": ["server_environment", "web"],
+    "data": ["views/template.xml"],
+    "installable": True,
+    "active": False,
+}
