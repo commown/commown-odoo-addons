c197303cd53ef4b26f731f74282f535a7beb0c88
louck <luc.parent@commown.coop>
2024-08-22 18:14:37 +0200

    Set show_all_move_line default to True

--- commown_devices/tests/test_contract.py
+++ commown_devices/tests/test_contract.py
@@ -79,10 +79,10 @@ class ContractTC(SavepointCase):
         self.assertEqual(self.contract.lot_nb, 0)
 
     def test_compute_move_lines_view_ids(self):
-        self.assertFalse(self.contract.show_all_view_move_lines)
-        self.assertEqual(self.contract.move_line_view_ids, self.move_line_with_lot)
+        self.assertTrue(self.contract.show_all_view_move_lines)
+        self.assertEqual(self.contract.move_line_view_ids, self.all_move_lines)
 
-        self.contract.show_all_view_move_lines = True
+        self.contract.show_all_view_move_lines = False
         self.contract._compute_move_line_view_ids()
 
-        self.assertEqual(self.contract.move_line_view_ids, self.all_move_lines)
+        self.assertEqual(self.contract.move_line_view_ids, self.move_line_with_lot)
