3c4e7727469d39e94ea4ef8088122b1b67aaf7bd
louck <luc.parent@commown.coop>
2024-09-04 10:41:43 +0200

    Add a module to define commown user groups

---
+++ commown_user_groups/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2022-today: Commown (https://commown.coop)
+# @author: Luc Parent
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "Commown user groups",
+    "summary": "Definition of Commown's user groups",
+    "category": "User types",
+    "version": "12.0.1.0.1",
+    "author": "Commown SCIC",
+    "license": "AGPL-3",
+    "website": "https://commown.coop",
+    "depends": ["base"],
+    "data": ["data/res_groups.xml"],
+    "installable": True,
+}
