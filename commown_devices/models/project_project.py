3259022f445d449b4aee8777d9dff7831089777f
louck <luc.parent@commown.coop>
2024-09-25 16:50:19 +0200

    Copy the move line view from the contracts to the project tasks
    The move lines displayed on project tasks are filtered so only the one
    that comes from the current tasks are shown.
    Also add the corresponding configuration on project.

--- commown_devices/models/project_project.py
+++ commown_devices/models/project_project.py
@@ -5,3 +5,5 @@ class Project(models.Model):
     _inherit = "project.project"
 
     device_tracking = fields.Boolean("Use for device tracking?", default=False)
+
+    show_related_move_lines = fields.Boolean("Show related move lines?", default=False)
