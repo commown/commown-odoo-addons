3893df98536d4253cb6a9cb9cd3c660164fc3ffa
Florent Cayré <florent@commown.coop>
2024-10-04 06:31:47 +0200

    [tests] Fix spurious warnings in tests
    ... either by muting some loggers or by changing the test code slightly

--- commown_shipping/tests/test_res_partner.py
+++ commown_shipping/tests/test_res_partner.py
@@ -12,7 +12,10 @@ class ResPartnerContraintsTC(SavepointCase):
 
     def test_street_length_low_level(self):
         with self.assertRaises(IntegrityError) as err:
-            partner = self.env["res.partner"].create({"name": "T", "street": "a" * 36})
+            with mute_logger("odoo.sql_db"):
+                partner = self.env["res.partner"].create(
+                    {"name": "T", "street": "a" * 36}
+                )
         self.assertIn("res_partner_street_max_size_colissimo", err.exception.pgerror)
 
     def test_street2_length_low_level(self):
