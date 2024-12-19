23bfae2b15db96ca8e78c99dcc19408ae0ac5017
Lena MONTENOT <lena.montenot@gmail.com>
2024-12-19 11:53:53 +0100

    Added Crosscall test, abeit still non-functionnal

--- commown_self_troubleshooting/static/src/self_troubleshooting_tour.js
+++ commown_self_troubleshooting/static/src/self_troubleshooting_tour.js
@@ -401,6 +401,55 @@ odoo.define("commown_self_troubleshooting.tour_fp2_battery", function(require) {
   );
 
   tour.register(
+    "commown_self_troubleshooting_need_new_screen_for_crosscall",
+    { url : "/my "},
+    [
+      {
+        content: "Go to the screen replacement page with a Crosscall contract",
+        trigger: 'a[href="/page/self-troubleshoot-crosscall-screen"]',
+      },
+      commonSteps.fillInContract,
+      commonSteps.gotoNextStep,
+      {
+        content: "Select yes on the presence of screen protection on this device",
+        trigger: "input[id=has_protection_yes]",
+        run: "text Non",
+      },
+      {
+        content: "Check step 2 nav link is not disabled",
+        trigger: "#smartwizard a.nav-link:eq(2):not(.disabled)",
+        run: function() {},
+      },
+      {
+        content: "Check step 3 nav link is disabled",
+        trigger: "#smartwizard a.nav-link:eq(3).disabled",
+        run: function() {},
+      },
+      commonSteps.gotoNextStep,
+      {
+        content: "Select my display needs to be replaced",
+        trigger: "input[id=replace_screen_yes_step2]",
+        run: "text Mon écran doit être remplacé",
+      },
+      {
+        content: "Check step 4 nav link is disabled",
+        trigger: "#smartwizard a.nav-link:eq(4).disabled",
+        run: function() {},
+      },
+      {
+        content: "Check step 5 nav link is not disabled",
+        trigger: "#smartwizard a.nav-link:eq(5):not(.disabled)",
+        run: function() {},
+      },
+      commonSteps.gotoNextStep,
+      commonSteps.checkInputNamesMatchesUser,
+      commonSteps.gotoNextStep,
+      ...commonSteps.funcAddMoreInfo("text My display need to be replaced!"),
+      ...commonSteps.funcCreateAndCheckTicket("écran muni"),
+    ]
+  );
+
+  tour.register(
     "commown_self_troubleshooting_need_new_fairphone",
     { url: "/my" },
     [
--- commown_self_troubleshooting/tests/test_tour.py
+++ commown_self_troubleshooting/tests/test_tour.py
@@ -8,7 +8,6 @@ from odoo.tests.common import ChromeBrowser
 
 from odoo.addons.commown_devices.tests.common import DeviceAsAServiceTC
 
-
 @contextmanager
 def chrome_suppress_origin():
     old_open_websocket = ChromeBrowser._open_websocket
@@ -105,6 +104,12 @@ class TestPageSmartphone(TestPageTC):
     def test_smartphone_need_display_and_tool(self):
         self._run_tour(
             "commown_self_troubleshooting_smartphone_need_display_and_tool"
+        )
+    
+    def test_smartphone_need_new_screen_for_crosscall(self):
+        self.contract_name = "CC/B2C/Premium/12/2020-05"
+        self._run_tour(
+            "commown_self_troubleshooting_need_new_screen_for_crosscall"
         )
 
     def test_need_new_fairphone(self):
