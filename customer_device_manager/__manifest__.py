8606754e19c7d750360b3fd11a873fc3dd6683f5
logan <loganlenevez@gmail.com>
2025-02-14 12:12:49 +0100

    New empty module to help customers manage their devices

---
+++ customer_device_manager/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Commown <contact@commown.coop>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Customer device manager",
+    "summary": "Allow customers to assign devices to users",
+    "version": "12.0.1.0.1",
+    "development_status": "Alpha",
+    "category": "Manager customer",
+    "website": "https://commown.coop",
+    "author": "Commown SCIC",
+    "maintainers": ["loganSIO"],
+    "license": "AGPL-3",
+    "depends": [
+        # Commown modules
+        "customer_manager_base",
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+}
