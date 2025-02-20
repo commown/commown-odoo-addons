9cb30c701a5df2b6e5167765fd40e4dc717fde18
Florent Cayré <florent@commown.coop>
2025-02-20 14:12:15 +0100

    Add an action to add storable products on sales with sale-with-contract service products
    The added storable products are the one configured on the sold
    service(s) that must be a product of type "service" and a contract of
    "customer" stock_ownership as a property_contract_template_id.

--- commown_devices/models/sale_order.py
+++ commown_devices/models/sale_order.py
@@ -1,4 +1,5 @@
-from odoo import models
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
@@ -20,3 +21,32 @@ class SaleOrder(models.Model):
             dest_partner.property_stock_customer = dest_loc
 
         return super().action_confirm()
+
+    @api.multi
+    def action_add_services_storable_products(self):
+        "Action to add the storable products configured on sale-with-contract services"
+
+        self.ensure_one()
+        if self.state != "draft":
+            raise UserError(_("Sale must be draft to use this action."))
+
+        def _contract_sale_with_service(so_line):
+            return (
+                so_line.product_id.property_contract_template_id.stock_ownership
+                == "customer"
+            )
+
+        for so_line in self.order_line.filtered(_contract_sale_with_service):
+            storables = (
+                so_line.product_id.primary_storable_variant_id
+                | so_line.product_id.secondary_storable_variant_ids
+            )
+            for storable in storables:
+                new_so_line = self.env["sale.order.line"].create(
+                    {
+                        "order_id": self.id,
+                        "product_id": storable.id,
+                        "product_uom_qty": so_line.product_uom_qty,
+                    }
+                )
+                new_so_line.product_id_change()
