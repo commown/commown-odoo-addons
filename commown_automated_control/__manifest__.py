18ddcce07e8d7234f73bf7e646893dc32f5f6360
Florent Cayré <florent@commown.coop>
2025-02-28 14:14:47 +0100

    commown_automated_control 1.0.2

--- commown_automated_control/__manifest__.py
+++ commown_automated_control/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Commown automated controls",
     "category": "Technical",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.2",
     "author": "Commown SCIC",
     "license": "AGPL-3",
     "website": "https://commown.coop",
