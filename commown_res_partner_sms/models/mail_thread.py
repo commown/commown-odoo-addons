6b5ba21c067b33f610e876275a10ef34ca38a241
louck <luc.parent@commown.coop>
2025-03-13 17:04:52 +0100

    Refactor sms posting function

--- commown_res_partner_sms/models/mail_thread.py
+++ commown_res_partner_sms/models/mail_thread.py
@@ -4,15 +4,17 @@ from odoo import models
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def message_post_send_sms_html(self, template, record, *args, **kwargs):
-        """Send given mail template as sms, with given record partner language
+    def send_sms_from_template(self, template, record, *args, **kwargs):
+        """Send given template as sms, with given record partner language
 
         The template is rendered with record as the object variable.
         """
         assert template.model == record._name
         template = template.with_context(lang=record.partner_id.lang or "en_US")
-        html_message = self.env["mail.template"]._render_template(
-            template.body_html, template.model, record.id, post_process=True
+        body = self.env["sms.template"]._render_template(
+            template.body, record._name, [record.id]
+        )[record.id]
+        subtype_id = self.env["ir.model.data"]._xmlid_to_res_id("mail.mt_note")
+        return self.message_post(
+            message_type="sms", body=body, subtype_id=subtype_id, *args, **kwargs
         )
-        text_message = self.env["ir.fields.converter"].text_from_html(html_message)
-        return self.message_post_send_sms(text_message, *args, **kwargs)
--- commown_res_partner_sms/tests/test_mail_thread.py
+++ commown_res_partner_sms/tests/test_mail_thread.py
@@ -21,30 +21,32 @@ class MailThreadTC(SavepointCase):
         cls.loader.restore_registry()
         super().tearDownClass()
 
-    def test_message_post_send_sms_html(self):
+    def test_send_sms_from_template(self):
         mail_thread = self.env["mail.thread.test"].create({})
-        template = self.env["mail.template"].create(
-            {"body_html": r"<div>TEST message to ${object.name}</div>"}
-        )
-        # Use partner as the record to wich template apply
         user = self.env.ref("base.user_demo")
-        with self.assertRaises(AssertionError):
-            mail_thread.message_post_send_sms_html(
-                template, user, numbers=["0600070022"]
-            )
-
-        template.model = user._name
+        user_model = self.env["ir.model"]._get(user._name)
+        template = self.env["sms.template"].create(
+            {"body": r"TEST message to {{object.name}}", "model_id": user_model.id}
+        )
 
         message_num = len(mail_thread.message_ids)
         with requests_mock.Mocker() as rm:
             rm.get("https://www.ovh.com/cgi-bin/sms/http2sms.cgi", text="OK")
-            mail_thread.message_post_send_sms_html(
-                template, user, numbers=["0600070022"]
+            mail_thread.send_sms_from_template(
+                template, user, sms_numbers=["0600070022"]
             )
 
         self.assertEqual(len(mail_thread.message_ids), message_num + 1)
         sms = mail_thread.message_ids[0]
         self.assertEqual(sms.subtype_id, self.env.ref("mail.mt_note"))
 
-        expected_message = "<p>SMS message sent: TEST message to %s</p>" % user.name
+        expected_message = "<p>TEST message to %s</p>" % user.name
         self.assertEqual(expected_message, sms.body)
+
+        # Check raise when object model is different than template one
+        partner_model = self.env["ir.model"]._get(user.partner_id._name)
+        template.model_id = partner_model
+        with self.assertRaises(AssertionError):
+            mail_thread.send_sms_from_template(
+                template, user, sms_numbers=["0600070022"]
+            )
