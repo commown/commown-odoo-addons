54eab849b39b59416cbb4971ceb045c98f365519
logan <loganlenevez@gmail.com>
2025-03-14 11:57:37 +0100

    Create Device Assignment lifecycle
    Device assignments should not be created nor deleted by customers.
    So we create them when a device arrives at their company.
    And delete them when the device is returned in our stock.

--- customer_device_manager/models/__init__.py
+++ customer_device_manager/models/__init__.py
@@ -1,3 +1,4 @@
 from . import device_assignment
 from . import device_assignment_history
+from . import stock_move
 from . import stock_production_lot
---
+++ customer_device_manager/models/stock_move.py
@@ -0,0 +1,36 @@
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _set_lot_contract(self, lots, contract, **kwargs):
+        "Create Device Assignment on device expedition validation"
+        super()._set_lot_contract(lots, contract, **kwargs)
+
+        partner = contract.partner_id.commercial_partner_id
+        if partner.is_company:
+            assign_model = self.env["customer_device_manager.device_assignment"]
+            hist_model = self.env["customer_device_manager.device_assignment_history"]
+            for lot in lots:
+                self.env["customer_device_manager.device_assignment"].create(
+                    {
+                        "device_id": lot.id,
+                        "partner_id": partner.id,
+                        "assignment_date": fields.Datetime.now(),
+                    }
+                )
+
+    def _unset_lot_contract(self, lots, contract, location_dest, **kwargs):
+        "Archive Device Assignment on device return validation"
+        super()._unset_lot_contract(lots, contract, location_dest, **kwargs)
+
+        partner = contract.partner_id.commercial_partner_id
+        assignments = self.env["customer_device_manager.device_assignment"].search(
+            [
+                ("device_id", "in", lots.ids),
+                ("partner_id", "child_of", partner.id),
+                ("active", "=", True),
+            ]
+        )
+        assignments.update({"active": False})
