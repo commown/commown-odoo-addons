b69e6d72494b94ee502acdc8803c511250084052
louck <luc.parent@commown.coop>
2025-03-18 15:29:46 +0100

    [MIG] ir_attachment_lang: Migration to 16.0

--- ir_attachment_lang/__manifest__.py
+++ ir_attachment_lang/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Attachment language",
     "summary": "Add a language field to an attachment",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     # see https://odoo-community.org/page/development-status
     "development_status": "Alpha",
     "category": "Uncategorized",
