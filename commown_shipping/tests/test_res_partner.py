3e32626c8dd41d298903518e4b5ac6292711654f
louck <luc.parent@commown.coop>
2025-03-19 13:42:20 +0100

    [IMP] commown_shipping: pre-commit execution

--- commown_shipping/tests/test_res_partner.py
+++ commown_shipping/tests/test_res_partner.py
@@ -13,17 +13,13 @@ class ResPartnerContraintsTC(SavepointCase):
     def test_street_length_low_level(self):
         with self.assertRaises(IntegrityError) as err:
             with mute_logger("odoo.sql_db"):
-                self.env["res.partner"].create(
-                    {"name": "T", "street": "a" * 36}
-                )
+                self.env["res.partner"].create({"name": "T", "street": "a" * 36})
         self.assertIn("res_partner_street_max_size_colissimo", err.exception.pgerror)
 
     def test_street2_length_low_level(self):
         with self.assertRaises(IntegrityError) as err:
             with mute_logger("odoo.sql_db"):
-                self.env["res.partner"].create(
-                    {"name": "T", "street2": "a" * 36}
-                )
+                self.env["res.partner"].create({"name": "T", "street2": "a" * 36})
         self.assertIn("res_partner_street2_max_size_colissimo", err.exception.pgerror)
 
     def test_street_validation(self):
