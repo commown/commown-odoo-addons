0d78bf831696d9b6dcce0ceceb84a69f8f685f4c
Florent Cayré <florent@commown.coop>
2025-04-01 14:07:34 +0200

    Post-merge updates

--- scic/__manifest__.py
+++ scic/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "SCIC (Société Coopérative d'Intérêts Collectifs)",
     "category": "Custom",
     "summary": "Module that defines functionalities related to the SCIC companies",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "description": """
     SCIC
     ====
