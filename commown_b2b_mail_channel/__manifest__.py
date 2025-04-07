bc1c622e7a080cf166ba9bd3bd4a66c2276e8f07
louck <luc.parent@commown.coop>
2025-04-07 12:17:50 +0200

    [IMP] commown_b2b_mail_channel: black, isort, prettier

--- commown_b2b_mail_channel/__manifest__.py
+++ commown_b2b_mail_channel/__manifest__.py
@@ -10,7 +10,7 @@
     "version": "12.0.1.0.2",
     "author": "Commown SCIC",
     "license": "AGPL-3",
-    "website": "https://commown.coop",
+    "website": "https://github.com/commown/commown-odoo-addons",
     "depends": ["commown_user_roles", "contract", "mail"],
     "data": [
         "data/ir_config_parameter.xml",
