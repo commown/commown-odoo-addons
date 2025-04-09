cf1fb459b9b371453a8b4ca318bf8bee4242d78f
Lena MONTENOT <lena.montenot@gmail.com>
2025-04-09 11:27:58 +0200

    [IMP] commown_ergonomy_asset: black, isort, prettier

--- commown_ergonomy_asset/__manifest__.py
+++ commown_ergonomy_asset/__manifest__.py
@@ -1,14 +1,13 @@
-# Copyright 2020 Commown SCIC SAS (https://commown.fr)
+# Copyright 2020 Commown SCIC (https://commown.coop)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Commown ergonomy: assets",
     "category": "Accounting & Finance",
     "summary": "Assets ergonomy improvements for Commown",
     "version": "12.0.1.0.0",
-    "description": "Assets ergonomy improvements for Commown",
-    "author": "Commown SCIC SAS",
+    "author": "Commown SCIC",
     "license": "AGPL-3",
-    "website": "https://commown.coop",
+    "website": "https://github.com/commown/commown-odoo-addons",
     "depends": [
         "account_asset_management",
     ],
