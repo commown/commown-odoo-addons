cf8f016e224e1c92a7f54febeb62e8154d47a6ad
Lena MONTENOT <lena.montenot@gmail.com>
2025-04-15 11:59:24 +0200

    [MIG] commown_accounting: Migration to 16.0

--- commown_accounting/__manifest__.py
+++ commown_accounting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Commown accounting",
     "category": "Accounting & Finance",
-    "version": "12.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Commown SCIC",
     "license": "AGPL-3",
     "website": "https://github.com/commown/commown-odoo-addons",
