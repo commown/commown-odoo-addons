02bf705b2312addcef9501b9950309cfbd9166de
Lena MONTENOT <lena.montenot@gmail.com>
2025-04-16 10:44:52 +0200

    Changed _track_template behavior and argument name
    In Odoo 12.0, _track_template takes as an argument tracking, a dict containing all changed tracked fields of modified records was passed, meaning you had to get the changes list corresponding to the current record.
    
    However, after the following commit, _track_template now takes as argument changes, a list containing changed tracked fields for the current record.
    https://github.com/odoo/odoo/commit/92e9e84b63a266b39d556c1db1d336373dc8b303

--- crm_stage_email_template/models/crm_lead.py
+++ crm_stage_email_template/models/crm_lead.py
@@ -4,10 +4,10 @@ from odoo import models
 class CrmLead(models.Model):
     _inherit = "crm.lead"
 
-    def _track_template(self, tracking):
-        res = super()._track_template(tracking)
+    def _track_template(self, changes):
+        res = super()._track_template(changes)
         test_lead = self[0]
-        changes, tracking_value_ids = tracking[test_lead.id]
+
         if "stage_id" in changes and test_lead.stage_id.mail_template_id:
             res["stage_id"] = test_lead.stage_id.mail_template_id, {}
         return res
