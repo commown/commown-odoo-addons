33f91c0173d237cb359bc3fe02762c22f017c850
louck <luc.parent@commown.coop>
2025-04-24 16:00:53 +0200

    Override mail message create method in batch

--- product_rental/models/mail_message.py
+++ product_rental/models/mail_message.py
@@ -4,8 +4,8 @@ from odoo import api, models
 class Message(models.Model):
     _inherit = "mail.message"
 
-    @api.model
-    def create(self, values):
+    @api.model_create_multi
+    def create(self, vals_list):
         """Circumvent an odoo bug
 
         The default_attachment_ids context key is ignored in standard odoo code,
@@ -15,6 +15,10 @@ class Message(models.Model):
         See https://github.com/odoo/odoo/blob/12.0/addons/mail/models/mail_message.py#L961
         """
 
-        if "attachment_ids" not in values and "default_attachment_ids" in self._context:
-            values["attachment_ids"] = self._context["default_attachment_ids"]
-        return super().create(values)
+        for values in vals_list:
+            if (
+                "attachment_ids" not in values
+                and "default_attachment_ids" in self._context
+            ):
+                values["attachment_ids"] = self._context["default_attachment_ids"]
+        return super().create(vals_list)
