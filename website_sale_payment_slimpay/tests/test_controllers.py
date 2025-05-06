91f0f44c22d25b6965697deac7731b20c3879496
Florent Cayré <florent@commown.coop>
2025-05-06 19:42:14 +0200

    Add tests for improved coverage

--- website_sale_payment_slimpay/tests/test_controllers.py
+++ website_sale_payment_slimpay/tests/test_controllers.py
@@ -1,3 +1,7 @@
+from lxml import html
+
+import odoo.http
+
 from .common import SlimpayControllersTC
 
 
@@ -51,3 +55,44 @@ class SlimpayPaymentControllersTC(SlimpayControllersTC):
         self.authenticate("portal", "portal")
         resp = self.simulate_feedback("non-existing-ref", assert_code=200)
         self.assertEqual(resp, "Incorrect transaction reference")
+
+    def test_checkout_form_validate(self):
+        partner = self.env.ref("base.partner_demo_portal")
+        self.assertEqual(partner.user_ids.login, "portal")
+
+        self.authenticate("portal", "portal")
+        self.add_product_to_user_cart()
+        so_id = odoo.http.root.session_store.get(self.session.sid)["sale_order_id"]
+
+        def _address(**kwargs):
+            kwargs["csrf_token"] = self.csrf_token(self.url_open("/shop/address").text)
+            return self.post("/shop/address", data=kwargs)
+
+        def _check_invalid(text, msg, *inputs):
+            self.assertIn(msg, text)
+            self.assertEqual(
+                html.fromstring(text).xpath(
+                    "//form//input[hasclass('is-invalid')]/@name"
+                ),
+                list(inputs),
+            )
+
+        page = html.fromstring(_address(partner_id=partner.id))
+        form_values = page.xpath("//form")[0].form_values()
+
+        base_data = {k: v for k, v in form_values if k in partner._fields}
+        base_data["submitted"] = "true"
+
+        # Billing address: partner_id is given
+        text = _address(**dict(base_data, firstname="", partner_id=partner.id))
+        _check_invalid(text, "Some required fields are empty.", "firstname")
+
+        invalid_data = dict(base_data, country_id=self.env.ref("base.fr").id, zip="0")
+        text = _address(**dict(invalid_data, partner_id=partner.id))
+        _check_invalid(text, "Incorrect zip code (should be 5 figures)", "zip")
+
+        # Shipping address: partner_id is not given
+        text = _address(**dict(base_data, firstname="Shipping"))
+        so = self.env["sale.order"].browse(so_id)
+        self.assertTrue(so.partner_shipping_id != so.partner_id)
+        self.assertEqual(so.partner_shipping_id.firstname, "Shipping")
