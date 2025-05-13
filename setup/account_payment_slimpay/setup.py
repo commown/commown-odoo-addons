29b549e8091175b80610cf973a48803da7e16136
Florent Cayré <florent@commown.coop>
2025-05-14 00:44:20 +0200

    Fix the hal_codec dependency using an override in setup.py

--- setup/account_payment_slimpay/setup.py
+++ setup/account_payment_slimpay/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'hal_codec': 'hal_codec @ git+https://github.com/fcayre/python-hal-codec.git@unique-link-keys',
+            },
+        },
+    },
 )
