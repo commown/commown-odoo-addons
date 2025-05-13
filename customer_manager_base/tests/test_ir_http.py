786d16d5ee930e39f6510d4854988c2ef56b51c9
Lena MONTENOT <lena.montenot@gmail.com>
2025-05-13 18:06:12 +0200

    [test] Changed variables to match more recent implementations

--- customer_manager_base/tests/test_ir_http.py
+++ customer_manager_base/tests/test_ir_http.py
@@ -2,9 +2,9 @@ import json
 
 from lxml import html
 from werkzeug.test import Client
-from werkzeug.wrappers import BaseResponse
+from werkzeug.wrappers import Response
 
-from odoo.service import wsgi_server
+from odoo import http
 from odoo.tests.common import HttpCase, get_db_name
 
 
@@ -41,7 +41,7 @@ class SessionInfoTC(HttpCase):
 
     def portal_client(self):
         user = self.partner.user_ids.ensure_one()
-        test_client = Client(wsgi_server.application, BaseResponse)
+        test_client = Client(http.root, Response)
 
         login_form = self.get_form(test_client, "/web/login/", db=get_db_name())
         login_form.update(
