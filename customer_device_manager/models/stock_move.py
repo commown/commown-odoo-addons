05eada966a8ff5f3d720c50aa99ba95825414098
logan <loganlenevez@gmail.com>
2025-05-16 11:50:30 +0200

    Improve stock_move to update same assignation if necessary instead of creating new one every time

--- customer_device_manager/models/stock_move.py
+++ customer_device_manager/models/stock_move.py
@@ -13,24 +13,57 @@ class StockMove(models.Model):
             assign_model = self.env["customer_device_manager.device_assignment"]
             hist_model = self.env["customer_device_manager.device_assignment_history"]
             for lot in lots:
-                self.env["customer_device_manager.device_assignment"].create(
-                    {
-                        "device_id": lot.id,
-                        "partner_id": partner.id,
-                        "assignment_date": fields.Datetime.now(),
-                    }
+                latest_assignment = assign_model.search(
+                    [
+                        ("device_id", "=", lot.id),
+                        ("partner_id.commercial_partner_id", "=", partner.id),
+                    ],
+                    limit=1,
                 )
+
+                if latest_assignment:
+                    hist_model.sudo().create(
+                        {
+                            "assignment_id": latest_assignment.id,
+                            "date": fields.Datetime.now(),
+                            "partner_id": latest_assignment.partner_id.id,
+                            "device_location": "at_customer",
+                        }
+                    )
+
+                else:
+                    assign_model.create(
+                        {
+                            "device_id": lot.id,
+                            "partner_id": partner.id,
+                            "assignment_date": fields.Datetime.now(),
+                            "device_location": "at_customer",
+                        }
+                    )
 
     def _unset_lot_contract(self, lots, contract, location_dest, **kwargs):
         "Archive Device Assignment on device return validation"
         super()._unset_lot_contract(lots, contract, location_dest, **kwargs)
 
         partner = contract.partner_id.commercial_partner_id
-        assignments = self.env["customer_device_manager.device_assignment"].search(
-            [
-                ("device_id", "in", lots.ids),
-                ("partner_id", "child_of", partner.id),
-                ("device_location", "=", "at_customer"),
-            ]
-        )
-        assignments.update({"device_location": "at_commown"})
+        for lot in lots:
+            assignment = self.env["customer_device_manager.device_assignment"].search(
+                [
+                    ("device_id", "=", lot.id),
+                    ("partner_id", "child_of", partner.id),
+                    ("device_location", "=", "at_customer"),
+                ],
+                limit=1,
+            )
+
+            if assignment:
+                self.env[
+                    "customer_device_manager.device_assignment_history"
+                ].sudo().create(
+                    {
+                        "assignment_id": assignment.id,
+                        "date": fields.Datetime.now(),
+                        "partner_id": assignment.partner_id.id,
+                        "device_location": "at_commown",
+                    }
+                )
--- customer_device_manager/tests/test_stock_move.py
+++ customer_device_manager/tests/test_stock_move.py
@@ -1,6 +1,7 @@
 from odoo.exceptions import AccessError
 from odoo.tests.common import SavepointCase
 
+from odoo.addons.commown_devices.models.common import do_new_transfer, internal_picking
 from odoo.addons.commown_devices.tests.common import create_lot_and_quant
 
 
@@ -51,51 +52,39 @@ class StockMoveTC(SavepointCase):
         self.internal_user = self.env.ref("base.group_user")
 
     def get_assignments(self, lot, company):
-        return self.env["customer_device_manager.device_assignment"].search(
-            [("device_id", "=", lot.id), ("partner_id", "=", company.id)],
+        """Find device assignments for a lot and company, including assignments to contacts of the company."""
+        assignments = self.env["customer_device_manager.device_assignment"].search(
+            [
+                ("device_id", "=", lot.id),
+            ]
+        )
+        return assignments.filtered(
+            lambda a: a.partner_id.commercial_partner_id
+            == company.commercial_partner_id
         )
 
-    def get_visible_assignments(self, lot, user):
-        "Return visible assignments for a given user"
-        return (
-            self.env["customer_device_manager.device_assignment"]
-            .sudo(user)
-            .search(
-                [
-                    ("device_id", "=", lot.id),
-                ],
-            )
-        )
+    def _send(self, lot, orig, dest):
+        moves = internal_picking(lot, {}, None, orig, dest, False)
+        picking = moves[0].picking_id
+        do_new_transfer(picking, picking.scheduled_date)
+        return picking
 
     def test_device_assignment_lifecycle(self):
         """
         Test the lifecycle of a device assignment:
-        - Check that the assignment is not visible to the customer and the internal user at the beginning.
         - Send the device.
-        - Check that the assignment is visible to the customer and the internal user.
         - Update the assignment as a customer assigner.
         - Receive the device.
-        - Check that the assignment is not visible anymore.
+        - Check that an history line is created at each step
         """
         # Check test prerequisite, no assignment exists
         self.assertFalse(self.get_assignments(self.lot, self.company))
-        self.assertFalse(self.get_visible_assignments(self.lot, self.customer_employee))
-
-        self.assertFalse(self.get_visible_assignments(self.lot, self.internal_user))
 
         self.contract.send_devices(self.lot, {}, do_transfer=True)
         assignment = self.get_assignments(self.lot, self.company)
         self.assertEqual(len(assignment), 1)
         self.assertEqual(assignment.device_location, "at_customer")
-
-        self.assertEqual(
-            len(self.get_visible_assignments(self.lot, self.customer_employee)), 1
-        )
-
-        self.assertEqual(
-            len(self.get_visible_assignments(self.lot, self.customer_device_assigner)),
-            1,
-        )
+        self.assertEqual(len(assignment.history_ids), 1)
 
         with self.assertRaises(AccessError):
             assignment.sudo(self.customer_employee).update(
@@ -111,8 +100,34 @@ class StockMoveTC(SavepointCase):
             }
         )
         self.assertEqual(assignment.assignment_notes, "Update as a customer assigner")
+        self.assertEqual(len(assignment.history_ids), 2)
 
         loc_to_check = self.env.ref("commown_devices.stock_location_devices_to_check")
         self.contract.receive_devices(self.lot, {}, loc_to_check, do_transfer=True)
 
         self.assertEqual(assignment.device_location, "at_commown")
+        self.assertEqual(len(assignment.history_ids), 3)
+
+        loc_repacked = self.env.ref(
+            "commown_devices.stock_repackaged_modules_and_accessories"
+        )
+        picking = self._send(self.lot, loc_to_check, loc_repacked)
+        self.assertEqual(picking.state, "done")
+
+        dest_loc_parent = self.env.ref(
+            "commown_devices.stock_location_available_for_rent"
+        )
+        dest_loc = self.env["stock.location"].search(
+            [
+                ("location_id", "=", dest_loc_parent.id),
+                ("usage", "=", "internal"),
+            ],
+            limit=1,
+        )
+
+        self.lot.grade_id = self.env.ref("commown_grade.grade_D1")
+        self.contract.send_devices(self.lot, {}, do_transfer=True)
+
+        self.assertEqual(assignment, self.get_assignments(self.lot, self.company))
+        self.assertEqual(assignment.device_location, "at_customer")
+        self.assertEqual(len(assignment.history_ids), 4)
