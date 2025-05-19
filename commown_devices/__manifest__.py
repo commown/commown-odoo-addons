78f17573519ac96f4c056335b398c30fa6c4f317
Florent Cayré <florent@commown.coop>
2025-05-19 13:55:49 +0200

    commown_devices 1.2.11

--- commown_devices/__manifest__.py
+++ commown_devices/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Commown devices",
     "category": "stock",
-    "version": "12.0.1.2.10",
+    "version": "12.0.1.2.11",
     "author": "Commown SCIC",
     "license": "AGPL-3",
     "website": "https://commown.coop",
