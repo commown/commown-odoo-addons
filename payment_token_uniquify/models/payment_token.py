f0ef582d6f51a88e940f4f32f881f8de4605bceb
Lena MONTENOT <lena.montenot@gmail.com>
2025-05-21 14:53:09 +0200

    Added batch create support for payment.token

--- payment_token_uniquify/models/payment_token.py
+++ payment_token_uniquify/models/payment_token.py
@@ -4,8 +4,11 @@ from odoo import api, models
 class PaymentToken(models.Model):
     _inherit = "payment.token"
 
-    @api.model
-    def create(self, vals):
-        res = super().create(vals)
-        res.provider_id.with_delay(max_retries=1).run_obsolete_token_actions(res)
-        return res
+    @api.model_create_multi
+    def create(self, vals_list):
+        tokens = super().create(vals_list)
+        for token in tokens:
+            token.provider_id.with_delay(max_retries=1).run_obsolete_token_actions(
+                token
+            )
+        return tokens
