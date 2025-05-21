f5aa807c60a79bf649e1ebf5687219bcf48e1503
Lena MONTENOT <lena.montenot@gmail.com>
2025-05-21 14:13:12 +0200

    Removed depreciated @api.returns decorator

--- payment_token_uniquify/models/payment_token.py
+++ payment_token_uniquify/models/payment_token.py
@@ -5,7 +5,6 @@ class PaymentToken(models.Model):
     _inherit = "payment.token"
 
     @api.model
-    @api.returns("self", lambda value: value.id)
     def create(self, vals):
         res = super().create(vals)
         res.acquirer_id.with_delay(max_retries=1).run_obsolete_token_actions(res)
