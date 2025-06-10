88287685f2c935dfabfb4c5493646657845a345d
Lena MONTENOT <lena.montenot@gmail.com>
2025-06-10 15:48:00 +0200

    Added optional argument to _action_launch_stock_rule overload

--- commown_devices/models/sale_order_line.py
+++ commown_devices/models/sale_order_line.py
@@ -4,10 +4,10 @@ from odoo import models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _action_launch_stock_rule(self):
+    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
         "Deactivate procurement/picking logic when the sale contains a contract"
 
         if self.mapped("order_id.order_line.product_id.property_contract_template_id"):
             return True
         else:
-            return super()._action_launch_stock_rule()
+            return super()._action_launch_stock_rule(previous_product_uom_qty)
