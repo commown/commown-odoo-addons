9f4bfe897ca81b49228eb86920468cbc53748f4a
Lena MONTENOT <lena.montenot@gmail.com>
2025-06-18 16:09:03 +0200

    [test] Changed depreciated WSGI application entrypoint

--- commown_self_troubleshooting/tests/test_pages.py
+++ commown_self_troubleshooting/tests/test_pages.py
@@ -6,7 +6,7 @@ from mock import patch
 from werkzeug.test import Client
 from werkzeug.wrappers import Response
 
-from odoo.service import wsgi_server
+from odoo import http
 from odoo.tests.common import HttpCase
 
 from ..models.res_partner import ResPartner
@@ -24,7 +24,7 @@ class PagesTC(HttpCase):
     def setUp(self):
         super(PagesTC, self).setUp()
         self.werkzeug_environ = {"REMOTE_ADDR": "127.0.0.1"}
-        self.test_client = Client(wsgi_server.application, Response)
+        self.test_client = Client(http.root, Response)
         self.password = b"portal"
         self.user = self.env.ref("base.demo_user0")
         self.dbname = self.env.cr.dbname
