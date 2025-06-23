a9aa656d5f90211d765f17113cae1729e3d086d0
Florent Cayré <florent@commown.coop>
2025-06-23 12:33:20 +0200

    Remove useless forecast related contract line method override
    In 12.0, updating contract date_start used to call contract line write
    twice, triggering forecast recomputation twice too. This is no longer
    the case, so the method overriden to circumvent this can be removed.

--- rental_fees/models/__init__.py
+++ rental_fees/models/__init__.py
@@ -1,6 +1,5 @@
 from . import account_move
 from . import account_move_line
-from . import contract_line
 from . import purchase_order
 from . import rental_fees_computation
 from . import rental_fees_definition
