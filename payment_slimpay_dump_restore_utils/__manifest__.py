1ef4cbbd4af19ebb8b55f8bf0e6f4bdcd975ecb9
Lena MONTENOT <lena.montenot@gmail.com>
2025-07-01 12:15:06 +0200

    [MIG] payment_slimpay_dump_restore_utils: Migration to 16.0

--- payment_slimpay_dump_restore_utils/__manifest__.py
+++ payment_slimpay_dump_restore_utils/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Payment Slimpay dump and restore utils",
     "category": "Website",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Commown SCIC",
     "license": "AGPL-3",
     "website": "https://github.com/commown/commown-odoo-addons",
