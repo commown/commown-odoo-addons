5522be004f119a342b87815a4ff84f56b1125d1d
Lena MONTENOT <lena.montenot@gmail.com>
2025-07-01 12:20:13 +0200

    Adapted to 'account.acquirer' -> 'account.provider' migration
    We also renamed variables for consistency.

--- payment_slimpay_dump_restore_utils/models/payment.py
+++ payment_slimpay_dump_restore_utils/models/payment.py
@@ -3,21 +3,21 @@ from odoo import api, models
 from .utils import dump_all_mandates, restore_all_missing_mandates
 
 
-class PaymentAcquirerSlimpayDumpRestore(models.Model):
-    _inherit = "payment.acquirer"
+class PaymentProviderSlimpayDumpRestore(models.Model):
+    _inherit = "payment.provider"
 
     @api.model
     def _slimpay_dump_all_mandates(
-        self, refresh=True, acquirer=None, mandates_fpath="/tmp/mandates.json", **params
+        self, refresh=True, provider=None, mandates_fpath="/tmp/mandates.json", **params
     ):
-        if acquirer is None:
-            acquirer = self.env.ref("account_payment_slimpay.payment_acquirer_slimpay")
-        dump_all_mandates(acquirer, refresh, mandates_fpath, **params)
+        if provider is None:
+            provider = self.env.ref("account_payment_slimpay.payment_provider_slimpay")
+        dump_all_mandates(provider, refresh, mandates_fpath, **params)
 
     @api.model
     def _slimpay_restore_mandates(
-        self, acquirer=None, mandates_fpath="/tmp/mandates.json", **params
+        self, provider=None, mandates_fpath="/tmp/mandates.json", **params
     ):
-        if acquirer is None:
-            acquirer = self.env.ref("account_payment_slimpay.payment_acquirer_slimpay")
-        restore_all_missing_mandates(acquirer, mandates_fpath, **params)
+        if provider is None:
+            provider = self.env.ref("account_payment_slimpay.payment_provider_slimpay")
+        restore_all_missing_mandates(provider, mandates_fpath, **params)
--- payment_slimpay_dump_restore_utils/models/utils.py
+++ payment_slimpay_dump_restore_utils/models/utils.py
@@ -12,22 +12,22 @@ _logger = logging.getLogger(__name__)
 # Low level helpers ###########################################################
 
 
-def mandate_doc_ref(acquirer, mandate_doc):
+def mandate_doc_ref(provider, mandate_doc):
     "Return the reference of the mandate supplied as an HAPI representation"
     return (mandate_doc["reference"], mandate_doc["id"])
 
 
-def get_partner(acquirer, mandate_doc):
-    client = acquirer.slimpay_client
+def get_partner(provider, mandate_doc):
+    client = provider.slimpay_client
     subscriber_url = mandate_doc[client.method_name("get-subscriber")].url
     pid = subscriber_url.rsplit("/", 1)[-1]
-    partner_model = acquirer.env["res.partner"]
+    partner_model = provider.env["res.partner"]
     if pid.isdigit():
         partner = partner_model.browse(int(pid))
     else:
-        partner = acquirer.env["res.partner"].search(
+        partner = provider.env["res.partner"].search(
             [
-                ("payment_token_id.acquirer_ref", "=", mandate_doc["id"]),
+                ("payment_token_id.provider_ref", "=", mandate_doc["id"]),
             ]
         )
         if len(partner) > 1:
@@ -36,12 +36,12 @@ def get_partner(acquirer, mandate_doc):
     return partner
 
 
-def mandate_doc_to_repr(acquirer, mandate_doc):
+def mandate_doc_to_repr(provider, mandate_doc):
     """Return a json representation of the supplied HAPI mandate doc that
     is suitable for creating a copy using the `create-mandates` HAPI call.
     """
-    partner = get_partner(acquirer, mandate_doc)
-    bank_account_doc = acquirer.slimpay_client.action(
+    partner = get_partner(provider, mandate_doc)
+    bank_account_doc = provider.slimpay_client.action(
         "GET", "get-bank-account", doc=mandate_doc
     )
     if partner:
@@ -59,18 +59,18 @@ def mandate_doc_to_repr(acquirer, mandate_doc):
         }
 
 
-def get_all_mandates_repr(acquirer, transformer_func, **params):
-    """Query Slimpay API's `acquirer` account for all mandates with
+def get_all_mandates_repr(provider, transformer_func, **params):
+    """Query Slimpay API's `provider` account for all mandates with
     optional search criteria `params` and return them after applying
     the given transformer function.
     """
-    client = acquirer.slimpay_client
-    params["creditorReference"] = acquirer.slimpay_creditor
+    client = provider.slimpay_client
+    params["creditorReference"] = provider.slimpay_creditor
     _logger.debug("Fetching first mandates...")
     doc = client.action("GET", "search-mandates", params=params)
     if "mandates" in doc:
         for mandate_doc in doc["mandates"]:
-            result = transformer_func(acquirer, mandate_doc)
+            result = transformer_func(provider, mandate_doc)
             if result:
                 yield result
         page_num = doc["page"]["totalPages"]
@@ -78,16 +78,16 @@ def get_all_mandates_repr(acquirer, transformer_func, **params):
             _logger.debug("Fetching page %d / %d...", page + 1, page_num)
             doc = client.get(doc.links["next"].url)
             for mandate_doc in doc["mandates"]:
-                result = transformer_func(acquirer, mandate_doc)
+                result = transformer_func(provider, mandate_doc)
                 if result:
                     yield result
 
 
-def set_mandate(acquirer, partner, mandate_id):
+def set_mandate(provider, partner, mandate_id):
     partner.payment_token_id.update(
         {
-            "acquirer_ref": mandate_id,
-            "acquirer_id": acquirer.id,
+            "provider_ref": mandate_id,
+            "provider_id": provider.id,
         }
     )
 
@@ -127,12 +127,12 @@ def set_contract_for_invoice_merge_autopay(contract):
     )
 
 
-def replace_mandate(acquirer, mandate_repr):
+def replace_mandate(provider, mandate_repr):
     """Replace partner's mandate by a new one described by `mandate_repr`
-    in the context of given `acquirer`.
+    in the context of given `provider`.
     """
     partner = (
-        acquirer.env["res.partner"]
+        provider.env["res.partner"]
         .browse(mandate_repr["subscriber"]["reference"])
         .ensure_one()
     )
@@ -150,11 +150,11 @@ def replace_mandate(acquirer, mandate_repr):
     # crashes) and can be computed automatically by Slimpay from the IBAN
     mandate_repr["signatory"]["bankAccount"].pop("bic", None)
 
-    mandate_repr["creditor"] = {"reference": acquirer.slimpay_creditor}
-    new_mandate_doc = acquirer.slimpay_client.action(
+    mandate_repr["creditor"] = {"reference": provider.slimpay_creditor}
+    new_mandate_doc = provider.slimpay_client.action(
         "POST", "create-mandates", params=mandate_repr
     )
-    set_mandate(acquirer, partner, new_mandate_doc["id"])
+    set_mandate(provider, partner, new_mandate_doc["id"])
     _logger.debug(
         "Created new mandate %s for %s", new_mandate_doc["reference"], partner.name
     )
@@ -163,7 +163,7 @@ def replace_mandate(acquirer, mandate_repr):
 # High level helpers ##########################################################
 
 
-def dump_all_mandates(acquirer, refresh, mandates_fpath, **params):
+def dump_all_mandates(provider, refresh, mandates_fpath, **params):
     """Extract all mandates and dump them as a json descr in `mandates_fpath`
     If the `refresh` parameter is True (the default), try to read given file
     for a previous mandate list and only append newly signed mandates.
@@ -178,18 +178,18 @@ def dump_all_mandates(acquirer, refresh, mandates_fpath, **params):
                 params["dateSignedAfter"] = max(m["dateSigned"] for m in old_mandates)
 
     mandates = old_mandates + list(
-        get_all_mandates_repr(acquirer, mandate_doc_to_repr, **params)
+        get_all_mandates_repr(provider, mandate_doc_to_repr, **params)
     )
     json.dump(mandates, open(mandates_fpath, "w"))
 
 
-def filter_has_contract(acquirer, mandate_repr):
+def filter_has_contract(provider, mandate_repr):
     "mandate partner has no contract"
-    partner = acquirer.env["res.partner"].browse(
+    partner = provider.env["res.partner"].browse(
         mandate_repr["subscriber"]["reference"]
     )
     return bool(
-        acquirer.env["contract.contract"].search(
+        provider.env["contract.contract"].search(
             [
                 ("commercial_partner_id", "=", partner.commercial_partner_id.id),
             ]
@@ -198,7 +198,7 @@ def filter_has_contract(acquirer, mandate_repr):
 
 
 def restore_all_missing_mandates(
-    acquirer,
+    provider,
     mandates_fpath="/tmp/mandates.json",
     filter_func=filter_has_contract,
     **params,
@@ -207,11 +207,11 @@ def restore_all_missing_mandates(
 
     mandates_repr = json.load(open(mandates_fpath))
     known_mandate_refs = dict(
-        get_all_mandates_repr(acquirer, mandate_doc_ref, **params)
+        get_all_mandates_repr(provider, mandate_doc_ref, **params)
     )
     for mandate_repr in mandates_repr:
         partner = (
-            acquirer.env["res.partner"]
+            provider.env["res.partner"]
             .browse(mandate_repr["subscriber"]["reference"])
             .exists()
         )
@@ -226,7 +226,7 @@ def restore_all_missing_mandates(
                 ),
             )
             continue
-        if filter_func and not filter_func(acquirer, mandate_repr):
+        if filter_func and not filter_func(provider, mandate_repr):
             _logger.info(
                 "Skipping mandate %s of %s: %s",
                 mandate_repr["reference"],
@@ -237,7 +237,7 @@ def restore_all_missing_mandates(
         ref = mandate_repr["reference"] = "TEST" + mandate_repr["reference"][4:]
         if ref not in known_mandate_refs:
             try:
-                replace_mandate(acquirer, mandate_repr)
+                replace_mandate(provider, mandate_repr)
             except MissingError:
                 _logger.error(
                     "Partner not found when trying to replace mandate for %s"
@@ -254,11 +254,11 @@ def restore_all_missing_mandates(
                 )
                 continue
             mandate_repr_ = next(
-                get_all_mandates_repr(acquirer, mandate_doc_ref, mandateReference=ref)
+                get_all_mandates_repr(provider, mandate_doc_ref, mandateReference=ref)
             )
             known_mandate_refs[ref] = mandate_repr_
         else:
-            set_mandate(acquirer, partner, known_mandate_refs[ref])
+            set_mandate(provider, partner, known_mandate_refs[ref])
             _logger.debug("Pre-existing mandate %s assigned to %s", ref, partner.name)
 
-    acquirer.env.cr.commit()
+    provider.env.cr.commit()
