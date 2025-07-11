d7fc87ec4572c8c2ec1ae1f600cebaa285f65d27
Florent Cayré <florent@commown.coop>
2025-07-11 14:31:54 +0200

    [tests] Fix and complete controller tests to also test the new submit feedback one

--- project_rating_nps/tests/test_controllers.py
+++ project_rating_nps/tests/test_controllers.py
@@ -12,19 +12,40 @@ from .common import RatingTestMixin
 
 
 class TestControllers(RatingTestMixin, HttpCase):
-    def test_open_rating(self):
+    def test_action_open_then_submit_rating(self):
         "Check rating_apply override works and uses present module images"
 
         test_client = Client(http.root, Response)
         werkzeug_environ = {"REMOTE_ADDR": "127.0.0.1"}
 
         response = test_client.get(
-            "/rating/%s/7" % self.rating.access_token, environ_base=werkzeug_environ
+            "/rate/%s/7" % self.rating.access_token, environ_base=werkzeug_environ
         )
-
-        self.assertIn("7/10", response.data.decode("utf-8"))
 
         doc = html.fromstring(response.data)
         self.assertEqual(
-            doc.xpath("//img/@src"), ["/project_rating_nps/static/src/img/passive.png"]
+            doc.xpath("//input[@type='radio'][@checked='True']/@value"), ["7"]
         )
+
+        post_values = dict(doc.xpath("//form")[0].form_values())
+        post_values["feedback"] = "Nice service, keep on the good work!"
+
+        response = test_client.post(
+            "/rate/%s/submit_feedback" % self.rating.access_token,
+            environ_base=werkzeug_environ,
+            data=post_values,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Thank you, we appreciate your feedback!", response.data)
+
+        self.rating.invalidate_recordset()
+        self.assertEqual(self.rating.rating, 7)
+        self.assertEqual(self.rating.feedback, post_values["feedback"])
+
+        # Check response in case of get
+        response2 = test_client.get(
+            "/rate/%s/submit_feedback" % self.rating.access_token,
+            environ_base=werkzeug_environ,
+        )
+
+        self.assertIn(b"Thank you, we appreciate your feedback!", response2.data)
