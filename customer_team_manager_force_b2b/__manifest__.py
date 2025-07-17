e6b5323beeb684d0ee85d16c8f563483ffd6cf70
Lena MONTENOT <lena.montenot@gmail.com>
2025-07-17 15:12:22 +0200

    Created customer_team_manager_force_b2b from commown

---
+++ customer_team_manager_force_b2b/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Customer team manager - force user onto B2B website",
+    "summary": "When granting employees portal access through customer_team_manager, force their site to be the B2B site",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Uncategorized",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "customer_team_manager",
+        "website_sale_b2b",
+    ],
+}
