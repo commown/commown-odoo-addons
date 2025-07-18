c69c6e8794495da631149e90f9fe41cb3d8653ff
Florent Cayré <florent@commown.coop>
2025-07-18 12:39:00 +0200

    Adapt to account.move / invoice new API

--- commown_investment_sale/models/account_move.py
+++ commown_investment_sale/models/account_move.py
@@ -24,23 +24,16 @@ class AccountInvoice(models.Model):
                 if line.product_id.product_tmpl_id.id in product_ids
             )
 
-            if invoice.residual != 0 or not has_invests:
+            if invoice.amount_residual != 0 or not has_invests:
                 continue
 
-            invoice.payment_move_line_ids.remove_move_reconcile()
-            self.env.cache.invalidate()
+            invoice.button_draft()
 
-            invoice.action_invoice_cancel()
-            self.env.cache.invalidate()
-
-            invoice.action_invoice_draft()
-            investment_payment_term = self.env.ref("commown.investment_payment_term")
-            invoice.write({"payment_term_id": investment_payment_term.id})
-            self.env.cache.invalidate()
+            pay_term = self.env.ref("commown_investment_sale.investment_payment_term")
+            invoice.write({"invoice_payment_term_id": pay_term.id})
 
             for line in invoice.invoice_line_ids:
                 if line.product_id.product_tmpl_id.id in product_ids:
                     line.update({"price_unit": line.price_unit * multiplier})
 
-            invoice.action_invoice_open()
-            self.env.cache.invalidate()
+            invoice.action_post()
