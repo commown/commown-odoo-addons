551fdeb1155e1850a9f4735c8f4161c8b5e77387
Florent Cayré <florent@commown.coop>
2025-07-22 18:16:36 +0200

    Create commown_payment_token_uniquify to extract code from the commown module

---
+++ commown_payment_token_uniquify/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Commown payment token uniquify customization",
+    "summary": "Add more payment_token_uniquify obsolescence actions to automatically reattribute invoices and contracts.",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Accounting/Payment",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+}
