ab23f2defd0967cc44948399a5b25f910fbae973
Lena MONTENOT <lena.montenot@gmail.com>
2025-07-31 12:32:46 +0200

    [test] Inserted common operations in setUpClass method
    - The same sources are used in both tests, so we define them in the setUpClass method.
    - In order for the remove action on source1 to fail in test_action_remove,
      there needs to be a record affiliated with source1, which blocks the deletion.
      So, the dummy record's source is source1 by default.

--- utm_utils_merge_and_remove_actions/tests/test_utm_source.py
+++ utm_utils_merge_and_remove_actions/tests/test_utm_source.py
@@ -6,33 +6,33 @@ from odoo.tests import TransactionCase, tagged
 
 @tagged("-at_install", "post_install")
 class UtmSourceTC(TransactionCase):
-    def test_action_merge(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
         # Register the test model:
-        self.loader = FakeModelLoader(self.env, self.__module__)
-        self.loader.backup_registry()
+        cls.loader = FakeModelLoader(cls.env, cls.__module__)
+        cls.loader.backup_registry()
 
         from .models import TestUtmDummy
 
-        self.loader.update_registry((TestUtmDummy,))
+        cls.loader.update_registry((TestUtmDummy,))
 
-        ref = self.env.ref
-        source1 = ref("utm.utm_source_search_engine")
-        source2 = source1.copy({"name": "test"})
-        rec = self.env["test.utm_dummy"].create({"source_id": source2.id})
+        cls.source1 = cls.env.ref("utm.utm_source_search_engine")
+        cls.source2 = cls.source1.copy({"name": "test"})
+        cls.dummy_rec = cls.env["test.utm_dummy"].create({"source_id": cls.source1.id})
 
-        self.env["utm.source"].browse((source1.id, source2.id)).action_merge()
+    def test_action_merge(self):
+        self.dummy_rec.source_id = self.source2.id
+        self.env["utm.source"].browse((self.source1.id, self.source2.id)).action_merge()
 
-        self.assertEqual(rec.source_id, source1)
+        self.assertEqual(self.dummy_rec.source_id, self.source1)
 
     def test_action_remove(self):
-        ref = self.env.ref
-        source1 = ref("utm.utm_source_search_engine")
-        source2 = source1.copy({"name": "test"})
-
         with self.assertRaises(UserError):
-            source1.action_remove()
+            self.source1.action_remove()
 
         try:
-            source2.action_remove()
+            self.source2.action_remove()
         except Exception as exc:
             self.fail("Source removal raised '%s' unexpectedly" % exc)
