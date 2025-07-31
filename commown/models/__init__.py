9673d8ccf4411459d5950926b0a5b583e58ff73e
Florent Cayré <florent@commown.coop>
2025-07-31 11:33:57 +0200

    Remove now useless code
    Odoo workflows are deprecated, remove related code.

--- commown/models/__init__.py
+++ commown/models/__init__.py
@@ -1,7 +1,6 @@
 from . import account_bank_statement
 from . import account_move
 from . import account_move_line
-from . import account_partial_reconcile
 from . import contract
 from . import crm_lead
 from . import ir_http
--- commown/models/account_partial_reconcile.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-
-class CommownAccountPartialReconcile(models.Model):
-    _inherit = "account.partial.reconcile"
-
-    def step_workflow(self):
-        return True
