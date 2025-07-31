848cc6c4ea3e5c2401403ee80f3da7914ea1f953
Lena MONTENOT <lena.montenot@gmail.com>
2025-07-31 14:15:04 +0200

    Create partner_firstname_portal module to replace name by firstname/lastname

---
+++ partner_firstname_portal/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Portal - Add firstname/lastname fields edition",
+    "summary": "Replace the name field by firstname/lastname from partner_firstname in the portal templates.",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+}
---
+++ partner_firstname_portal/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Replace the name field by firstname/lastname from partner_firstname in the portal templates.
