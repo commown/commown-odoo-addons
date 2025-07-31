a14221d914e1fbf66516ab7620538a570e39600f
Lena MONTENOT <lena.montenot@gmail.com>
2025-07-31 12:19:44 +0200

    [test] Replaced CRM demo record call by a dummy model record
    Beforehand, the module necessitated a dependency to the CRM module, because we used a demo crm.lead in the test,
    in order to assert the success of the sources merge.
    
    Since this is the only use, it's better to declare a dummy model which inherits utm.mixin, and to run tests on records from that model.

--- utm_utils_merge_and_remove_actions/tests/test_utm_source.py
+++ utm_utils_merge_and_remove_actions/tests/test_utm_source.py
@@ -1,3 +1,5 @@
+from odoo_test_helper import FakeModelLoader
+
 from odoo.exceptions import UserError
 from odoo.tests import TransactionCase, tagged
 
@@ -5,15 +7,22 @@ from odoo.tests import TransactionCase, tagged
 @tagged("-at_install", "post_install")
 class UtmSourceTC(TransactionCase):
     def test_action_merge(self):
+        # Register the test model:
+        self.loader = FakeModelLoader(self.env, self.__module__)
+        self.loader.backup_registry()
+
+        from .models import TestUtmDummy
+
+        self.loader.update_registry((TestUtmDummy,))
+
         ref = self.env.ref
         source1 = ref("utm.utm_source_search_engine")
         source2 = source1.copy({"name": "test"})
-        lead = ref("crm.crm_case_1")
-        lead.source_id = source2.id
+        rec = self.env["test.utm_dummy"].create({"source_id": source2.id})
 
         self.env["utm.source"].browse((source1.id, source2.id)).action_merge()
 
-        self.assertEqual(lead.source_id, source1)
+        self.assertEqual(rec.source_id, source1)
 
     def test_action_remove(self):
         ref = self.env.ref
