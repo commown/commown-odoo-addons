d05a21769215c3dc329f30003087c4a21fa203c8
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-01 09:14:38 +0200

    Adapted controller compatibility with new auth_signup implementation
    - When the signup/password reset requests fetch the qcontext, it no longer copies all parameters,
      but only a select few keys defined in the SIGN_UP_REQUEST_PARAMS set, which does not contain
      the firstname/lastname keys, and is declared outside of a class meaning it can't be modified.
      As such, we copy the firstname/lastname values straight from the request parameters into the qcontext.
    
    - Since the _prepare_signup_values method already checks the password and assigns a valid language,
      we only assign the name value in the qcontext constructed from the first and last name,
      then call the parent method.

--- partner_firstname_portal/controllers/signup.py
+++ partner_firstname_portal/controllers/signup.py
@@ -10,32 +10,24 @@ _logger = logging.getLogger(__name__)
 class CommownAuthSignup(AuthSignupHome):
     _commown_signup_auth_keys = ("firstname", "lastname")
 
-    def do_signup(self, qcontext):
-        """Add firstname and lastname compatibility to the auth_signup module
+    def _prepare_signup_values(self, qcontext):
+        """
+        Add firstname and lastname compatibility to the auth_signup module
 
-        The original controller drops all key of the qweb context but
-        login, name and password. We compute a `name` because
-        `res_user` `_signup_create_user` methods asserts one is
-        supplied.
+        The original controller retrieves every key in SIGN_UP_REQUEST_PARAMS,
+        then drops all keys of the qweb context but login, name and password.
+        We compute a `name` because `res_user` `_signup_create_user` methods
+        asserts one is supplied.
 
         """
-        values = {
-            key: qcontext.get(key)
-            for key in ("login", "password") + self._commown_signup_auth_keys
-        }
-        assert values.get("password") == qcontext.get(
-            "confirm_password"
-        ), "Passwords do not match; please retype them."
+        qcontext.update(
+            {key: request.params.get(key) for key in self._commown_signup_auth_keys}
+        )
+
         if qcontext.get("name") is None:
             Partner = request.env["res.partner"]
-            values["name"] = Partner._get_computed_name(
-                values["lastname"], values["firstname"]
+            qcontext["name"] = Partner._get_computed_name(
+                qcontext["lastname"], qcontext["firstname"]
             )
-        else:
-            values["name"] = qcontext["name"]
-        for lang in request.env["res.lang"].sudo().search_read([], ["code"]):
-            if request.lang == lang["code"]:
-                values["lang"] = request.lang
-                break
-        self._signup_with_values(qcontext.get("token"), values)
-        request.env.cr.commit()
+
+        return super()._prepare_signup_values(qcontext)
