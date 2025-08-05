83bc69ea3b758fffde7845a5d4451e9004edc07c
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-05 09:55:49 +0200

    Adapted to account.move field changes

--- contract_transaction_label/models/contract.py
+++ contract_transaction_label/models/contract.py
@@ -22,9 +22,9 @@ class Contract(models.Model):
         lang = self.env["res.lang"].search([("code", "=", self.partner_id.lang)])
         date_format = lang.date_format or "%m/%d/%Y"
         label = self.transaction_label
-        label = label.replace("#START#", invoice.date_invoice.strftime(date_format))
+        label = label.replace("#START#", invoice.invoice_date.strftime(date_format))
         label = label.replace("#END#", last_date_invoiced.strftime(date_format))
-        label = label.replace("#INV#", invoice.number)
+        label = label.replace("#INV#", invoice.name)
         return label
 
     def _pay_invoice(self, invoice):
@@ -35,6 +35,6 @@ class Contract(models.Model):
                 invoice.mapped("invoice_line_ids.contract_line_id.last_date_invoiced")
             )
             label = self._format_transaction_label(invoice, last_date_invoiced)
-            _logger.debug("Bank label for invoice %s: %s", invoice.number, label)
+            _logger.debug("Bank label for invoice %s: %s", invoice.name, label)
             self = self.with_context(payment_transaction_label=label)
         return super()._pay_invoice(invoice)
