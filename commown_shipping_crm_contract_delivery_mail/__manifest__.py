91a5ffdfac0225753db2e4b59e475b78bac571c4
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-07 11:53:52 +0200

    Moved out code from commown related to CRM delivery mail sending based on contract

--- commown_shipping_crm_contract_delivery_mail/__manifest__.py
+++ commown_shipping_crm_contract_delivery_mail/__manifest__.py
@@ -12,6 +12,7 @@
     "maintainers": ["fcayre", "Honeyxilia"],
     "license": "AGPL-3",
     "depends": [
-        "base",
+        "commown_lead_risk_analysis",
+        "commown_shipping",
     ],
 }
