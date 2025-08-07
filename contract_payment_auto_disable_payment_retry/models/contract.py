63470f3be24f3c725e4c9979ca336fa2e8280ca2
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-07 08:37:51 +0200

    Reworded comments for better explaination and idiomacy

--- contract_payment_auto_disable_payment_retry/models/contract.py
+++ contract_payment_auto_disable_payment_retry/models/contract.py
@@ -6,20 +6,20 @@ class Contract(models.AbstractModel):
 
     @api.model
     def _default_invoice_mail_template_id(self):
-        "Inactivate default invoice_mail_template_id"
+        "Disable by default invoice_mail_template_id"
         return False
 
     @api.model
     def _default_pay_retry_mail_template_id(self):
-        "Inactivate default pay_retry_mail_template_id"
+        "Disable by default pay_retry_mail_template_id"
         return False
 
     @api.model
     def _default_pay_fail_mail_template_id(self):
-        "Inactivate default pay_fail_mail_template_id"
+        "Disable by default pay_fail_mail_template_id"
         return False
 
     @api.model
     def _default_auto_pay_retries(self):
-        "Disable auto_pay_retries"
+        "Disable by default auto_pay_retries"
         return 0
