924af57887efb082e7acf34b8da74321af2c98f3
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-08 11:06:24 +0200

    Adapted to required shipping fields prototype change in controller

--- website_sale_commown_shipping/controllers/website_sale.py
+++ website_sale_commown_shipping/controllers/website_sale.py
@@ -4,8 +4,8 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class CommownShippingWebsiteSaleController(WebsiteSale):
-    def _get_mandatory_shipping_fields(self):
-        fields = super()._get_mandatory_billing_fields()
+    def _get_mandatory_fields_shipping(self, country_id=False):
+        fields = super()._get_mandatory_fields_shipping(country_id=country_id)
         if "email" not in fields:
             fields.append("email")
         return fields
