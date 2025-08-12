811acb8c395509f286803f9c5774a2aaf2d7f44b
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-12 14:15:11 +0200

    Adapted res.users create overload to support batch creation

--- commown/models/res_users.py
+++ commown/models/res_users.py
@@ -8,10 +8,10 @@ _logger = logging.getLogger(__name__)
 class res_users(models.Model):
     _inherit = "res.users"
 
-    @api.model
-    def create(self, vals):
+    @api.model_create_multi
+    def create(self, vals_list):
         "Disable automatic email sending when creating users"
         if self._context.get("import_file", False):
             _logger.info("Reset password is disabled while importing users")
             self = self.with_context(no_reset_password=True)
-        return super().create(vals)
+        return super().create(vals_list)
