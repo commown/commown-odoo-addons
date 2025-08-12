1c61f33bf326c3e937c176e1b9deabfa403c7132
Lena MONTENOT <lena.montenot@gmail.com>
2025-08-12 12:32:52 +0200

    [test] Adapted account.move merge test to new payment API and test helpers

--- commown/tests/test_account_move.py
+++ commown/tests/test_account_move.py
@@ -1,41 +1,48 @@
-from unittest.mock import patch
-
 from odoo.tests import tagged
 
-from odoo.addons.account_invoice_merge_auto_pay.tests.common import (
-    AutoPayInvoiceTC,
-    fake_do_tx_ok,
+from odoo.addons.account_invoice_merge_auto_pay.tests.test_account_move import (
+    AccountMoveTC,
 )
-from odoo.addons.payment.models.payment_provider import PaymentTransaction
 
 
 @tagged("-at_install", "post_install")
-class AccountInvoiceTC(AutoPayInvoiceTC):
+class AccountInvoiceTC(AccountMoveTC):
     def test_user_id_ignored_in_invoice_merge(self):
-        inv_1 = self.create_invoice(
-            self.partner_1,
-            "2019-05-09",
-            payment_mode_id=self.payment_mode.id,
-            user_id=self.env.user.id,
-        )
-        inv_2 = self.create_invoice(
-            self.partner_1, "2019-05-10", payment_mode_id=self.payment_mode.id
-        )
+        """
+        When merging invoices, the (invoice_)user_id should not
+        be used as a key to sync invoices, even if they differ.
+        """
+        demo_user_1 = self.env.ref("base.user_demo")
+        demo_user_2 = self.env.ref("base.demo_user0")
+        self.assertNotIn(self.env.user, (demo_user_1 | demo_user_2))
 
-        with patch.object(PaymentTransaction, "s2s_do_transaction", fake_do_tx_ok):
-            self._multiple_invoice_merge_test([inv_1, inv_2])
+        inv_1 = self.create_invoice(self.partner_a, "2019-05-09", 60)
+        inv_1.invoice_user_id = demo_user_1
+        inv_2 = self.create_invoice(self.partner_a, "2019-05-10", 60)
+        inv_2.invoice_user_id = demo_user_2
+
+        # Testing the method overload
+        self.assertNotIn("user_id", inv_1._get_invoice_key_cols())
+
+        # Merge the invoices
+        # Their (invoice_)user_id fields shouldn't interfere with the merge or be used.
+        _, merge_infos = self.env["account.move"]._cron_invoice_merge("2019-05-16")
+        new_inv = self.env["account.move"].browse(list(merge_infos.keys())[0]).exists()
+
+        self.assertEqual(new_inv.user_id, self.env.user)
 
     def test_merge_auto_pay_sends_email(self):
-        p_inv = self.partner_1.copy({"type": "invoice", "parent_id": self.partner_1.id})
-        inv_1 = self.create_invoice(
-            p_inv, "2019-05-09", payment_mode_id=self.payment_mode.id
+        p_inv = self.partner_a.copy(
+            {
+                "type": "invoice",
+                "parent_id": self.partner_a.id,
+                "email": "test@test.coop",
+            }
         )
-        inv_2 = self.create_invoice(
-            p_inv, "2019-05-10", payment_mode_id=self.payment_mode.id
-        )
+        self.create_invoice(p_inv, "2019-05-09", 60)
+        self.create_invoice(p_inv, "2019-05-10", 60)
 
-        with patch.object(PaymentTransaction, "s2s_do_transaction", fake_do_tx_ok):
-            new_inv = self._multiple_invoice_merge_test([inv_1, inv_2])
+        new_inv = self._merge_and_pay()
 
         mail = self.env["mail.mail"].search(
             [
