2dc5f14cd97aac984cdda30f311552e74175ac7d
Florent Cayré <florent@commown.coop>
2025-08-26 11:01:37 +0200

    Factor out a function to set the recurrent price of a product on variant change
    We wrap all the file in an IIFE to avoid polluting the global
    scope with that function.

--- product_rental/static/src/js/website_sale.js
+++ product_rental/static/src/js/website_sale.js
@@ -1,48 +1,44 @@
-odoo.define("product_rental.VariantMixin", function (require) {
+(function () {
     "use strict";
 
-    var VariantMixin = require("sale.VariantMixin");
-    var publicWidget = require("web.public.widget");
+    function set_recurrent_price(priceToStr, $parent, combination) {
+        const $recurrent_payment_amount = $parent.find(".oe_recurrent_payment_amount");
 
-    require("website_sale.website_sale");
+        const ratio = parseFloat(
+            $recurrent_payment_amount.data("recurrent-payment-ratio")
+        );
+        const price = priceToStr(combination.price / ratio);
+        $recurrent_payment_amount.find(".oe_currency_value").html(price);
+    }
 
-    publicWidget.registry.WebsiteSale.include({
-        _onChangeCombination: function (ev, $parent, combination) {
-            this._super.apply(this, arguments);
-            const $recurrent_payment_amount = $parent.find(
-                ".oe_recurrent_payment_amount"
-            );
-            const ratio = parseFloat(
-                $recurrent_payment_amount.data("recurrent-payment-ratio")
-            );
-            const price = this._priceToStr(combination.price / ratio);
-            $recurrent_payment_amount.find(".oe_currency_value").html(price);
-        },
+    odoo.define("product_rental.VariantMixin", function (require) {
+        var VariantMixin = require("sale.VariantMixin");
+        var publicWidget = require("web.public.widget");
+
+        require("website_sale.website_sale");
+
+        publicWidget.registry.WebsiteSale.include({
+            _onChangeCombination: function (ev, $parent, combination) {
+                this._super.apply(this, arguments);
+                set_recurrent_price(this._priceToStr, $parent, combination);
+            },
+        });
+
+        return VariantMixin;
     });
 
-    return VariantMixin;
-});
+    odoo.define("product_rental.OptionalProductsModal", function (require) {
+        const {
+            OptionalProductsModal,
+        } = require("@sale_product_configurator/js/product_configurator_modal");
 
-odoo.define("product_rental.OptionalProductsModal", function (require) {
-    "use strict";
+        OptionalProductsModal.include({
+            _onChangeCombination: function (ev, $parent, combination) {
+                this._super.apply(this, arguments);
+                set_recurrent_price(this._priceToStr, $parent, combination);
+            },
+        });
 
-    const {
-        OptionalProductsModal,
-    } = require("@sale_product_configurator/js/product_configurator_modal");
-
-    OptionalProductsModal.include({
-        _onChangeCombination: function (ev, $parent, combination) {
-            this._super.apply(this, arguments);
-            const $recurrent_payment_amount = $parent.find(
-                ".oe_recurrent_payment_amount"
-            );
-            const ratio = parseFloat(
-                $recurrent_payment_amount.data("recurrent-payment-ratio")
-            );
-            const price = this._priceToStr(combination.price / ratio);
-            $recurrent_payment_amount.find(".oe_currency_value").html(price);
-        },
+        return OptionalProductsModal;
     });
-
-    return OptionalProductsModal;
-});
+})();
