778e72f3232b7592e4cc10f88bca0f34a6cb0aab
Lena MONTENOT <lena.montenot@gmail.com>
2025-09-04 13:57:00 +0200

    Added commown_partner_property_accounts as a commown dependency

--- commown/__manifest__.py
+++ commown/__manifest__.py
@@ -30,6 +30,7 @@
         "payment_slimpay_issue",
         "commown_res_partner_sms",
         "commown_payment_token_uniquify",
+        "commown_partner_property_accounts",
         "commown_partner_set_invoice_recipient",
         "contract_payment_auto_disable_payment_retry",
         "partner_firstname_portal",
