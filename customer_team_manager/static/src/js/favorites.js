d0ca87c209ca726ba1acb114db3fa6fa111e5338
Florent Cayré <florent@commown.coop>
2025-09-08 15:44:37 +0200

    Remove the Favorites menu from the search view when user is a customer
    This is a bit radical, but this feature is not that useful in the
    context of customer dedicaded UIs and keeping it would require to hide
    some sub-functionnalities (namely: share a favorite with users and save
    a favorite to the dashboard).

---
+++ customer_team_manager/static/src/js/favorites.js
@@ -0,0 +1,17 @@
+odoo.define('customer_team_manager.SearchView', function(require) {
+  "use strict";
+
+  var session = require('web.session');
+  var SearchView = require('web.SearchView');
+
+  SearchView.include({
+    /*
+      Disable the widget alltogether for customer admins
+    */
+    willStart: function () {
+      this.options.disable_favorites = session.is_customer;
+      return this._super();
+    },
+  });
+
+});
