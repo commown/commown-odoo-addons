521a47a6c8aed99837707516f31301c2a32887d8
Florent Cayré <florent@commown.coop>
2025-09-09 20:07:47 +0200

    Post-merge updates

--- commown_devices/__manifest__.py
+++ commown_devices/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Commown devices",
     "category": "stock",
-    "version": "12.0.1.2.13",
+    "version": "12.0.1.2.14",
     "author": "Commown SCIC",
     "license": "AGPL-3",
     "website": "https://commown.coop",
