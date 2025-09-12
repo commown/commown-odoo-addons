31f77c52bbca7fc788e954def3c27251b5444ddb
Lena MONTENOT <lena.montenot@gmail.com>
2025-09-12 11:58:54 +0200

    Inlined update instruction

--- commown/models/payment_transaction.py
+++ commown/models/payment_transaction.py
@@ -7,9 +7,5 @@ class SlimpayTransaction(models.Model):
     def _slimpay_tx_completed(self, client, order_doc, **tx_attrs):
         "Use last slimpay transaction token as partner payment_token_id"
         token = super()._slimpay_tx_completed(client, order_doc, **tx_attrs)
-        self.mapped("sale_order_ids.partner_id").update(
-            {
-                "payment_token_id": token.id,
-            }
-        )
+        self.mapped("sale_order_ids.partner_id").update({"payment_token_id": token.id})
         return token
