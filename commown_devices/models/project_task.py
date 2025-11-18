295ae91d23dab47ecd4f83ac4d91981ebff2c5a0
Florent Cayré <florent@commown.coop>
2025-11-18 21:40:43 +0100

    Fix the picking origin computed by the task sending the initial device of a contract
    It must match the contract name so the check requiring tasks to have a
    picking assigned before moving to a stage configured for this check does
    not erroneously raise.

--- commown_devices/models/project_task.py
+++ commown_devices/models/project_task.py
@@ -1,9 +1,12 @@
 import json
+import re
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, Warning
 
 from .common import ToCustomerPickingMixin, _assigned
+
+REGEX_SO = re.compile(r"^\[(SO[0-9]+-[0-9]+)\].*$")
 
 CHECK_CONTRACT_QUANT_NB_STAGE_XML_IDS = [
     "commown_devices.diagnostic_stage",
@@ -276,4 +279,11 @@ class ProjectTask(ToCustomerPickingMixin, models.Model):
         }
 
     def get_name_for_origin(self):
-        return "Task-%s" % self.id
+        """Return an origin for the picking(s) current task is related to:
+
+        - "SOxxxx-xx" if current task is the task used to send the
+          initial device of a contract
+        - "Task-<id>" otherwise
+        """
+        match = REGEX_SO.match(self.name or "")
+        return match.groups()[0] if match else "Task-%s" % self.id
