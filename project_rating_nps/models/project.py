f1a4cf5b920c686b4d6f983b5a9a37a78ef0e590
Lena MONTENOT <lena.montenot@gmail.com>
2025-11-19 09:46:32 +0100

    [IMP] Assigned NPS compute method to rating_count
    Since the total amount of ratings is already computed in the _compute_net_promoter_score method,
    we can simply assign rating_count in it, to avoid it being computed twice.

--- project_rating_nps/models/project.py
+++ project_rating_nps/models/project.py
@@ -13,6 +13,9 @@ class Project(models.Model):
         compute="_compute_net_promoter_score", string="NPS", store=True, default=False
     )
 
+    # Originally computed in _compute_rating_satisfaction_percentage,
+    rating_count = fields.Integer(compute="_compute_net_promoter_score")
+
     @api.depends("task_ids.rating_ids.rating")
     def _compute_net_promoter_score(self):
         for record in self:
@@ -34,6 +37,7 @@ class Project(models.Model):
                 ),
             ]
             total_count = self.env["rating.rating"].search_count(base_domain)
+            record.rating_count = total_count
             if total_count == 0:
                 record.net_promoter_score = False
             else:
@@ -54,28 +58,12 @@ class Project(models.Model):
         between 0 and 5, which doesn't fit our NPS rating (ranging from 0 to 10),
         this leads to a AssertionError exception when computing affected fields.
 
-        The rating_count value is used in the project.project kanban view, so we assign it.
-        We don't use the other fields, so we assign dummy values to them.
+        We don't use these fields, so we assign dummy values to them.
         """
-        for record in self:
-            domain = [
-                ("parent_res_model", "=", record._name),
-                ("parent_res_id", "=", record.id),
-                ("consumed", "=", True),
-            ]
-            if self._rating_satisfaction_days:
-                dt = fields.datetime.now() - timedelta(
-                    days=record._rating_satisfaction_days
-                )
-                domain += [("write_date", ">=", fields.Datetime.to_string(dt))]
-
-            record.rating_count = self.env["rating.rating"].search_count(domain)
-
-            # Assigning dummy values to unused fields
-            record.write(
-                {
-                    "rating_percentage_satisfaction": -1,
-                    "rating_avg": -1,
-                    "rating_avg_percentage": -1,
-                }
-            )
+        self.write(
+            {
+                "rating_percentage_satisfaction": -1,
+                "rating_avg": -1,
+                "rating_avg_percentage": -1,
+            }
+        )
