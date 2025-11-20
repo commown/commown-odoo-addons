19cbadf4498f55215cd6aae4c4d1d1d46ac50e96
Lena MONTENOT <lena.montenot@gmail.com>
2025-11-20 08:40:44 +0100

    [IMP] Removed datetime to string convertion in search domain
    We can directly use datetimes in the search domain - as such, we remove the datetime to string conversion
    in order to increase lisibility of the search domain.

--- project_rating_nps/models/project.py
+++ project_rating_nps/models/project.py
@@ -23,13 +23,7 @@ class Project(models.Model):
                 ("parent_res_model", "=", record._name),
                 ("parent_res_id", "=", record.id),
                 ("consumed", "=", True),
-                (
-                    "create_date",
-                    ">=",
-                    fields.Datetime.to_string(
-                        fields.datetime.now() - timedelta(days=30)
-                    ),
-                ),
+                ("create_date", ">=", fields.datetime.now() - timedelta(days=30)),
             ]
             ratings = self.env["rating.rating"].search(base_domain)
             record.rating_count = total_count = len(ratings)
