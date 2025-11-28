ef837c6fcd7aa2003c80cec26d2a008233ed5aed
Lena MONTENOT <lena.montenot@gmail.com>
2025-11-28 11:30:04 +0100

    [ADD] Created view_contract_lines_hide_action_button_labels module

---
+++ view_contract_lines_hide_action_button_labels/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Contract - Hide Action label on Contract lines buttons",
+    "summary": "Hides the labels from the action buttons ('Stop', 'Stop Plan Successor'...) from the contract line tree nested view, to render the contract line narrower.",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+}
---
+++ view_contract_lines_hide_action_button_labels/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Hides the labels from the action buttons ("Stop", "Stop Plan Successor"...) from the contract line tree nested view, to render the contract line narrower.
