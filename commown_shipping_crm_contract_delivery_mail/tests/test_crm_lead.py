b74c55b3c9cb9530a3a4b411e4b8844ccc7f71b6
Lena MONTENOT <lena.montenot@gmail.com>
2026-01-28 11:20:57 +0100

    [REF (tests)] commown_shipping_crm_contract_delivery_mail: Fused default send_email_on_delivery value tests in one
    The goal of this test is to check whether the default send_email_on_delivery value is set well,
    when the contract_id value is set.
    
    As such, it's clearer to have both use cases in a single test.

--- commown_shipping_crm_contract_delivery_mail/tests/test_crm_lead.py
+++ commown_shipping_crm_contract_delivery_mail/tests/test_crm_lead.py
@@ -10,37 +10,41 @@ class CrmLeadTC(RentalSaleOrderTC):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.so = cls.create_sale_order()
-        cls.so.team_id.default_perform_actions_on_delivery = True
-        cls.so.mapped("order_line.product_id").update(
-            {"followup_sales_team_id": cls.so.team_id.id}
+        cls.so1 = cls.create_sale_order()
+        cls.so2 = cls.create_sale_order()
+
+        cls.so1.team_id.default_perform_actions_on_delivery = True
+
+        (cls.so1 | cls.so2).mapped("order_line.product_id").update(
+            {"followup_sales_team_id": cls.so1.team_id.id}
         )
 
     def test_default_action_on_delivery_with_contract(self):
-        self.so.team_id.default_perform_actions_on_delivery = True
-        lead = self._create_ra_leads(self.so).filtered("contract_id")[0]
+        # Case 1: default actions are enabled
+        self.so1.team_id.default_perform_actions_on_delivery = True
+        lead1 = self._create_ra_leads(self.so1).filtered("contract_id")[0]
 
-        self.assertTrue(lead.send_email_on_delivery)
+        self.assertTrue(lead1.send_email_on_delivery)
+
+        # Case 2: default actions are disabled
+        self.so2.team_id.default_perform_actions_on_delivery = False
+        lead2 = self._create_ra_leads(self.so2).filtered("contract_id")[0]
+
+        self.assertFalse(lead2.send_email_on_delivery)
 
     def test_default_action_on_delivery_without_contract(self):
         "Even when team perform actions on delivery"
         # Remove all contract products from sale order to get leads without a contract
-        self.so.order_line.filtered("product_id.is_contract").unlink()
+        self.so1.order_line.filtered("product_id.is_contract").unlink()
 
-        self.so.team_id.default_perform_actions_on_delivery = True
-        lead = self._create_ra_leads(self.so).filtered(lambda l: not l.contract_id)[0]
+        self.so1.team_id.default_perform_actions_on_delivery = True
+        lead = self._create_ra_leads(self.so1).filtered(lambda l: not l.contract_id)[0]
 
         self.assertFalse(lead.contract_id)  # Check pre-requisite
         self.assertFalse(lead.send_email_on_delivery)
 
         # Check delivery does not crash
         lead.delivery_date = date(2017, 1, 1)
-
-    def test_default_no_action_on_delivery(self):
-        self.so.team_id.default_perform_actions_on_delivery = False
-        lead = self._create_ra_leads(self.so)[0]
-
-        self.assertFalse(lead.send_email_on_delivery)
 
     def _create_ra_leads(self, so):
         "Confirm the sale and return all its just-created risk-analysis leads"
