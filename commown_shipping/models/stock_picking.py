9ac3a057d703cc95a047407fd258b037dbc487c1
Florent Cayré <florent@commown.coop>
2026-04-26 12:13:48 +0200

    [FIX] WIP (missing test): return package to sender unless not applicable

--- commown_shipping/models/stock_picking.py
+++ commown_shipping/models/stock_picking.py
@@ -7,6 +7,11 @@ class StockPicking(models.Model):
     def _laposte_fr_get_service(self, account, package=None):
         vals = self._roulier_get_service(account, package=package)
 
+        if vals.get("product") != "COLI":
+            vals.pop("returnTypeChoice", None)
+        else:
+            vals["returnTypeChoice"] = 3  # do not return to sender
+
         if getattr(origin := self.origin_entity(), "get_label_ref", None):
             vals["reference1"] = origin.get_label_ref()
 
