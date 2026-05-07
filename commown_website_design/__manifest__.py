6333c3c6b95b154bc5716a2e16cd8fb3ce9fc822
Lena MONTENOT / Honeyxilia <lena.montenot@gmail.com>
2026-05-07 13:47:44 +0200

    [ADD] commown_website_design
    This module will contain all customized view overrides for the Odoo Commown website.

---
+++ commown_website_design/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Commown - Website designs",
+    "summary": "Module to contain custom website view overrides, to better maintain them",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+}
