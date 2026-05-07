93b186ff4da3e078852744b427d8c3297642d9c6
Lena MONTENOT / Honeyxilia <lena.montenot@gmail.com>
2026-05-07 14:05:53 +0200

    [IMP] commown_website_design: Added copies of Slogan template views to insert website-specific titles
    While it's possible to insert custom slogan values with the website editor, it's more stable and easier to maintain
    for future migrations within the code.
    
    Indeed, when a header template is applied on a specific website, the template and its children are copied with the same key,
    but with a specific website - as such, we insert QWeb templates with the required key and the specified websites in order
    to trick Odoo into using our slogans directly when the views are copied.

--- commown_website_design/__manifest__.py
+++ commown_website_design/__manifest__.py
@@ -12,6 +12,9 @@
     "maintainers": ["fcayre", "Honeyxilia"],
     "license": "AGPL-3",
     "depends": [
-        "base",
+        "website_b2b",
+    ],
+    "data": [
+        "views/website_templates.xml",
     ],
 }
---
+++ commown_website_design/tests/test_views.py
@@ -0,0 +1,37 @@
+from lxml import html
+
+from odoo.tests import HttpCase
+
+
+def slogan_text(html_string):
+    return html.fromstring(html_string).xpath("//*[@data-name='Slogan']/text()")
+
+
+class CommownWebsiteDesignTC(HttpCase):
+    def test_custom_slogans_in_header_template(self):
+        """
+        When selecting the Slogan Header template, our views with our customs Slogans should be used
+        """
+        # Setup
+        website = self.env.ref("website.default_website")
+        website_b2b = self.env.ref("website_b2b.b2b_website")
+        theme_utils = self.env["theme.utils"]
+
+        tmpl_key = "website.template_header_slogan"
+
+        # We authenticate as a user possessing the groups required to use the /website/force/ endpoint
+        self.authenticate("demo", "demo")
+
+        # Default state: there shouldn't be a Slogan in the view
+        default_res = self.url_open("/")
+        self.assertFalse(slogan_text(default_res.text))
+
+        # Setting the Slogan Header, on both sites we use
+        theme_utils.with_context(website_id=website.id).enable_view(tmpl_key)
+        theme_utils.with_context(website_id=website_b2b.id).enable_view(tmpl_key)
+
+        res_b2c = self.url_open("/")
+        res_b2b = self.url_open(f"/website/force/{website_b2b.id}")
+
+        self.assertEqual(slogan_text(res_b2c.text), ["Commown pour les particuliers"])
+        self.assertEqual(slogan_text(res_b2b.text), ["Commown pour les professionnels"])
