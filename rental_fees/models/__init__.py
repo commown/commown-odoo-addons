8a3d227ad4e58cc5cc08816f2ac744234e6f1aa1
Florent Cayré <florent@commown.coop>
2026-06-01 11:19:41 +0200

    Turn rental periods computation into a lot method
    Before that it was a rental fees computation method although the only fees
    computation data that was use was its date. It is now possible to use this
    method for any device.

--- rental_fees/models/__init__.py
+++ rental_fees/models/__init__.py
@@ -3,4 +3,5 @@ from . import account_move_line
 from . import purchase_order
 from . import rental_fees_computation
 from . import rental_fees_definition
+from . import stock_lot
 from . import wizard_project_task_to_employee
