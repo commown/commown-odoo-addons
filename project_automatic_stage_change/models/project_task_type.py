c2bf71d879248d6184be583389d2a526fd0294b8
Lena MONTENOT / Honeyxilia <lena.montenot@gmail.com>
2026-06-03 10:09:47 +0200

    [IMP] project_automatic_stage_change: Added constraint for stage change days and destination stage
    - If the amount of days on a stage is null or negative, then moving the task to that stage would lead
      to a direct move of the task to the destination stage after the automatic action sweeps the tasks.
      At this point, you'd be better off moving the task to the desired destination.
    
    - Having a null destination stage would lead to a writing error upon the action execution.

--- project_automatic_stage_change/models/project_task_type.py
+++ project_automatic_stage_change/models/project_task_type.py
@@ -1,11 +1,40 @@
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProjectTaskType(models.Model):
     _inherit = "project.task.type"
 
     has_timely_stage_change = fields.Boolean()
-    timely_stage_change_days = fields.Integer()
+    timely_stage_change_days = fields.Integer(
+        help=(
+            "This number can only be set as strictly positive, "
+            "otherwise, any task moved into this stage would be "
+            "moved directly to the defined destination stage."
+        ),
+    )
     timely_stage_dest = fields.Many2one(
         "project.task.type", domain="[('project_ids', '=', project_ids)]"
     )
+
+    @api.constrains(
+        "has_timely_stage_change", "timely_stage_change_days", "timely_stage_dest"
+    )
+    def _check_timely_stage_change_fields(self):
+        for stage in self:
+            if stage.has_timely_stage_change:
+                if not stage.timely_stage_dest:
+                    raise ValidationError(
+                        _(
+                            "You must set a destination stage for the passive change, "
+                            "as the passive change is active on this stage."
+                        )
+                    )
+
+                if stage.timely_stage_change_days <= 0:
+                    raise ValidationError(
+                        _(
+                            "You must set a strictly positive number of days! "
+                            "(or else, any task would be immediatly moved after being set in this stage)"
+                        )
+                    )
