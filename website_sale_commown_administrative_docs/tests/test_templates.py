edad1e6d00e5e10b59129558a9a1d3573d4e7654
Lena MONTENOT / Honeyxilia <lena.montenot@gmail.com>
2026-06-04 18:04:01 +0200

    [IMP] website_sale_commown_administrative_docs: Added check if logged in partner already has docs uploaded in their profile
    If the customer already uploaded their documents in their customer space, then the reminder may seem redundent.
    
    As such, before displaying the reminder, we check if they at least have one of the ID card field set,
    and if they have either the company record or proof of address depending on their site,

--- website_sale_commown_administrative_docs/tests/test_templates.py
+++ website_sale_commown_administrative_docs/tests/test_templates.py
@@ -1,9 +1,14 @@
+from base64 import b64encode
+from pathlib import Path
+
 from lxml import html
 
 from odoo.http import root
 from odoo.tests import HttpCase
 
 from odoo.addons.product_rental.tests.common import RentalSaleOrderTC
+
+HERE = (Path(__file__) / "..").resolve()
 
 
 class WebsiteSaleAdminDocsTC(RentalSaleOrderTC, HttpCase):
@@ -25,6 +30,18 @@ class WebsiteSaleAdminDocsTC(RentalSaleOrderTC, HttpCase):
                 raise ValueError("Incorrect page type")
 
         root.session_store.save(session)
+
+    def _set_documents(self):
+        with open(HERE / "smallest.pdf", "rb") as fobj:
+            doc = b64encode(fobj.read())
+
+        vals = {"id_card1": doc}
+        if self.partner.website_id == self.b2b_website:
+            vals["company_record"] = doc
+        else:
+            vals["proof_of_address"] = doc
+
+        self.partner.write(vals)
 
     def test_docs_reminder_in_payment_b2c(self):
         self._login_and_set_sale_order(self.so, "payment")
@@ -51,6 +68,27 @@ class WebsiteSaleAdminDocsTC(RentalSaleOrderTC, HttpCase):
         docs_desc = "".join(page.xpath("//div[@id='admin_docs_reminder']//a/text()"))
         self.assertIn("KBIS", docs_desc)
 
+    def test_docs_already_set_in_payment_b2c(self):
+        self._login_and_set_sale_order(self.so, "payment")
+        self._set_documents()
+
+        req = self.url_open("/shop/payment", allow_redirects=False)
+        self.assertEqual(req.status_code, 200)
+
+        page = html.fromstring(req.text)
+        self.assertFalse(page.xpath("//div[@id='admin_docs_reminder']"))
+
+    def test_docs_already_set_in_payment_b2b(self):
+        self._login_and_set_sale_order(self.so, "payment")
+        self.partner.website_id = self.b2b_website
+        self._set_documents()
+
+        req = self.url_open("/shop/payment", allow_redirects=False)
+        self.assertEqual(req.status_code, 200)
+
+        page = html.fromstring(req.text)
+        self.assertFalse(page.xpath("//div[@id='admin_docs_reminder']"))
+
     def test_docs_reminder_in_confirmation_b2c(self):
         self._login_and_set_sale_order(self.so, "confirmation")
 
@@ -75,3 +113,24 @@ class WebsiteSaleAdminDocsTC(RentalSaleOrderTC, HttpCase):
         self.assertTrue(page.xpath("//div[@id='admin_docs_reminder']"))
         docs_desc = "".join(page.xpath("//div[@id='admin_docs_reminder']/text()"))
         self.assertIn("Kbis", docs_desc)
+
+    def test_docs_already_set_in_confirmation_b2c(self):
+        self._login_and_set_sale_order(self.so, "confirmation")
+        self._set_documents()
+
+        req = self.url_open("/shop/confirmation", allow_redirects=False)
+        self.assertEqual(req.status_code, 200)
+
+        page = html.fromstring(req.text)
+        self.assertFalse(page.xpath("//div[@id='admin_docs_reminder']"))
+
+    def test_docs_already_set_in_confirmation_b2b(self):
+        self._login_and_set_sale_order(self.so, "confirmation")
+        self.partner.website_id = self.b2b_website
+        self._set_documents()
+
+        req = self.url_open("/shop/confirmation", allow_redirects=False)
+        self.assertEqual(req.status_code, 200)
+
+        page = html.fromstring(req.text)
+        self.assertFalse(page.xpath("//div[@id='admin_docs_reminder']"))
