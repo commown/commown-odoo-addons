a0ef75a1fae4aee635c1545a8280618dfa4ed735
Florent Cayré <florent@commown.coop>
2026-06-18 19:24:30 +0200

    [FIX,IMP] commown_devices: make stock.lot current_location method work as expected
    ... and test the expected behaviour.
    The behaviour was wrong when the raise parameters were False.
    
    We also make it possible to search current lot location without supplying a parent location.

--- commown_devices/tests/test_stock_lot.py
+++ commown_devices/tests/test_stock_lot.py
@@ -2,23 +2,67 @@ from .common import BaseLotTC
 
 
 class StockProductionLotTC(BaseLotTC):
-    def test_current_location(self):
+    def test_current_location_ok(self):
+        to_check_loc = self.env.ref("commown_devices.stock_location_devices_to_check")
+
+        self.assertTrue(self.quant.quantity)  # Test prerequisite
+        self.assertFalse(self.quant.reserved_quantity)  # Test prerequisite
+
+        self.assertEqual(
+            self.lot.current_location(),
+            self.location_internal_available,
+        )
+
         self.assertEqual(
             self.lot.current_location(self.location_available_for_rent),
             self.location_internal_available,
         )
-        self.quant.quantity = 0
-        with self.assertRaises(Warning) as err1:
-            self.lot.current_location(self.location_available_for_rent)
+
+        self.assertFalse(
+            self.lot.current_location(to_check_loc),
+            self.location_internal_available,
+        )
+
+        self.quant.reserved_quantity = 1
+        self.assertEqual(
+            self.lot.current_location(),
+            self.location_internal_available,
+        )
+
+    def test_current_location_raises(self):
+        to_check_loc = self.env.ref("commown_devices.stock_location_devices_to_check")
+        for_rent_loc = self.location_available_for_rent
+
+        with self.assertRaises(Warning) as err:
+            self.lot.current_location(to_check_loc, raise_if_not_found=True)
         self.assertEqual(
             "Lot %s not found in available stock" % self.lot.name,
-            err1.exception.args[0],
+            err.exception.args[0],
         )
+
+        self.quant.quantity = 0
+        with self.assertRaises(Warning) as err:
+            self.lot.current_location(raise_if_not_found=True)
+        self.assertEqual(
+            "Lot %s not found in available stock" % self.lot.name,
+            err.exception.args[0],
+        )
+
         self.quant.quantity = 1
         self.quant.reserved_quantity = 1
-        with self.assertRaises(Warning) as err2:
-            self.lot.current_location(self.location_available_for_rent)
+        with self.assertRaises(Warning) as err:
+            self.lot.current_location(for_rent_loc, raise_if_reserved=True)
         self.assertEqual(
             "Lot %s is already reserved" % self.lot.name,
-            err2.exception.args[0],
+            err.exception.args[0],
+        )
+
+        # Not found error has priority over reserved
+        with self.assertRaises(Warning) as err:
+            self.lot.current_location(
+                to_check_loc, raise_if_not_found=True, raise_if_reserved=True
+            )
+        self.assertEqual(
+            "Lot %s not found in available stock" % self.lot.name,
+            err.exception.args[0],
         )
