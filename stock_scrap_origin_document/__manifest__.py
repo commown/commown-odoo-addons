87fdc0405f6cb8b66b128f052277c250fb235a0e
Florent Cayré <florent@commown.coop>
2026-07-17 11:23:22 +0200

    [ADD] stock_scrap_origin_document: use any document as the origin of a scrap
    This commit only adds the module, it does not contain code.

---
+++ stock_scrap_origin_document/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Stock swap origin document",
+    "summary": "Adds an origin document reference to stock scraps",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Inventory/Inventory",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+}
