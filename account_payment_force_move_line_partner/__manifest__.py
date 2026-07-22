854c07591941676ad0e7858b4548c1be197b3d3b
Lena MONTENOT / Honeyxilia <lena.montenot@gmail.com>
2026-07-22 16:27:03 +0200

    [ADD] account_payment_force_move_line_partner

---
+++ account_payment_force_move_line_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Commown SCIC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Account payment - Force move line partner",
+    "summary": "Wizard to force a different partner on an account move line related to an account payment",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/commown/commown-odoo-addons",
+    "author": "Commown SCIC",
+    "maintainers": ["fcayre", "Honeyxilia"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+}
