d87537e77d0d7272b551b8de8d8f05d183a8593d
Florent Cayré <florent@commown.coop>
2026-07-30 23:32:05 +0200

    [IMP] commown_netinstaller: add post install script to contract public method

--- commown_netinstaller/tests/test_contract.py
+++ commown_netinstaller/tests/test_contract.py
@@ -37,8 +37,43 @@ class NetinstallerContractTC(RentalSaleOrderTC, NetinstallMixin):
         cls.so.action_confirm()
         cls.contract = cls.env["contract.contract"].of_sale(cls.so)
 
-    def test_contract_netinstaller_specs(self):
+    def test_contract_netinstaller_specs_default(self):
         self.assertEqual(
             self.contract.netinstaller_specs(),
-            {"RAM": 8, "MODEL": "NV4XMB,ME,MZ"},
+            {
+                "RAM": 8,
+                "MODEL": "NV4XMB,ME,MZ",
+                "post_install_script": [
+                    {
+                        "git_clone_url": "https://gitlab.com/commown/grantees/install",
+                        "git_branch_name": "main",
+                        "cmd": "./commownScript.sh -u",
+                    }
+                ],
+            },
+        )
+
+    def test_contract_netinstaller_specs_custom_script(self):
+        self.partner.commercial_partner_id.write(
+            {
+                "netinstaller_exec_default_script": False,
+                "netinstaller_scripts": [
+                    Command.link(self.lref("custom_post_install_script").id)
+                ],
+            }
+        )
+
+        self.assertEqual(
+            self.contract.netinstaller_specs(),
+            {
+                "RAM": 8,
+                "MODEL": "NV4XMB,ME,MZ",
+                "post_install_script": [
+                    {
+                        "git_clone_url": "https://git.commown.coop/myscript",
+                        "git_branch_name": "main",
+                        "cmd": "./launch.sh",
+                    }
+                ],
+            },
         )
